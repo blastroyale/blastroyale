a554e6664906031a9db301e8a7cb02e63e804b25
Matt Ryan <vertexbuffer@protonmail.com>
2021-11-30 12:38:37 +0000

    code cleanup

--- Assets/Src/FirstLight/Game/StateMachines/MainMenuState.cs
+++ Assets/Src/FirstLight/Game/StateMachines/MainMenuState.cs
@@ -116,8 +116,7 @@ namespace FirstLight.Game.StateMachines
 			
 			initial.Transition().Target(screenCheck);
 			initial.OnExit(OpenUiVfxPresenter);
-			//initial.OnExit(OpenUiTooltipPresenter);
-
+			
 			screenCheck.Transition().Condition(CheckAutoLootBoxes).Target(collectLoot);
 			screenCheck.Transition().Condition(CheckOverflowLootSpentHc).Target(collectLoot);
 			screenCheck.Transition().Condition(CheckOverflowLoot).Target(overflowLootMenu);
@@ -338,12 +337,7 @@ namespace FirstLight.Game.StateMachines
 			_uiService.CloseUi<LootOptionsScreenPresenter>();
 			_services.MessageBrokerService.Publish(new LootScreenClosedMessage());
 		}
-		
-		private void OpenUiTooltipPresenter()
-		{ 
-			_uiService.OpenUi<UiTooltipPresenter>();
-		}
-		
+
 		private void OpenTrophyRoadMenuUI()
 		{
 			var data = new TrophyRoadScreenPresenter.StateData

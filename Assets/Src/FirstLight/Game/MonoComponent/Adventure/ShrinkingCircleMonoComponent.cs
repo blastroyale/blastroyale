4aaced63eceaec501042baf26bf81f709856ab80
Matt Ryan <vertexbuffer@protonmail.com>
2021-12-13 15:22:26 +0000

    shrinking circle TYPO fix

--- Assets/Src/FirstLight/Game/MonoComponent/Adventure/ShrinkingCircleMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/Adventure/ShrinkingCircleMonoComponent.cs
@@ -16,10 +16,17 @@ namespace FirstLight.Game.MonoComponent.Adventure
 		private void HandleUpdateView(CallbackUpdateView callback)
 		{
 			var frame = callback.Game.Frames.Verified;
-			var shrinkingCircle = frame.GetSingleton<ShrinkingCircle>();
+			var circle = frame.GetSingleton<ShrinkingCircle>();
+			
 
-			var size = shrinkingCircle.CurrentRadius.AsFloat * 2f;
-			transform.localScale = new Vector3(size, 1f, size);
+			var lerp = Mathf.Max(0, (frame.Time.AsFloat - circle.ShrinkingStartTime.AsFloat) / circle.ShrinkingDurationTime.AsFloat);
+			var diameter = Mathf.Lerp(circle.CurrentRadius.AsFloat, circle.TargetRadius.AsFloat, lerp) * 2f;
+			var center = Vector2.Lerp(circle.CurrentCircleCenter.ToUnityVector2(), circle.TargetCircleCenter.ToUnityVector2(), lerp);
+			
+			//Debug.Log($"ShrinkingCircleMonoComponent -> HandleUpdateView {lerp} {diameter} {center}");
+			
+			transform.localScale = new Vector3(diameter, 1f, diameter);
+			transform.position = new Vector3(center.x, transform.position.y, center.y);
 		}
 	}
 }

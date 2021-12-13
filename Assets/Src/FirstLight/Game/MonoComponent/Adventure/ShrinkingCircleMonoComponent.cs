6591726c41f13df9fef006a63f604658b3ad6da8
Matt Ryan <vertexbuffer@protonmail.com>
2021-12-13 15:49:32 +0000

    code cleanup

--- Assets/Src/FirstLight/Game/MonoComponent/Adventure/ShrinkingCircleMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/Adventure/ShrinkingCircleMonoComponent.cs
@@ -18,12 +18,9 @@ namespace FirstLight.Game.MonoComponent.Adventure
 			var frame = callback.Game.Frames.Verified;
 			var circle = frame.GetSingleton<ShrinkingCircle>();
 			
-
 			var lerp = Mathf.Max(0, (frame.Time.AsFloat - circle.ShrinkingStartTime.AsFloat) / circle.ShrinkingDurationTime.AsFloat);
 			var diameter = Mathf.Lerp(circle.CurrentRadius.AsFloat, circle.TargetRadius.AsFloat, lerp) * 2f;
 			var center = Vector2.Lerp(circle.CurrentCircleCenter.ToUnityVector2(), circle.TargetCircleCenter.ToUnityVector2(), lerp);
-			
-			//Debug.Log($"ShrinkingCircleMonoComponent -> HandleUpdateView {lerp} {diameter} {center}");
 			
 			transform.localScale = new Vector3(diameter, 1f, diameter);
 			transform.position = new Vector3(center.x, transform.position.y, center.y);

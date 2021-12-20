d73c12ca53cf515a2b6e78132e9f83b8e0914694
Miguel Tomas <game.gamester@gmail.com>
2021-12-20 15:25:30 +0000

    Fixed white safe area only showing when it should

--- Assets/Src/FirstLight/Game/MonoComponent/Adventure/ShrinkingCircleMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/Adventure/ShrinkingCircleMonoComponent.cs
@@ -1,3 +1,5 @@
+using FirstLight.Game.Services;
+using FirstLight.Game.Utils;
 using FirstLight.Game.Views;
 using Quantum;
 using UnityEngine;
@@ -12,9 +14,14 @@ namespace FirstLight.Game.MonoComponent.Adventure
 		[SerializeField] private CircleLineRenderer _shrinkingCircleLinerRenderer;
 		[SerializeField] private CircleLineRenderer _safeAreaCircleLinerRenderer;
 		[SerializeField] private Transform _damageZoneTransform;
+
+		private QuantumShrinkingCircleConfig _config;
+		private IGameServices _services;
 		
 		private void Awake()
 		{
+			_services = MainInstaller.Resolve<IGameServices>();
+			
 			QuantumCallback.Subscribe<CallbackUpdateView>(this, HandleUpdateView);
 		}
 
@@ -22,22 +29,31 @@ namespace FirstLight.Game.MonoComponent.Adventure
 		{
 			var frame = callback.Game.Frames.Verified;
 			var circle = frame.GetSingleton<ShrinkingCircle>();
-
 			var targetCircleCenter = circle.TargetCircleCenter.ToUnityVector2();
 			var targetRadius = circle.TargetRadius.AsFloat;
+			var safeArea = new Vector3(targetCircleCenter.x, _safeAreaCircleLinerRenderer.transform.position.y, targetCircleCenter.y);
 			var lerp = Mathf.Max(0, (frame.Time.AsFloat - circle.ShrinkingStartTime.AsFloat) / circle.ShrinkingDurationTime.AsFloat);
 			var radius = Mathf.Lerp(circle.CurrentRadius.AsFloat, targetRadius, lerp);
 			var center = Vector2.Lerp(circle.CurrentCircleCenter.ToUnityVector2(), targetCircleCenter, lerp);
-
+			var position = new Vector3(center.x, _shrinkingCircleLinerRenderer.transform.position.y, center.y);
 			var cachedTransform = _damageZoneTransform;
+
+			if (_config.Step != circle.Step)
+			{
+				_config = _services.ConfigsProvider.GetConfig<QuantumShrinkingCircleConfig>(circle.Step);
+			}
 			
-			var targetCenter = new Vector3(center.x, _shrinkingCircleLinerRenderer.transform.position.y, center.y);
-			
-			cachedTransform.position = targetCenter;
+			cachedTransform.position = position;
 			cachedTransform.localScale = new Vector3(radius * 2f, cachedTransform.localScale.y, radius * 2f);
 			
-			_shrinkingCircleLinerRenderer.Draw(targetCenter, radius);
-			_safeAreaCircleLinerRenderer.Draw(new Vector3(targetCircleCenter.x, _safeAreaCircleLinerRenderer.transform.position.y, targetCircleCenter.y), targetRadius);
+			_shrinkingCircleLinerRenderer.Draw(position, radius);
+
+			if (frame.Time < circle.ShrinkingStartTime - _config.WarningTime)
+			{
+				return;
+			}
+			
+			_safeAreaCircleLinerRenderer.Draw(safeArea, targetRadius);
 		}
 	}
 }

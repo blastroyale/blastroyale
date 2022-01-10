ad6926f38f5c0773e6b57683708e9a6ebc665d87
Nikita Denissov <nik.denissov@gmail.com>
2022-01-10 17:30:05 +0000

    The winner of BattleRoyale is the one with the least deaths

--- Quantum/quantum_code/quantum.code/Systems/GameSystem.cs
+++ Quantum/quantum_code/quantum.code/Systems/GameSystem.cs
@@ -18,12 +18,28 @@ namespace Quantum.Systems
 			var container = f.Unsafe.GetPointerSingleton<GameContainer>();
 			var matchData = container->PlayersData;
 			var playerWinner = (PlayerRef) 0;
-
-			for (var i = 1; i < f.RuntimeConfig.PlayersLimit; i++)
+			
+			// Deathmatch winner is the one with the most kills
+			if (container->GameMode == GameMode.Deathmatch)
 			{
-				if (matchData[i].PlayersKilledCount > matchData[playerWinner].PlayersKilledCount)
+				for (var i = 1; i < f.RuntimeConfig.PlayersLimit; i++)
 				{
-					playerWinner = i;
+					if (matchData[i].PlayersKilledCount > matchData[playerWinner].PlayersKilledCount)
+					{
+						playerWinner = i;
+					}
+				}
+			}
+			// BattleRoyale winner is the one with the least deaths (the last survivor)
+			else
+			{
+				for (var i = 1; i < f.RuntimeConfig.PlayersLimit; i++)
+				{
+					if (matchData[i].DeathCount + matchData[i].SuicideCount <
+					    matchData[playerWinner].DeathCount + matchData[playerWinner].SuicideCount)
+					{
+						playerWinner = i;
+					}
 				}
 			}
 			

0bdcdf4ede206d136a1c1d8ec4f72765c8e1da27
Matt Ryan <vertexbuffer@protonmail.com>
2022-01-17 17:18:26 +0000

    removed non local player icons

--- Assets/Src/FirstLight/Game/Views/RadarMapView.cs
+++ Assets/Src/FirstLight/Game/Views/RadarMapView.cs
@@ -11,15 +11,12 @@ namespace FirstLight.Game.Views
         [SerializeField] private Transform _playerRadarPing;
         [SerializeField] private Camera _camera;
         [SerializeField] private RectTransform _radarBackground;
-        [SerializeField] private GameObject _enemyPingPrefab;
         [SerializeField] private Animation _animation;
         [SerializeField] private AnimationClip _smallMiniMapClip;
         [SerializeField] private AnimationClip _extendedMiniMapClip;
         
         private const float CameraHeight = 10;
         private Transform _cameraTransform;
-        private readonly List<Transform> _players = new List<Transform>(30);
-        private readonly List<Transform> _pingTransforms = new List<Transform>();
         private IGameServices _services;
         private EntityView _playerEntityView;
         private bool _miniMapActivated = true;
@@ -30,11 +27,8 @@ namespace FirstLight.Game.Views
             _services = MainInstaller.Resolve<IGameServices>();
 
             _cameraTransform = _camera.transform;
-
-            QuantumEvent.Subscribe<EventOnPlayerSpawned>(this, HandlePlayerSpawned);
+            
             QuantumEvent.Subscribe<EventOnLocalPlayerSpawned>(this, OnLocalPlayerSpawned);
-            QuantumEvent.Subscribe<EventOnPlayerLeft>(this, OnEventOnPlayerLeft);
-            QuantumEvent.Subscribe<EventOnPlayerDead>(this, HandleOnPlayerDead);
         }
         
 
@@ -52,9 +46,6 @@ namespace FirstLight.Game.Views
 
         private void UpdateTick(float deltaTime)
         {
-            RegulatePingInstances();
-            PlotPingPositions();
-
             if (_miniMapActivated)
             {
                 _playerRadarPing.localPosition = Vector3.zero;
@@ -67,36 +58,7 @@ namespace FirstLight.Game.Views
             var position =  _miniMapActivated ? _playerEntityView.transform.position : Vector3.zero;
             _cameraTransform.position = new Vector3(position.x, CameraHeight, position.z);
         }
-
-        private void HandleOnPlayerDead(EventOnPlayerDead callback)
-        {
-            var playerEntityView = _services.EntityViewUpdaterService.GetManualView(callback.Entity);
-
-            _players.Remove(playerEntityView.transform);
-        }
-
-        private void OnEventOnPlayerLeft(EventOnPlayerLeft callback)
-        {
-            var playerEntityView = _services.EntityViewUpdaterService.GetManualView(callback.Entity);
-
-            _players.Remove(playerEntityView.transform);
-        }
-
-        private void HandlePlayerSpawned(EventOnPlayerSpawned callback)
-        {
-            var playerEntityView = _services.EntityViewUpdaterService.GetManualView(callback.Entity);
-            
-            var frame = callback.Game.Frames.Verified;
-            var player = frame.Get<PlayerCharacter>(playerEntityView.EntityRef).Player;
-
-            if (callback.Game.PlayerIsLocal(player))
-            {
-                return;
-            }
-
-            _players.Add(playerEntityView.transform);
-        }
-
+        
         public void ToggleMiniMapView()
         {
             _animation.clip = !_miniMapActivated ? _smallMiniMapClip : _extendedMiniMapClip;
@@ -105,18 +67,6 @@ namespace FirstLight.Game.Views
             _miniMapActivated = !_miniMapActivated;
         }
         
-        private void PlotPingPositions()
-        {
-            var count = _players.Count;
-            for (var i = 0; i < count; i++)
-            {
-                var player = _players[i];
-                var pingTransform = _pingTransforms[i];
-                
-                pingTransform.gameObject.SetActive(true);
-                SetPingPosition(pingTransform, player.transform.position);
-            }
-        }
 
         private void SetPingPosition(Transform pingTransform, Vector3 positionWorldSpace)
         {
@@ -128,32 +78,5 @@ namespace FirstLight.Game.Views
             
             pingTransform.localPosition = screenDelta;
         }
-
-        private void RegulatePingInstances()
-        {
-            var required = _players.Count;
-            while (_pingTransforms.Count < required)
-            {
-                AddPingInstance();
-            }
-
-            while (_pingTransforms.Count > required)
-            {
-                RemovePingInstance();
-            }
-        }
-
-        private void AddPingInstance()
-        {
-            _pingTransforms.Add(Instantiate(_enemyPingPrefab, _radarBackground).transform);
-        }
-
-        private void RemovePingInstance()
-        {
-            var lastIndex = _pingTransforms.Count - 1;
-            var last = _pingTransforms[lastIndex];
-            _pingTransforms.RemoveAt(lastIndex);
-            Destroy(last.gameObject);
-        }
-	}
+    }
 }

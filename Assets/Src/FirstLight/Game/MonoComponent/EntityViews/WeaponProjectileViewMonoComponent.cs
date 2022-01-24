d48efc36f15141dfda24f7a01ab3bd64906f6080
Matt Ryan <vertexbuffer@protonmail.com>
2022-01-24 12:16:44 +0000

    added weapon projectile view component

--- Assets/Src/FirstLight/Game/MonoComponent/EntityViews/WeaponProjectileViewMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/EntityViews/WeaponProjectileViewMonoComponent.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 namespace FirstLight.Game.MonoComponent.EntityViews
 {
 	/// <summary>
-	/// This Mono component handles spawning vfx when a weapon receives a quantum player attack event.
+	/// This Mono component handles spawning vfx when a weapon receives a quantum projectile fire and player attack event.
 	/// </summary>
-	public class WeaponViewMonoComponent : EntityViewBase
+	public class WeaponProjectileViewMonoComponent : EntityViewBase
 	{
 		[SerializeField] private ParticleSystem _particleSystem;
+		[SerializeField] private AdventureVfxSpawnerMonoComponent _spawnVfx;
 		
 		protected override void OnInit()
 		{
 			QuantumEvent.Subscribe<EventOnPlayerAttacked>(this, OnEventOnPlayerAttacked);
+			QuantumEvent.Subscribe<EventOnProjectileFired>(this, OnEventOnProjectileFired);
+		}
+		
+		private void OnEventOnProjectileFired(EventOnProjectileFired callback)
+		{
+			if (_spawnVfx && callback.ProjectileData.Attacker == EntityRef)
+			{
+				_spawnVfx.Spawn();
+			}
 		}
 		
 		private void OnEventOnPlayerAttacked(EventOnPlayerAttacked callback)

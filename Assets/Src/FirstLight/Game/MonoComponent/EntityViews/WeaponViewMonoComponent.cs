c8a33e776cc7c981b80ec15ebc6b6c16f19c76da
Matt Ryan <vertexbuffer@protonmail.com>
2022-01-25 14:10:58 +0000

    weapon vfx aim logic

--- Assets/Src/FirstLight/Game/MonoComponent/EntityViews/WeaponViewMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/EntityViews/WeaponViewMonoComponent.cs
@@ -1,31 +1,70 @@
+using FirstLight.Game.Input;
 using FirstLight.Game.MonoComponent.Vfx;
 using Quantum;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace FirstLight.Game.MonoComponent.EntityViews
 {
 	/// <summary>
-	/// This Mono component handles spawning vfx when a weapon receives a quantum player attack event.
+	/// This Mono component handles particle system playback when player aims.
 	/// </summary>
-	public class WeaponViewMonoComponent : EntityViewBase
+	public class WeaponViewMonoComponent : EntityViewBase, LocalInput.IGameplayActions
 	{
 		[SerializeField] private ParticleSystem _particleSystem;
 		
+		private LocalInput _localInput;
+		
 		protected override void OnInit()
 		{
-			QuantumEvent.Subscribe<EventOnPlayerAttacked>(this, OnEventOnPlayerAttacked);
+			_localInput = new LocalInput();
+			_localInput.Gameplay.SetCallbacks(this);
+			_localInput.Enable();
 		}
 		
-		private void OnEventOnPlayerAttacked(EventOnPlayerAttacked callback)
+		public void OnMove(InputAction.CallbackContext context)
 		{
-			if (_particleSystem)
+		}
+
+		public void OnAim(InputAction.CallbackContext context)
+		{
+			if (_particleSystem == null)
 			{
-				if (callback.PlayerEntity == EntityRef)
-				{
-					_particleSystem.Simulate(0.0f, true, true);
-					_particleSystem.Play();
-				}
+				return;
 			}
+
+			var game = QuantumRunner.Default;
+			var frame = game == null ? null : game.Game?.Frames?.Verified;
+			var isEmptied = frame != null && frame.TryGet<Weapon>(EntityView.EntityRef, out var weapon) && weapon.IsEmpty;
+			var direction = context.ReadValue<Vector2>();
+			var isShooting = direction.sqrMagnitude > 0;
+			var isPlaying = _particleSystem.isPlaying;
+			
+			if (!isPlaying && !isEmptied && isShooting)
+			{
+				_particleSystem.Simulate(0.0f, true, true);
+				_particleSystem.Play();
+			}
+			else if (isPlaying && (!isShooting || isEmptied))
+			{
+				_particleSystem.Stop();
+			}
+		}
+
+		public void OnSpecialAim(InputAction.CallbackContext context)
+		{
+		}
+
+		public void OnAimButton(InputAction.CallbackContext context)
+		{
+		}
+
+		public void OnSpecialButton0(InputAction.CallbackContext context)
+		{
+		}
+
+		public void OnSpecialButton1(InputAction.CallbackContext context)
+		{
 		}
 	}
 }

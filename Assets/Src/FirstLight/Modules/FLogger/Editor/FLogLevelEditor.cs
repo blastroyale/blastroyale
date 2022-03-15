f86d9fe752651345ea480e7d73e6e1fb8536eae8
Miha Krajnc <miha@krajnc.dev>
2022-03-15 17:15:37 +0100

    Clean up FLogger code

--- Assets/Src/FirstLight/Modules/FLogger/Editor/FLogLevelEditor.cs
+++ Assets/Src/FirstLight/Modules/FLogger/Editor/FLogLevelEditor.cs
@@ -23,7 +23,7 @@ namespace FirstLight.FLogger.Editor
 		private const string MenuNameInfo = "First Light Games/Logging/Info";
 		private const string MenuNameVerbose = "First Light Games/Logging/Verbose";
 
-		private static List<string> CurrentDefineSymbols;
+		private static readonly List<string> _currentDefineSymbols;
 
 		static FLogLevelEditor()
 		{
@@ -31,103 +31,117 @@ namespace FirstLight.FLogger.Editor
 				NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
 			var symbols = PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget);
 
-			CurrentDefineSymbols = symbols.Split(';').ToList();
-		}
-
-		private static FLogLevel? SelectedLevel
-		{
-			get
-			{
-				if (CurrentDefineSymbols.Contains(SymbolError)) return FLogLevel.Error;
-				if (CurrentDefineSymbols.Contains(SymbolWarn)) return FLogLevel.Warn;
-				if (CurrentDefineSymbols.Contains(SymbolInfo)) return FLogLevel.Info;
-				if (CurrentDefineSymbols.Contains(SymbolVerbose)) return FLogLevel.Verbose;
-
-				return null;
-			}
-
-			set
-			{
-				CurrentDefineSymbols.Remove(SymbolError);
-				CurrentDefineSymbols.Remove(SymbolWarn);
-				CurrentDefineSymbols.Remove(SymbolInfo);
-				CurrentDefineSymbols.Remove(SymbolVerbose);
-
-				switch (value)
-				{
-					case FLogLevel.Error:
-						CurrentDefineSymbols.Add(SymbolError);
-						break;
-					case FLogLevel.Warn:
-						CurrentDefineSymbols.Add(SymbolWarn);
-						break;
-					case FLogLevel.Info:
-						CurrentDefineSymbols.Add(SymbolInfo);
-						break;
-					case FLogLevel.Verbose:
-						CurrentDefineSymbols.Add(SymbolVerbose);
-						break;
-					case null:
-						// Do nothing - null means no logging, no symbols needed
-						break;
-					default:
-						throw new ArgumentOutOfRangeException(nameof(value), value, null);
-				}
-
-				var namedBuildTarget =
-					NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-				PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, CurrentDefineSymbols.ToArray());
-			}
+			_currentDefineSymbols = symbols.Split(';').ToList();
 		}
 
 		[MenuItem(MenuNameNone)]
-		private static void SetLevelNone() => SelectedLevel = null;
+		private static void SetLevelNone()
+		{
+			SetSelectedLevel(null);
+		}
 
 		[MenuItem(MenuNameError)]
-		private static void SetLevelError() => SelectedLevel = FLogLevel.Error;
+		private static void SetLevelError()
+		{
+			SetSelectedLevel(FLogLevel.Error);
+		}
 
 		[MenuItem(MenuNameWarn)]
-		private static void SetLevelWarn() => SelectedLevel = FLogLevel.Warn;
+		private static void SetLevelWarn()
+		{
+			SetSelectedLevel(FLogLevel.Warn);
+		}
 
 		[MenuItem(MenuNameInfo)]
-		private static void SetLevelInfo() => SelectedLevel = FLogLevel.Info;
+		private static void SetLevelInfo()
+		{
+			SetSelectedLevel(FLogLevel.Info);
+		}
 
 		[MenuItem(MenuNameVerbose)]
-		private static void SetLevelVerbose() => SelectedLevel = FLogLevel.Verbose;
+		private static void SetLevelVerbose()
+		{
+			SetSelectedLevel(FLogLevel.Verbose);
+		}
 
 		[MenuItem(MenuNameNone, true)]
 		private static bool SetLevelNoneValidate()
 		{
-			Menu.SetChecked(MenuNameNone, SelectedLevel == null);
-			return SelectedLevel != null;
+			return ValidateSetLevel(MenuNameNone, null);
 		}
 
 		[MenuItem(MenuNameError, true)]
 		private static bool SetLevelErrorValidate()
 		{
-			Menu.SetChecked(MenuNameError, SelectedLevel == FLogLevel.Error);
-			return SelectedLevel != FLogLevel.Error;
+			return ValidateSetLevel(MenuNameError, FLogLevel.Error);
 		}
 
 		[MenuItem(MenuNameWarn, true)]
 		private static bool SetLevelWarnValidate()
 		{
-			Menu.SetChecked(MenuNameWarn, SelectedLevel == FLogLevel.Warn);
-			return SelectedLevel != FLogLevel.Warn;
+			return ValidateSetLevel(MenuNameWarn, FLogLevel.Warn);
 		}
 
 		[MenuItem(MenuNameInfo, true)]
 		private static bool SetLevelInfoValidate()
 		{
-			Menu.SetChecked(MenuNameInfo, SelectedLevel == FLogLevel.Info);
-			return SelectedLevel != FLogLevel.Info;
+			return ValidateSetLevel(MenuNameInfo, FLogLevel.Info);
 		}
 
 		[MenuItem(MenuNameVerbose, true)]
 		private static bool SetLevelVerboseValidate()
 		{
-			Menu.SetChecked(MenuNameVerbose, SelectedLevel == FLogLevel.Verbose);
-			return SelectedLevel != FLogLevel.Verbose;
+			return ValidateSetLevel(MenuNameVerbose, FLogLevel.Verbose);
+		}
+
+		private static FLogLevel? GetSelectedLevel()
+		{
+			if (_currentDefineSymbols.Contains(SymbolError)) return FLogLevel.Error;
+			if (_currentDefineSymbols.Contains(SymbolWarn)) return FLogLevel.Warn;
+			if (_currentDefineSymbols.Contains(SymbolInfo)) return FLogLevel.Info;
+			if (_currentDefineSymbols.Contains(SymbolVerbose)) return FLogLevel.Verbose;
+
+			return null;
+		}
+
+		private static void SetSelectedLevel(FLogLevel? level)
+		{
+			_currentDefineSymbols.Remove(SymbolError);
+			_currentDefineSymbols.Remove(SymbolWarn);
+			_currentDefineSymbols.Remove(SymbolInfo);
+			_currentDefineSymbols.Remove(SymbolVerbose);
+
+			switch (level)
+			{
+				case FLogLevel.Error:
+					_currentDefineSymbols.Add(SymbolError);
+					break;
+				case FLogLevel.Warn:
+					_currentDefineSymbols.Add(SymbolWarn);
+					break;
+				case FLogLevel.Info:
+					_currentDefineSymbols.Add(SymbolInfo);
+					break;
+				case FLogLevel.Verbose:
+					_currentDefineSymbols.Add(SymbolVerbose);
+					break;
+				case null:
+					// Do nothing - null means no logging, no symbols needed
+					break;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(level), level, null);
+			}
+
+			var namedBuildTarget =
+				NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+			PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, _currentDefineSymbols.ToArray());
+		}
+
+		private static bool ValidateSetLevel(string menuName, FLogLevel? level)
+		{
+			var selectedLevel = GetSelectedLevel();
+			Menu.SetChecked(menuName, selectedLevel == level);
+			return selectedLevel != level;
 		}
 	}
 }
--- Assets/Src/FirstLight/Modules/FLogger/Runtime/FileFLogWriter.cs
+++ Assets/Src/FirstLight/Modules/FLogger/Runtime/FileFLogWriter.cs
@@ -13,7 +13,7 @@ namespace FirstLight.FLogger
 	{
 		private const string LogPathsKey = "FLogger.LogPaths";
 		private const int LogFilesToKeep = 10;
-		private const int MaxLogFileSize = 100; // In bytes
+		private const int MaxLogFileSize = 1000000; // In bytes
 		private const int MaxLogFileAge = 1; // In hours
 		private string LogsFolder = Path.Combine(Application.persistentDataPath, "Logs");
 
@@ -24,44 +24,41 @@ namespace FirstLight.FLogger
 
 		private void Init()
 		{
-			lock (this)
+			var pathsString = PlayerPrefs.GetString(LogPathsKey);
+			_logPaths = string.IsNullOrEmpty(pathsString) ? new List<string>() : pathsString.Split(';').ToList();
+			_logPath = _logPaths.Count == 0 ? null : _logPaths[^1];
+
+			// Create new log file if current one is too big or too old
+			var logInfo = _logPath == null ? null : new FileInfo(_logPath);
+			if (logInfo == null || logInfo.Length > MaxLogFileSize ||
+			    logInfo.LastWriteTime.AddHours(MaxLogFileAge) < DateTime.Now)
 			{
-				var pathsString = PlayerPrefs.GetString(LogPathsKey);
-				_logPaths = string.IsNullOrEmpty(pathsString) ? new List<string>() : pathsString.Split(';').ToList();
-				_logPath = _logPaths.Count == 0 ? null : _logPaths[^1];
-
-				// Create new log file if current one is too big or too old
-				var logInfo = _logPath == null ? null : new FileInfo(_logPath);
-				if (logInfo == null || logInfo.Length > MaxLogFileSize ||
-				    logInfo.LastWriteTime.AddHours(MaxLogFileAge) < DateTime.Now)
+				if (!Directory.Exists(LogsFolder))
 				{
-					if (!Directory.Exists(LogsFolder))
-					{
-						Directory.CreateDirectory(LogsFolder);
-					}
-
-					var logFileName = $"BlastRoyale_{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.log";
-					_logPath = Path.Combine(LogsFolder, logFileName);
-				}
-				else
-				{
-					_logPath = _logPaths[^1];
+					Directory.CreateDirectory(LogsFolder);
 				}
 
-				// Remove oldest log if we have more than the maximum allowed
-				_logPaths.Add(_logPath);
-				if (_logPaths.Count > LogFilesToKeep)
-				{
-					var oldestLog = _logPaths[0];
-					File.Delete(oldestLog);
-					_logPaths.RemoveAt(0);
-				}
-
-				PlayerPrefs.SetString(LogPathsKey, string.Join(';', _logPaths));
-
-				_streamWriter = new StreamWriter(_logPath, true) {AutoFlush = true};
-				_streamWriter.WriteLine("~~~~~~~~~ NEW SESSION ~~~~~~~~~");
+				var logFileName = $"BlastRoyale_{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.log";
+				_logPath = Path.Combine(LogsFolder, logFileName);
 			}
+			else
+			{
+				_logPath = _logPaths[^1];
+			}
+
+			// Remove oldest log if we have more than the maximum allowed
+			_logPaths.Add(_logPath);
+			if (_logPaths.Count > LogFilesToKeep)
+			{
+				var oldestLog = _logPaths[0];
+				File.Delete(oldestLog);
+				_logPaths.RemoveAt(0);
+			}
+
+			PlayerPrefs.SetString(LogPathsKey, string.Join(';', _logPaths));
+
+			_streamWriter = new StreamWriter(_logPath, true) {AutoFlush = true};
+			_streamWriter.WriteLine("~~~~~~~~~ NEW SESSION ~~~~~~~~~");
 		}
 
 		/// <inheritdoc />

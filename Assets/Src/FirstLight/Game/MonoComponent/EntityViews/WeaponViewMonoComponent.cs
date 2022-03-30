27db74ae59f0dc45d5347e51cee5fa7b96cb3e25
Fausto Fonseca <faustofonseca@gmail.com>
2022-03-30 10:56:07 +0100

    Refactoring weapon view component for lest config reads. Removing file that is not needed.

--- Assets/Src/FirstLight/Game/MonoComponent/EntityViews/WeaponViewMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/EntityViews/WeaponViewMonoComponent.cs
@@ -13,9 +13,25 @@ namespace FirstLight.Game.MonoComponent.EntityViews
 		
 		protected override void OnInit()
 		{
+			QuantumEvent.Subscribe<EventOnPlayerWeaponChanged>(this, OnWeaponChanged);
 			QuantumEvent.Subscribe<EventOnPlayerAttack>(this, OnEventOnPlayerAttack);
 			QuantumEvent.Subscribe<EventOnPlayerStopAttack>(this, OnEventOnPlayerStopAttack);
 			QuantumEvent.Subscribe<EventOnGameEnded>(this, OnEventOnGameEnded);
+			
+			var f = QuantumRunner.Default.Game.Frames.Verified;
+
+			var playerCharacter = f.Get<PlayerCharacter>(EntityRef);
+			UpdateParticleSystem((int)playerCharacter.CurrentWeapon.GameId);
+		}
+
+		private void OnWeaponChanged(EventOnPlayerWeaponChanged callback)
+		{
+			if (EntityRef != callback.Entity)
+			{
+				return;
+			}
+			
+			UpdateParticleSystem((int)callback.Weapon.GameId);
 		}
 
 		private void OnEventOnPlayerAttack(EventOnPlayerAttack callback)
@@ -25,24 +41,6 @@ namespace FirstLight.Game.MonoComponent.EntityViews
 				return;
 			}
 
-			if (!_particleSystem.isPlaying)
-			{
-				var config = Services.ConfigsProvider.GetConfig<QuantumWeaponConfig>((int) callback.WeaponConfigId);
-
-				if (config.AttackHitTime.AsFloat > 0)
-				{
-					// Particle System modules do not need to be reassigned back to the system; they are interfaces and not independent objects.
-					var main = _particleSystem.main;
-					main.startLifetime = config.AttackHitTime.AsFloat;
-					main.startSpeed = config.AttackRange.AsFloat / config.AttackHitTime.AsFloat;
-					main.startDelay = 0;
-					main.loop = false;
-					main.maxParticles = 10;
-					var emission = _particleSystem.emission;
-					emission.rateOverTime = 0.1f;
-				}
-			}
-			
 			_particleSystem.Stop();
 			_particleSystem.time = 0;
 			_particleSystem.Play();
@@ -61,6 +59,24 @@ namespace FirstLight.Game.MonoComponent.EntityViews
 		private void OnEventOnGameEnded(EventOnGameEnded callback)
 		{
 			_particleSystem.Stop();
+		}
+
+		private void UpdateParticleSystem(int weaponId)
+		{
+			var config = Services.ConfigsProvider.GetConfig<QuantumWeaponConfig>(weaponId);
+
+			if (config.AttackHitTime.AsFloat > 0)
+			{
+				// Particle System modules do not need to be reassigned back to the system; they are interfaces and not independent objects.
+				var main = _particleSystem.main;
+				main.startLifetime = config.AttackHitTime.AsFloat;
+				main.startSpeed = config.AttackRange.AsFloat / config.AttackHitTime.AsFloat;
+				main.startDelay = 0;
+				main.loop = false;
+				main.maxParticles = 10;
+				var emission = _particleSystem.emission;
+				emission.rateOverTime = 0.1f;
+			}
 		}
 	}
 }

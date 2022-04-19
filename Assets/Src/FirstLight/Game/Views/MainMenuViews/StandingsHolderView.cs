d77d3a67af2e8290acfb7db417449f524a01e0e8
Miha Krajnc <miha@krajnc.dev>
2022-04-19 14:02:25 +0200

    BR-56: Fix standings holder on deathmatch mode
    We no longer show trophies and spice in deathmatch mode.

--- Assets/Src/FirstLight/Game/Views/MainMenuViews/StandingsHolderView.cs
+++ Assets/Src/FirstLight/Game/Views/MainMenuViews/StandingsHolderView.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using FirstLight.Game.Configs;
 using FirstLight.Game.Logic;
 using FirstLight.Game.Services;
 using FirstLight.Game.Utils;
@@ -21,17 +20,20 @@ namespace FirstLight.Game.Views.MainMenuViews
 		[SerializeField] private RectTransform _contentTransform;
 		[SerializeField] private int _verticalEntrySpacing = 14;
 		[SerializeField] private UnityEngine.UI.Button _blockerButton;
-		
+
 		private IObjectPool<PlayerResultEntryView> _playerResultPool;
 		private IGameServices _services;
 		private IGameDataProvider _gameDataProvider;
+
+		private bool _showExtra;
 
 		private void Awake()
 		{
 			_services = MainInstaller.Resolve<IGameServices>();
 			_gameDataProvider = MainInstaller.Resolve<IGameDataProvider>();
 			var adventureInfo = _gameDataProvider.AppDataProvider.CurrentMapConfig;
-			_playerResultPool = new GameObjectPool<PlayerResultEntryView>((uint) adventureInfo.PlayersLimit, _resultEntryViewRef);
+			_playerResultPool =
+				new GameObjectPool<PlayerResultEntryView>((uint) adventureInfo.PlayersLimit, _resultEntryViewRef);
 
 			for (var i = 0; i < adventureInfo.PlayersLimit; i++)
 			{
@@ -41,30 +43,35 @@ namespace FirstLight.Game.Views.MainMenuViews
 			if (adventureInfo.PlayersLimit < 10)
 			{
 				var entryHeight = ((RectTransform) _resultEntryViewRef.transform).sizeDelta.y;
-				_contentTransform.sizeDelta = new Vector2(_contentTransform.sizeDelta.x, 
-				                                          (entryHeight + _verticalEntrySpacing) * (adventureInfo.PlayersLimit + 1));
+				_contentTransform.sizeDelta = new Vector2(_contentTransform.sizeDelta.x,
+				                                          (entryHeight + _verticalEntrySpacing) *
+				                                          (adventureInfo.PlayersLimit + 1));
 			}
-			
+
 			_blockerButton.onClick.AddListener(OnCloseClicked);
-			
-			
+
+
 			_resultEntryViewRef.gameObject.SetActive(false);
-			QuantumEvent.Subscribe<EventOnPlayerKilledPlayer>(this, OnEventOnPlayerKilledPlayer, onlyIfActiveAndEnabled : true);
+			QuantumEvent.Subscribe<EventOnPlayerKilledPlayer>(this, OnEventOnPlayerKilledPlayer,
+			                                                  onlyIfActiveAndEnabled: true);
 		}
 
 		/// <summary>
 		/// Initialises the Standings Holder with current player ranks, kills and deaths.
 		/// If _showExtra is set to true, also shows XP and coins earned.
 		/// </summary>
-		public void Initialise(List<QuantumPlayerMatchData> playerData, bool showExtra = true, bool enableBlockerButton = true)
+		public void Initialise(List<QuantumPlayerMatchData> playerData, bool showExtra = true,
+		                       bool enableBlockerButton = true)
 		{
 			_coinsHolder.SetActive(showExtra);
+			_xpHolder.SetActive(showExtra);
 			_blockerButton.gameObject.SetActive(enableBlockerButton);
+			_showExtra = showExtra;
 
-			Setup(playerData, showExtra);
+			Setup(playerData);
 		}
-		
-		private void Setup(List<QuantumPlayerMatchData> playerData, bool showExtra)
+
+		private void Setup(List<QuantumPlayerMatchData> playerData)
 		{
 			var pool = _playerResultPool.SpawnedReadOnly;
 			playerData.SortByPlayerRank();
@@ -73,7 +80,7 @@ namespace FirstLight.Game.Views.MainMenuViews
 			// Do the descending order. From the highest to the lowest value
 			for (var i = 0; i < pool.Count; i++)
 			{
-				pool[i].SetInfo(playerData[i], showExtra);
+				pool[i].SetInfo(playerData[i], _showExtra);
 			}
 		}
 
@@ -81,14 +88,13 @@ namespace FirstLight.Game.Views.MainMenuViews
 		{
 			gameObject.SetActive(false);
 		}
-		
 
 		/// <summary>
 		/// The scoreboard could update whilst it's open, e.g. players killed whilst looking at it, etc.
 		/// </summary>
 		private void OnEventOnPlayerKilledPlayer(EventOnPlayerKilledPlayer callback)
 		{
-			Setup(new List<QuantumPlayerMatchData>(callback.PlayersMatchData), false);
+			Setup(new List<QuantumPlayerMatchData>(callback.PlayersMatchData));
 		}
 	}
 }

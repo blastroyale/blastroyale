d374562be6da1c4c0bfd97bdd891f5ceae70067e
Robert Gesicki <robertgesicki@gmail.com>
2022-04-22 09:57:41 +0100

    + MatchmakingLoading now enables/disables relevant text in OnOpened (this present is part of initialization UI, so it's never unloaded, therfore stuff needs to be manually re-enabled)

--- Assets/Src/FirstLight/Game/Presenters/MatchmakingLoadingScreenPresenter.cs
+++ Assets/Src/FirstLight/Game/Presenters/MatchmakingLoadingScreenPresenter.cs
@@ -87,7 +87,10 @@ namespace FirstLight.Game.Presenters
 			var config = _gameDataProvider.AppDataProvider.SelectedMap.Value;
 
 			_allPlayersLoadedMatch = false;
-
+			
+			_getReadyToRumbleText.gameObject.SetActive(false);
+			_findingPlayersText.enabled = true;
+			_playersFoundText.enabled = true;
 			_playersFoundText.text = $"{0}/{config.PlayersLimit.ToString()}" ;
 			_rndWaitingTimeLowest = 2f / config.PlayersLimit;
 			_rndWaitingTimeBiggest = 8f / config.PlayersLimit;
@@ -104,6 +107,7 @@ namespace FirstLight.Game.Presenters
 			}
 			else
 			{
+				_roomNameRootObject.SetActive(true);
 				_roomNameText.text = string.Format(ScriptLocalization.MainMenu.RoomCurrentName, CurrentRoom.Name);
 				UpdatePlayersWaitingImages(CurrentRoom.PlayerCount);
 			}

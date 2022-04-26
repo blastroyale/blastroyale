6c622745a963c299c571014deffc7363291eafaa
Robert Gesicki <robertgesicki@gmail.com>
2022-04-26 12:05:19 +0100

    + Disconnection handling on match state, during final preload

--- Assets/Src/FirstLight/Game/StateMachines/MatchState.cs
+++ Assets/Src/FirstLight/Game/StateMachines/MatchState.cs
@@ -80,7 +80,8 @@ namespace FirstLight.Game.StateMachines
 			playerReadyCheck.Transition().Target(playerReadyWait);
 			
 			playerReadyWait.Event(AllPlayersReadyEvent).Target(gameSimulation);
-			
+			playerReadyWait.Event(NetworkState.PhotonDisconnectedEvent).OnTransition(CloseMatchmakingScreen).Target(unloading);
+
 			gameSimulation.Nest(_gameSimulationState.Setup).Target(unloading);
 			gameSimulation.Event(NetworkState.PhotonDisconnectedEvent).Target(unloading);
 			gameSimulation.Event(NetworkState.LeftRoomEvent).Target(unloading);

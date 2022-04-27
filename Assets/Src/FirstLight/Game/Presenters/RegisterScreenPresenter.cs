8022c5dfa8a7c76245bf63ca05ba269c014bb686
Robert Gesicki <robertgesicki@gmail.com>
2022-04-27 12:24:51 +0100

    + Presenter base scripts

---
+++ Assets/Src/FirstLight/Game/Presenters/RegisterScreenPresenter.cs
@@ -0,0 +1,38 @@
+using System;
+using FirstLight.Game.Services;
+using UnityEngine;
+using FirstLight.Game.Utils;
+using FirstLight.Game.Logic;
+using FirstLight.Game.Messages;
+using I2.Loc;
+using Quantum;
+using Button = UnityEngine.UI.Button;
+using Random = UnityEngine.Random;
+
+namespace FirstLight.Game.Presenters
+{
+	/// <summary>
+	/// This Presenter handles the Shop Menu.
+	/// </summary>
+	public class RegisterScreenPresenter : AnimatedUiPresenterData<RegisterScreenPresenter.StateData>
+	{
+		public struct StateData
+		{
+			public Action LoginClicked;
+			public Action GoToRegisterClicked;
+		}
+		
+		[SerializeField] private Button _closeButton;
+
+		private IGameDataProvider _gameDataProvider;
+		private IGameServices _services;
+
+		private void Awake()
+		{
+			_gameDataProvider = MainInstaller.Resolve<IGameDataProvider>();
+			_services = MainInstaller.Resolve<IGameServices>();
+
+			_closeButton.onClick.AddListener(Close);
+		}
+	}
+}

8191c116f9e4861184afc0abf13d80506230d4c8
Anil Das-Gupta <anil.sdasgupta@gmail.com>
2022-04-28 12:37:39 +0100

    Kill Holder view reneabled in Deathmatch and Battle Royale game modes.

--- Assets/Src/FirstLight/Game/Presenters/MatchHudPresenter.cs
+++ Assets/Src/FirstLight/Game/Presenters/MatchHudPresenter.cs
@@ -31,6 +31,7 @@ namespace FirstLight.Game.Presenters
 		[SerializeField] private TextMeshProUGUI _mapStatusText;
 		[SerializeField] private LeaderHolderView _leaderHolderView;
 		[SerializeField] private ScoreHolderView _scoreHolderView;
+		[SerializeField] private KillHolderView _killHolderView;
 		[SerializeField] private MapTimerView _mapTimerView;
 		[SerializeField] private ContendersLeftHolderMessageView _contendersLeftHolderMessageView;
 		[SerializeField] private ContendersLeftHolderView _contendersLeftHolderView;

725daace682ac628732bb82d50c97acfda186d3c
Gabriel Slomka <gabrielslomka@gmail.com>
2022-05-05 08:32:36 +0000

    User Command Mutex

---
+++ Backend/Backend/Db/IServerMutex.cs
@@ -0,0 +1,22 @@
+using System;
+using Backend.Db;
+using Medallion.Threading.Postgres;
+
+namespace Backend.Game;
+
+
+/// <summary>
+/// Interface for a distributed lock mechanism for the game server.
+/// </summary>
+public interface IServerMutex : IDisposable
+{
+	/// <summary>
+	/// Locks a given user from being modified. This should be a pessimistic mutex.
+	/// </summary>
+	void Lock(string userId);
+
+	/// <summary>
+	/// Unlocks the user for modifications. Releases the mutex.
+	/// </summary>
+	void Unlock(string userId);
+}

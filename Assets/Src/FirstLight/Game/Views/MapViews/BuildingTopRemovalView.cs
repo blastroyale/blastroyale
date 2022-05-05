b30dd825db2e77c35ce567ca6885630bc1d8b2d3
Fausto Fonseca <faustofonseca@gmail.com>
2022-05-05 16:25:23 +0100

    Added functionality to 4 buildings where we hide the top of the building when a player enters it

---
+++ Assets/Src/FirstLight/Game/Views/MapViews/BuildingTopRemovalView.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using FirstLight.Game.MonoComponent.EntityViews;
+using UnityEngine;
+
+namespace FirstLight.Game.Views.MapViews
+{
+	public class BuildingTopRemovalView : MonoBehaviour
+	{
+		private static readonly int _topAnimatorPlayerInsideParamNameHash = Animator.StringToHash("PlayerInside");
+			
+		[SerializeField] private Animator _topRemovalAnimator;
+
+		private HashSet<int> _playersInside = new ();
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if (other.gameObject.TryGetComponent<PlayerCharacterViewMonoComponent>(out _))
+			{
+				PlayerEnteredBuilding(other.gameObject);
+			}
+		}
+		private void OnTriggerExit(Collider other)
+		{
+			if (other.gameObject.TryGetComponent<PlayerCharacterViewMonoComponent>(out _))
+			{
+				PlayerExitedBuilding(other.gameObject);
+			}		
+		}
+
+		private void PlayerEnteredBuilding(GameObject player)
+		{
+			_playersInside.Add(player.GetInstanceID());
+
+			UpdateBuildingTop();
+		}
+
+		private void PlayerExitedBuilding(GameObject player)
+		{
+			_playersInside.Remove(player.GetInstanceID());
+
+			UpdateBuildingTop();
+		}
+
+		private void UpdateBuildingTop()
+		{
+			_topRemovalAnimator.SetBool(_topAnimatorPlayerInsideParamNameHash, _playersInside.Count > 0);
+		}
+	}
+}

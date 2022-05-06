3e906807dcfe8e59a9a4a2f999cf98073ff7e2cf
Robert Gesicki <robertgesicki@gmail.com>
2022-05-06 16:15:11 +0100

    + Res adjustments

--- Assets/Src/FirstLight/Game/Utils/GameConstants.cs
+++ Assets/Src/FirstLight/Game/Utils/GameConstants.cs
@@ -35,7 +35,7 @@ namespace FirstLight.Game.Utils
 		
 		// Min/max amounts of haptic vibration sharpness when a player is damaged
 		public const float DYNAMIC_RES_HIGH = 1f;
-		public const float DYNAMIC_RES_LOW = 0.4f; 
+		public const float DYNAMIC_RES_LOW = 0.55f; 
 
 		// Min/max amounts of haptic vibration sharpness when a player is damaged
 		public const float HAPTIC_IOS_DAMAGE_SHARPNESS_MIN = 0.3f;

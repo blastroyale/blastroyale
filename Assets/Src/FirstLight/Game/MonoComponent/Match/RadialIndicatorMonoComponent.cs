5b03dd3a75d21a467ec14a9c3a0d702db5f1b46c
Fausto Fonseca <faustofonseca@gmail.com>
2022-05-12 16:38:09 +0100

    Fix the raycast done by the Radial indicator in case it doesn't find any surface to apply the indicator

--- Assets/Src/FirstLight/Game/MonoComponent/Match/RadialIndicatorMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/Match/RadialIndicatorMonoComponent.cs
@@ -62,8 +62,14 @@ namespace FirstLight.Game.MonoComponent.Match
 			_position = new Vector3(move.x, _localHeight, move.y);;
 			transform.position = _playerTransform.position + _position;
 			var ray = new Ray(transform.position, Vector3.down);
-			Physics.Raycast(ray,out var raycastHit);
-			_indicator.transform.position = raycastHit.point;
+			if (Physics.Raycast(ray, out var raycastHit))
+			{
+				_indicator.transform.position = raycastHit.point;
+			}
+			else
+			{
+				_indicator.transform.position = Vector3.zero;
+			}
 		}
 	}
 }

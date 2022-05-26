d1dc73b32033cdad5f709db61d113966079658c4
Miha Krajnc <miha@krajnc.dev>
2022-05-26 21:50:47 +0200

    Support split value types in configs

---
+++ Quantum/quantum_code/quantum.code/Configs/QuantumGameModePair.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Quantum
+{
+	/// <summary>
+	/// Stores two values for a configurable field, one for BattleRoyale,
+	/// one for Deathmatch.
+	/// </summary>
+	[Serializable]
+	public struct QuantumGameModePair<TValue>
+	{
+		public TValue BattleRoyale;
+		public TValue Deathmatch;
+
+		public QuantumGameModePair(TValue battleRoyale, TValue deathmatch)
+		{
+			BattleRoyale = battleRoyale;
+			Deathmatch = deathmatch;
+		}
+
+		/// <summary>
+		/// Returns <see cref="QuantumGameModePair{TValue}.BattleRoyale"/> for <see cref="GameMode.BattleRoyale"/>
+		/// and <see cref="QuantumGameModePair{TValue}.Deathmatch"/> for <see cref="GameMode.Deathmatch"/>
+		/// </summary>
+		public TValue Get(GameMode mode)
+		{
+			switch (mode)
+			{
+				case GameMode.BattleRoyale:
+					return BattleRoyale;
+				case GameMode.Deathmatch:
+					return Deathmatch;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(mode), mode, null);
+			}
+		}
+
+		/// <inheritdoc cref="Get(GameMode)"/>
+		public TValue Get(Frame f)
+		{
+			return Get(f.RuntimeConfig.GameMode);
+		}
+
+		public override string ToString()
+		{
+			return $"[{BattleRoyale.ToString()},{Deathmatch.ToString()}]";
+		}
+	}
+}

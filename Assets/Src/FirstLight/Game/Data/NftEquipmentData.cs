f9215bdebd1eeb13a0eb773aa70a933c79daa3ea
Robert Gesicki <robertgesicki@gmail.com>
2022-05-27 12:51:50 +0100

    Add cooldown properties to equipment data

--- Assets/Src/FirstLight/Game/Data/NftEquipmentData.cs
+++ Assets/Src/FirstLight/Game/Data/NftEquipmentData.cs
@@ -7,7 +7,7 @@ using Quantum;
 namespace FirstLight.Game.Data
 {
 	/// <summary>
-	/// Holds NFT metada for the weapons a given player have. 
+	/// Holds NFT metadata for the weapons a given player have. 
 	/// </summary>
 	[Serializable]
 	public class NftEquipmentData
@@ -20,10 +20,16 @@ namespace FirstLight.Game.Data
 		public readonly Dictionary<UniqueId, string> TokenIds = new();
 		
 		/// <summary>
-		/// Some items might expire, due to being rented for instance. This maps the unique id and
-		/// the timestamp 
+		/// Some items might expire, due to being rented for instance.
+		/// This maps the unique id and the timestamp 
 		/// </summary>
 		public readonly Dictionary<UniqueId, ulong> ExpireTimestamps = new();
+		
+		/// <summary>
+		/// Items that have been transferred recently do not contribute to player's earning.
+		/// This maps the unique id and the timestamp 
+		/// </summary>
+		public readonly Dictionary<UniqueId, ulong> CooldownTimestamps = new();
 
 		/// <summary>
 		/// Field that holds the timestamp when nfts were last updated

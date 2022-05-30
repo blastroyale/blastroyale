e70d6bb343744c8e5733553722ece8119c8bcfc7
Gabriel Slomka <gabrielslomka@gmail.com>
2022-05-30 11:40:20 +0100

    MOved to own folder

--- Backend/ServerSDK/ServerPlugin.cs
+++ Backend/ServerSDK/ServerPlugin.cs
@@ -1,28 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using ServerSDK.Services;
+﻿namespace ServerSDK;
 
-namespace ServerSDK;
-
-/// <summary>
-/// Setup objects necessary for a plugin to initialize.
-/// This object holds hard-typed references to what can be accessed by a external plugin.
-/// </summary>
-public class PluginContext
-{
-	public readonly PluginEventManager PluginEventManager;
-	public readonly ILogger Log;
-	public readonly IServerStateService ServerState;
-	public readonly IServerMutex PlayerMutex;
-
-	public PluginContext(PluginEventManager evManager, IServiceProvider services)
-	{
-		PluginEventManager = evManager;
-		Log = services.GetService<ILogger>()!;
-		ServerState = services.GetService<IServerStateService>()!;
-		PlayerMutex = services.GetService<IServerMutex>()!;
-	}
-}
 
 /// <summary>
 /// Abstract class to be extended to implement new server plugins.

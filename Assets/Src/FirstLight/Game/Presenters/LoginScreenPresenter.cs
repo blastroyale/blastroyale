c8142995e6d37484c3b9865c67c9a6b154c11c4b
Eve <eve@firstlight.games>
2022-06-06 12:29:15 +0100

    Namespaces cleaned up Created a new button Dev Register Linked both buttons with marketplace Register button will redirect to marketplace, after getting back to the game there will be an option for a simple registration function without marketplace Dev Register button redirect to marketplace and will bring back to login screen only, no option for non marketplace registration Changed I2L from Register new account to Register Removed "or" text (login "or" register) now its only login, Register, Dev Register

--- Assets/Src/FirstLight/Game/Presenters/LoginScreenPresenter.cs
+++ Assets/Src/FirstLight/Game/Presenters/LoginScreenPresenter.cs
@@ -1,14 +1,7 @@
 using System;
-using FirstLight.Game.Services;
 using UnityEngine;
-using FirstLight.Game.Utils;
-using FirstLight.Game.Logic;
-using FirstLight.Game.Messages;
-using I2.Loc;
-using Quantum;
 using TMPro;
 using Button = UnityEngine.UI.Button;
-using Random = UnityEngine.Random;
 
 namespace FirstLight.Game.Presenters
 {
@@ -19,49 +12,31 @@ namespace FirstLight.Game.Presenters
 	{
 		public struct StateData
 		{
-			public Action<string,string> LoginClicked;
+			public Action<string, string> LoginClicked;
 			public Action GoToRegisterClicked;
 		}
-		
-		// TODO EVE
-		// This is where all of the magic happens.
-		// This script is linked to LoginScreen.prefab in the project, where all of the feature lives.
-		//
-		// First - you need to create a new button in LoginScreen.prefab
-		// * Duplicate RegisterButton, call it something like 'DevRegisterButton'
-		// * Under DevRegisterButton -> RegisterText - remove I2Localize component, and set the text to 'Dev Register'
-		// * Store reference to this button in this script like one of the other buttons
-		//
-		// The reason we remove the I2Localize component, is that this dev register button doesnt need translations - 
-		// its for developers only, so we can just write some text directly.
 
 		[SerializeField] private TMP_InputField _emailInputField;
 		[SerializeField] private TMP_InputField _passwordInputField;
-
-		// TODO EVE - remove this _registerRootObject and all lines that use it, not needed anymore
-		[SerializeField] private GameObject _registerRootObject;
 		[SerializeField] private Button _goToRegisterButton;
+		[SerializeField] private Button _goToDevRegisterButton;
 		[SerializeField] private Button _loginButton;
-		// TODO EVE - create and store the new dev register button
-		
 		[SerializeField] private GameObject _frontDimBlocker;
-		
+
 		private void Awake()
 		{
-			_registerRootObject.SetActive(Debug.isDebugBuild);
 			_goToRegisterButton.onClick.AddListener(GoToRegisterClicked);
 			_loginButton.onClick.AddListener(LoginClicked);
-			
-			// TODO EVE 
-			// Link the new _devRegisterButton to new GoToDevRegisterClicked method
-			// Activate this button _devRegisterButton on condition: Debug.isDebugBuild
+
+			_goToDevRegisterButton.onClick.AddListener(GoToDevRegisterClicked);
+			_goToDevRegisterButton.gameObject.SetActive(Debug.isDebugBuild);
 		}
-		
+
 		private void OnEnable()
 		{
 			SetFrontDimBlockerActive(false);
 		}
-		
+
 		/// <summary>
 		/// Sets the activity of the dimmed blocker image that covers the presenter
 		/// </summary>
@@ -69,7 +44,7 @@ namespace FirstLight.Game.Presenters
 		{
 			_frontDimBlocker.SetActive(active);
 		}
-		
+
 		private void LoginClicked()
 		{
 			Data.LoginClicked(_emailInputField.text, _passwordInputField.text);
@@ -79,24 +54,16 @@ namespace FirstLight.Game.Presenters
 		{
 			Data.GoToRegisterClicked();
 			
-			// TODO EVE - move the above line to GoToDevRegisterclicked instead
-			//
-			// Then you need this function to open the marketplace
-			// There is currently only dev marketplace online: http://flgmarketplacestorage.z33.web.core.windows.net/ 
-			// However, for release, we will need this function to open either the dev marketplace, or real marketplace,
-			// depending on whether we are playing on a debug build, or a release build.
-			//
-			// So:
-			// * Make an if statemen  based on Debug.isDebugBuild
-			// * Use method Application.OpenURL(""); to open marketplaces
-			// * For now, both cases in the if statements should open the dev marketplace
-			// * However, write a TODO in the else block
-			//	* The devs will need to know to replace the link with a real marketplace link once real market is open
-			//
-			// ***TEST*** on all platforms - editor, android, iOS, as OpenURL does different things on different platforms
+			if (Debug.isDebugBuild)
+			{
+				//The devs will need to know to replace the link with a real marketplace link once real market is open
+				Application.OpenURL("http://flgmarketplacestorage.z33.web.core.windows.net/");
+			}
 		}
 		
-		// TODO EVE - create a new 'GoToDevRegisterClicked' method
-		// This is a simple method that should only call Data.GoToRegisterClicked(); like in the current register function
+		private void GoToDevRegisterClicked()
+		{
+			Application.OpenURL("http://flgmarketplacestorage.z33.web.core.windows.net/");
+		}
 	}
 }

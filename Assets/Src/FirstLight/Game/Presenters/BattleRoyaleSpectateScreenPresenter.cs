39376fd437bae18a8c963581409806a97f0c2669
Miha Krajnc <miha@krajnc.dev>
2022-06-09 22:24:50 +0200

    Add initial spectate logic
    * Players are now able to spectate their killer
    * When the killer is killed we spectate their killer

---
+++ Assets/Src/FirstLight/Game/Presenters/BattleRoyaleSpectateScreenPresenter.cs
@@ -0,0 +1,33 @@
+using System;
+using FirstLight.UiService;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FirstLight.Game.Presenters
+{
+	/// <summary>
+	/// This is responsible for displaying the screen during spectate mode,
+	/// that follows your killer around.
+	/// </summary>
+	public class
+		BattleRoyaleSpectateScreenPresenter : UiPresenterData<BattleRoyaleSpectateScreenPresenter.StateData>
+	{
+		public struct StateData
+		{
+			public Action OnLeaveClicked;
+		}
+
+		[SerializeField, Required] private Button leaveButton;
+
+		public void Start()
+		{
+			leaveButton.onClick.AddListener(OnLeaveClicked);
+		}
+
+		private void OnLeaveClicked()
+		{
+			Data.OnLeaveClicked();
+		}
+	}
+}

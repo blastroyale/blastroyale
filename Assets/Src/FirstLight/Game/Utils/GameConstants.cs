f85bb51c47728a6d08ec658b5477b09874da19b3
Eve <eve@firstlight.games>
2022-06-13 10:25:59 +0100

    no message

--- Assets/Src/FirstLight/Game/Utils/GameConstants.cs
+++ Assets/Src/FirstLight/Game/Utils/GameConstants.cs
@@ -26,7 +26,6 @@ namespace FirstLight.Game.Utils
 		public static class Balance
 		{
 			public const float MAP_ROTATION_TIME_MINUTES = 10;
-			// TODO EVE - create a new 'const int' called NFT_AMOUNT_FOR_PLAY, set it to 3
 			public const int NFT_AMMOUNT_FOR_PLAY = 3;
 		}
 

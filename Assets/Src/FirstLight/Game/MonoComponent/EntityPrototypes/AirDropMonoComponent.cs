c1e365fe1391193c87f7178a2386a37fd90254c8
Eve <eve@firstlight.games>
2022-07-07 17:57:45 +0200

    Create airplane shadow moving through the map

--- Assets/Src/FirstLight/Game/MonoComponent/EntityPrototypes/AirDropMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/EntityPrototypes/AirDropMonoComponent.cs
@@ -1,6 +1,11 @@
+using System.Numerics;
+using Circuit;
+using DG.Tweening;
 using Quantum;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 namespace FirstLight.Game.MonoComponent.EntityPrototypes
 {
@@ -10,6 +15,7 @@ namespace FirstLight.Game.MonoComponent.EntityPrototypes
 	public class AirDropMonoComponent : EntityBase
 	{
 		[SerializeField, Required] private GameObject _parachute;
+		[SerializeField, Required] private GameObject _airplaneShadow;
 		[SerializeField, Required] private ParticleSystem _landingPS;
 		[SerializeField, Required] private Animation _landingAnim;
 
@@ -26,8 +32,24 @@ namespace FirstLight.Game.MonoComponent.EntityPrototypes
 				Services.AssetResolverService.RequestAsset<GameId, GameObject>(callback.AirDrop.Chest, true, true,
 				                                                               OnLoaded);
 				_parachute.SetActive(true);
+				
+				var airDrop = callback.AirDrop;
+
+				var airdropPosition = airDrop.Position.ToUnityVector3();
+				var airplaneDirection = airDrop.Direction.XOY.ToUnityVector3();
+
+				var distance = 150f;
+
+				Vector3 startingPosition = airdropPosition - airplaneDirection * distance + Vector3.up * 10f;
+				Vector3 targetPosition = airdropPosition + airplaneDirection * distance + Vector3.up * 10f;
+
+
+				_airplaneShadow.transform.position = startingPosition;
+				_airplaneShadow.transform.DOMove(targetPosition, 5f);
+				
 			}
 		}
+		
 
 		private void OnAirDropDropped(EventOnAirDropDropped callback)
 		{

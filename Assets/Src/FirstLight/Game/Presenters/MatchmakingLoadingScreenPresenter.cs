653f34bffb5d8c45bf3537806ddfe0805cf25463
Robert Gesicki <robertgesicki@gmail.com>
2022-07-11 15:58:15 +0100

    Fix overlapping UI bug in matchmaking screen

--- Assets/Src/FirstLight/Game/Presenters/MatchmakingLoadingScreenPresenter.cs
+++ Assets/Src/FirstLight/Game/Presenters/MatchmakingLoadingScreenPresenter.cs
@@ -163,17 +163,18 @@ namespace FirstLight.Game.Presenters
 				_loadingText.SetActive(false);
 				_leaveRoomButton.gameObject.SetActive(true);
 			}
-
-			var status = ScriptLocalization.AdventureMenu.ReadyStatusReady;
-
+			
 			if (_services.NetworkService.QuantumClient.LocalPlayer.IsMasterClient && !IsMatchmakingRoom &&
 			    _services.NetworkService.QuantumClient.CurrentRoom.IsOpen)
 			{
 				_lockRoomButton.gameObject.SetActive(true);
 				_botsToggleObjectRoot.SetActive(true);
 			}
-			
-			_spectateToggleObjectRoot.SetActive(true);
+
+			if (!IsMatchmakingRoom)
+			{
+				_spectateToggleObjectRoot.SetActive(true);
+			}
 
 			AddOrUpdatePlayerInList(_services.NetworkService.QuantumClient.LocalPlayer);
 

593eef5d75ef96aac152c9e92eef8ddd9ca06f14
Fausto Fonseca <faustofonseca@gmail.com>
2022-07-13 15:39:00 +0100

    Clean commented code

--- Assets/Src/FirstLight/Game/MonoComponent/Match/RadialIndicatorMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/Match/RadialIndicatorMonoComponent.cs
@@ -58,20 +58,7 @@ namespace FirstLight.Game.MonoComponent.Match
 		public void SetTransformState(Vector2 direction)
 		{
 			var move = direction * _maxRange;
-			//var move3 = new Vector3(move.x, 0, move.y);
 			var position = _playerTransform.position;
-			// position.y += 0.1f;
-			//
-			// // Testing if we have a blocker in the way
-			// var directionRay = new Ray(position, move3);
-			// if (Physics.Raycast(directionRay, out var directionRestrain, _maxRange))
-			// {
-			// 	var distanceToObstacle = (directionRestrain.point - position).magnitude;
-			// 	if (distanceToObstacle < _maxRange)
-			// 	{
-			// 		move = direction * distanceToObstacle;
-			// 	}
-			// }
 
 			// Getting the position the special is going to drop on
 			position += new Vector3(move.x, 10f, move.y);
@@ -82,6 +69,7 @@ namespace FirstLight.Game.MonoComponent.Match
 			}
 			else
 			{
+				// If there's no surface to drop the special on, we just define the y of the player
 				position = new Vector3(position.x, _playerTransform.position.y, position.z);
 			}
 			
--- Quantum/quantum_code/quantum.code/Components/SpecialStunGrenade.cs
+++ Quantum/quantum_code/quantum.code/Components/SpecialStunGrenade.cs
@@ -42,7 +42,6 @@ namespace Quantum
 			{
 				aimInput = FPVector2.ClampMagnitude(aimInput, FP._1);
 				targetPosition = attackerPosition + (aimInput * maxRange).XOY;
-				//targetPosition = QuantumHelpers.TryFindPosOnNavMesh(f, targetPosition, out var newPos) ? newPos : targetPosition;
 			}
 			
 			var targetRange = special.MaxRange;

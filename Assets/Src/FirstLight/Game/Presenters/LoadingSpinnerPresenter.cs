adc000b43aa9c109c35f2bd3812db0b8dcee3562
Fausto Fonseca <faustofonseca@gmail.com>
2022-07-19 16:58:36 +0100

    BR-277 Improve the way some dialogs open asynchronously without looking glitchy

--- Assets/Src/FirstLight/Game/Presenters/LoadingSpinnerPresenter.cs
+++ Assets/Src/FirstLight/Game/Presenters/LoadingSpinnerPresenter.cs
@@ -12,9 +12,28 @@ namespace FirstLight.Game.Presenters
 	{
 		[SerializeField] private RectTransform _spinnerImage;
 		[SerializeField] private float _anglePerSecond;
+		[SerializeField] private RectTransform _darkOverlay;
 
+		private float _startTime;
+		private bool _started;
+
+		private void Start()
+		{
+			_darkOverlay.gameObject.SetActive(false);
+			_spinnerImage.gameObject.SetActive(false);
+			_startTime = Time.time;
+		}
 		private void Update()
 		{
+			if (!_started && !(Time.time - _startTime > 0.3f)) return;
+
+			if (!_started)
+			{
+				_started = true;
+				_darkOverlay.gameObject.SetActive(true);
+				_spinnerImage.gameObject.SetActive(true);		
+			}
+			
 			_spinnerImage.Rotate(0f, 0f, _anglePerSecond * Time.deltaTime);
 		}
 	}

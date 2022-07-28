cd37ebebf9bfdc31b4aa9206ac0793cb6d464614
Miha Krajnc <miha@krajnc.dev>
2022-07-28 13:29:28 +0200

    BR-382: Fix player count in Spectate mode

--- Assets/Src/FirstLight/Game/Views/MainMenuViews/ContendersLeftView.cs
+++ Assets/Src/FirstLight/Game/Views/MainMenuViews/ContendersLeftView.cs
@@ -18,51 +18,59 @@ namespace FirstLight.Game.Views.MainMenuViews
 		[SerializeField, Required] private Animation _animation;
 		[SerializeField, Required] private AnimationClip _animationClipFadeInOut;
 		[SerializeField] private bool _displayNumberOnly;
-		
+
 		private IGameServices _services;
+		private IMatchServices _matchServices;
 
 		private void Awake()
 		{
 			_services = MainInstaller.Resolve<IGameServices>();
+			_matchServices = MainInstaller.Resolve<IMatchServices>();
+
 			_contendersLeftText.text = "0";
 
 			_services.MessageBrokerService.Subscribe<MatchStartedMessage>(OnMatchStarted);
-			
+			_matchServices.SpectateService.SpectatedPlayer.Observe(OnSpectatedPlayerChanged);
+
 			QuantumEvent.Subscribe<EventOnPlayerDead>(this, OnEventOnPlayerDead);
 		}
 
 		private void OnDestroy()
 		{
 			_services?.MessageBrokerService?.UnsubscribeAll(this);
+			_matchServices.SpectateService.SpectatedPlayer.StopObservingAll(this);
 		}
 
 		private void OnMatchStarted(MatchStartedMessage message)
 		{
 			UpdatePlayersAlive(QuantumRunner.Default.Game.Frames.Verified);
 		}
-		
+
 		private void OnEventOnPlayerDead(EventOnPlayerDead callback)
 		{
 			_animation.clip = _animationClipFadeInOut;
 			_animation.Rewind();
 			_animation.Play();
-			
+
 			UpdatePlayersAlive(callback.Game.Frames.Verified);
+		}
+
+		private void OnSpectatedPlayerChanged(SpectatedPlayer previous, SpectatedPlayer next)
+		{
+			// This is here because when joining as a spectator, OnMatchStarted is called
+			// sooner than we have all the necessary data to set up the first player count.
+			UpdatePlayersAlive(QuantumRunner.Default.Game.Frames.Verified);
 		}
 
 		private void UpdatePlayersAlive(Frame f)
 		{
-			var playersLeft = (f.GetSingleton<GameContainer>().TargetProgress+1) - f.GetSingleton<GameContainer>().CurrentProgress;
-			
-			if (_displayNumberOnly)
-			{
-				_contendersLeftText.text = playersLeft.ToString();
-			}
-			else
-			{
-				_contendersLeftText.text = string.Format(ScriptLocalization.AdventureMenu.ContendersRemaining, playersLeft);
-			}
-			
+			var playersLeft = f.GetSingleton<GameContainer>().TargetProgress + 1 -
+			                  f.GetSingleton<GameContainer>().CurrentProgress;
+
+			_contendersLeftText.text = _displayNumberOnly
+				                           ? playersLeft.ToString()
+				                           : string.Format(ScriptLocalization.AdventureMenu.ContendersRemaining,
+				                                           playersLeft);
 		}
 	}
 }

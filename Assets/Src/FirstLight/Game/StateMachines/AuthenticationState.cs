a8068198d2d7a87aaa053621a717232e1fe8a42e
Miguel Tomas <game.gamester@gmail.com>
2022-08-08 13:30:11 +0100

    Fix stage version PlayFab endpoint

--- Assets/Src/FirstLight/Game/StateMachines/AuthenticationState.cs
+++ Assets/Src/FirstLight/Game/StateMachines/AuthenticationState.cs
@@ -217,13 +217,13 @@ namespace FirstLight.Game.StateMachines
 			var quantumSettings = _services.ConfigsProvider.GetConfig<QuantumRunnerConfigs>().PhotonServerSettings;
 
 #if LIVE_SERVER
-			PlayFabSettings.TitleId = "***REMOVED***";
+			PlayFabSettings.TitleId = "302CF";
 			quantumSettings.AppSettings.AppIdRealtime = "***REMOVED***";
 #elif OFFCHAIN_SERVER
 			PlayFabSettings.TitleId = "***REMOVED***";
 			quantumSettings.AppSettings.AppIdRealtime = "81262db7-24a2-4685-b386-65427c73ce9d";
 #elif STAGE_SERVER
-			PlayFabSettings.TitleId = "302CF";
+			PlayFabSettings.TitleId = "***REMOVED***";
 			quantumSettings.AppSettings.AppIdRealtime = "***REMOVED***";
 #else
 			// Dev

11a4b0fd1eb814b44769cbd2a6cb9a16b26c57a3
Gabriel Slomka <gabrielslomka@gmail.com>
2022-08-15 13:58:53 +0000

    Server only udates playfab keys when needed

--- Backend/ServerSDK/Models/ServerState.cs
+++ Backend/ServerSDK/Models/ServerState.cs
@@ -9,6 +9,9 @@ namespace ServerSDK.Models;
 /// </summary>
 public class ServerState : Dictionary<string, string>
 {
+
+	private HashSet<Type> _updatedTypes = new HashSet<Type>();
+
 	public ServerState() 
 	{
 	}
@@ -16,6 +19,8 @@ public class ServerState : Dictionary<string, string>
 	public ServerState(Dictionary<string, string> data): base(data)
 	{
 	}
+
+	public HashSet<Type> UpdatedTypes => _updatedTypes;
 
 	public ulong GetVersion()
 	{
@@ -35,9 +40,13 @@ public class ServerState : Dictionary<string, string>
 	/// Sets a given model in server state.
 	/// Will serialize the model.
 	/// </summary>
-	public void SetModel(object model)
+	public void UpdateModel(object model)
 	{
 		var (typeName, data) = ModelSerializer.Serialize(model);
+		if(!this.TryGetValue(typeName, out var oldData) || oldData != data)
+		{ 
+			_updatedTypes.Add(model.GetType());
+		}
 		this[typeName] = data;
 	}
 
@@ -49,5 +58,24 @@ public class ServerState : Dictionary<string, string>
 		return TryGetValue(typeof(T).FullName, out var data)
 			       ? ModelSerializer.Deserialize<T>(data)
 			       : Activator.CreateInstance<T>();
+	}
+
+	public ServerState GetOnlyUpdatedState()
+	{
+		if(_updatedTypes.Count > 0)
+		{
+			var newStateToUpdate = new ServerState();
+			foreach(var updatedType in _updatedTypes)
+			{
+				if(this.TryGetValue(updatedType.FullName, out var oldData))
+				{
+					newStateToUpdate[updatedType.FullName] = oldData;
+				}
+			}
+			return newStateToUpdate;
+		} else
+		{
+			return this;
+		}
 	}
 }

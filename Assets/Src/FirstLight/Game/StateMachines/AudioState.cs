5c50c58003b7182f738bed020223fc8919c11548
Tiryon Williams <tiryon.w@gmail.com>
2022-08-18 10:38:13 +0100

    remove unneeded checks

--- Assets/Src/FirstLight/Game/StateMachines/AudioState.cs
+++ Assets/Src/FirstLight/Game/StateMachines/AudioState.cs
@@ -208,11 +208,7 @@ namespace FirstLight.Game.StateMachines
 
 		private void OnPlayerKillPlayer(EventOnPlayerKilledPlayer callback)
 		{
-			if (_matchServices.EntityViewUpdaterService == null)
-			{
-				return;
-			}
-
+			
 			var game = callback.Game;
 			var audio = AudioId.None;
 
@@ -246,11 +242,6 @@ namespace FirstLight.Game.StateMachines
 
 		private void OnChestOpened(EventOnChestOpened callback)
 		{
-			if (_matchServices.EntityViewUpdaterService == null)
-			{
-				return;
-			}
-
 			var audio = AudioId.ChestPickup;
 
 			if (audio != AudioId.None)
@@ -281,10 +272,6 @@ namespace FirstLight.Game.StateMachines
 
 		private void PlayExplosionSFX(GameId sourceId, Vector3 endPosition)
 		{
-			if (_matchServices.EntityViewUpdaterService == null)
-			{
-				return;
-			}
 
 			var audio = AudioId.None;
 
@@ -323,10 +310,6 @@ namespace FirstLight.Game.StateMachines
 
 		private void OnSpecialUsed(EventOnPlayerSpecialUsed callback)
 		{
-			if (_matchServices.EntityViewUpdaterService == null)
-			{
-				return;
-			}
 
 			var audio = AudioId.None;
 
@@ -354,10 +337,6 @@ namespace FirstLight.Game.StateMachines
 
 		private void OnCollectableCollected(EventOnCollectableCollected callback)
 		{
-			if (_matchServices.EntityViewUpdaterService == null)
-			{
-				return;
-			}
 
 			var audio = AudioId.None;
 			var collectableId = callback.CollectableId;
@@ -405,10 +384,6 @@ namespace FirstLight.Game.StateMachines
 
 		private void OnPlayerAttack(EventOnPlayerAttack callback)
 		{
-			if (_matchServices.EntityViewUpdaterService == null)
-			{
-				return;
-			}
 
 			if (_matchServices.EntityViewUpdaterService.TryGetView(callback.PlayerEntity, out var entityView))
 			{
@@ -422,10 +397,7 @@ namespace FirstLight.Game.StateMachines
 
 		private void OnDamageBlocked(EventOnDamageBlocked callback)
 		{
-			if (_matchServices.EntityViewUpdaterService == null)
-			{
-				return;
-			}
+
 			if (_matchServices.EntityViewUpdaterService.TryGetView(callback.Entity, out var entityView))
 			{
 				var audio = AudioId.DamageAbsorb;
@@ -435,10 +407,6 @@ namespace FirstLight.Game.StateMachines
 
 		private void OnPlayerDamaged(EventOnPlayerDamaged callback)
 		{
-			if (_matchServices.EntityViewUpdaterService == null)
-			{
-				return;
-			}
 
 			if (_matchServices.EntityViewUpdaterService.TryGetView(callback.Entity, out var entityView))
 			{

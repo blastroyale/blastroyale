83407938e7f63d97984b402b698dcccd09a0b27d
Gabriel Slomka <gabrielslomka@gmail.com>
2022-08-23 12:08:39 +0000

    Adding live operable feature flags

--- Assets/Src/FirstLight/Game/Utils/FeatureFlags.cs
+++ Assets/Src/FirstLight/Game/Utils/FeatureFlags.cs
@@ -1,3 +1,8 @@
+
+using System.Collections.Generic;
+using System.Reflection;
+using FirstLight.FLogger;
+
 namespace FirstLight.Game.Utils
 {
 	/// <summary>
@@ -17,17 +22,45 @@ namespace FirstLight.Game.Utils
 		/// If false, player can create/join rooms not locked by commit
 		/// </summary>
 		public static bool COMMIT_VERSION_LOCK = true;
-
-		/// <summary>
-		/// When true will point to new production environments.
-		/// </summary>
-		public static bool TEMP_PRODUCTION_PLAYFAB = false;
-
+		
 		/// <summary>
 		/// When true, will send end of match commands using quantum server consensus algorithm.
 		/// When false commands will go directly to our backend. 
 		/// To use this in our backend the backend needs to be compiled with this flag being False.
 		/// </summary>
 		public static bool QUANTUM_CUSTOM_SERVER = false;
+
+		/// <summary>
+		/// Parses the feature flags from a given input dictionary.
+		/// Keys of the dictionary will be matched as title feature flag keys referenced on the attributes.
+		/// Values will be converted to boolean ('true' or 'false)
+		/// </summary>
+		public static void ParseFlags(Dictionary<string, string> titleData)
+		{
+			if (TrySetFlag("QUANTUM_CUSTOM_SERVER", titleData, out var customServer))
+			{
+				QUANTUM_CUSTOM_SERVER = customServer;
+			}
+			if (TrySetFlag("COMMIT_VERSION_LOCK", titleData, out var commitVersionLock))
+			{
+				COMMIT_VERSION_LOCK = commitVersionLock;
+			}
+		}
+
+		private static bool TrySetFlag(string flagName, Dictionary<string, string> titleData, out bool flag)
+		{
+			if (titleData.TryGetValue(flagName, out var flagValue))
+			{
+				flag = flagValue.ToLower() == "true";
+				FLog.Verbose($"Setting title flag {flagName} to {flag}");
+				return true;
+			}
+			else
+			{
+				FLog.Verbose($"Disabling flag {flagName}");
+				flag = false;
+			}
+			return false;
+		}
 	}
 }

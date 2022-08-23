0630905207d4ac23f68a14186931b67b55d2a632
Robert Gesicki <robertgesicki@gmail.com>
2022-08-23 11:23:27 +0100

    Move LocalizationUtils.cs to proper namespace

--- Assets/Src/FirstLight/Game/Utils/LocalizationUtils.cs
+++ Assets/Src/FirstLight/Game/Utils/LocalizationUtils.cs
@@ -6,68 +6,71 @@ using I2.Loc;
 using Quantum;
 using UnityEngine;
 
-/// <summary>
-/// Helper utility methods to manage localization
-/// </summary>
-public static class LocalizationUtils
+namespace FirstLight.Game.Utils
 {
 	/// <summary>
-	/// Returns localized region name for the provided region key from Photon master servers
+	/// Helper utility methods to manage localization
 	/// </summary>
-	public static string GetRegionName(string regionKey)
+	public static class LocalizationUtils
 	{
-		switch (regionKey)
+		/// <summary>
+		/// Returns localized region name for the provided region key from Photon master servers
+		/// </summary>
+		public static string GetRegionName(string regionKey)
 		{
-			case "eu":
-				return ScriptLocalization.MainMenu.ServerEuropeName;
+			switch (regionKey)
+			{
+				case "eu":
+					return ScriptLocalization.MainMenu.ServerEuropeName;
 
-			case "us":
-				return ScriptLocalization.MainMenu.ServerAmericaName;
+				case "us":
+					return ScriptLocalization.MainMenu.ServerAmericaName;
 
-			case "hk":
-				return ScriptLocalization.MainMenu.ServerAsiaName;
-			
-			default:
-				return regionKey;
+				case "hk":
+					return ScriptLocalization.MainMenu.ServerAsiaName;
+
+				default:
+					return regionKey;
+			}
 		}
-	}
 
-	/// <summary>
-	/// Returns localized game mode name for the given Quantum game mode
-	/// </summary>
-	public static string GetGameModeName(GameMode mode)
-	{
-		switch (mode)
+		/// <summary>
+		/// Returns localized game mode name for the given Quantum game mode
+		/// </summary>
+		public static string GetGameModeName(GameMode mode)
 		{
-			case GameMode.Tutorial:
-				return ScriptLocalization.MainMenu.GameModeTuName;
-			
-			case GameMode.Deathmatch:
-				return ScriptLocalization.MainMenu.GameModeDmName;
-			
-			case GameMode.BattleRoyale:
-				return ScriptLocalization.MainMenu.GameModeBrName;
-			
-			default:
-				return "Invalid";
+			switch (mode)
+			{
+				case GameMode.Tutorial:
+					return ScriptLocalization.MainMenu.GameModeTuName;
+
+				case GameMode.Deathmatch:
+					return ScriptLocalization.MainMenu.GameModeDmName;
+
+				case GameMode.BattleRoyale:
+					return ScriptLocalization.MainMenu.GameModeBrName;
+
+				default:
+					return "Invalid";
+			}
 		}
-	}
-	
-	/// <summary>
-	/// Returns localized match type name for the given Quantum game mode
-	/// </summary>
-	public static string GetMatchTypeName(MatchType mode)
-	{
-		switch (mode)
+
+		/// <summary>
+		/// Returns localized match type name for the given Quantum game mode
+		/// </summary>
+		public static string GetMatchTypeName(MatchType mode)
 		{
-			case MatchType.Casual:
-				return ScriptLocalization.MainMenu.MatchTypeCasualName;
-			
-			case MatchType.Ranked:
-				return ScriptLocalization.MainMenu.MatchTypeRankedName;
-			
-			default:
-				return "Invalid";
+			switch (mode)
+			{
+				case MatchType.Casual:
+					return ScriptLocalization.MainMenu.MatchTypeCasualName;
+
+				case MatchType.Ranked:
+					return ScriptLocalization.MainMenu.MatchTypeRankedName;
+
+				default:
+					return "Invalid";
+			}
 		}
 	}
 }

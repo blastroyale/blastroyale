50ae970c11be3db4726d759c91fc66dfd174e4d8
Robert Gesicki <robertgesicki@gmail.com>
2022-08-25 11:05:53 +0100

    Changed whitespaces to tabs

--- Assets/Src/FirstLight/Game/Views/BootSplashscreenView.cs
+++ Assets/Src/FirstLight/Game/Views/BootSplashscreenView.cs
@@ -10,47 +10,49 @@ using FirstLight.Game.Data;
 using Newtonsoft.Json;
 using Sirenix.OdinInspector;
 using UnityEngine.Analytics;
+
 #pragma warning disable CS1998
 
 namespace FirstLight.Game.Views
 {
-   /// <summary>
-   /// The first entry object that shows the splash screen and boots the <see cref="Main"/> scene reference
-   /// </summary>
-   public class BootSplashscreenView : MonoBehaviour
-   {
-      private const string _bootSceneName = "Boot";
-      private const string _mainSceneName = "Main";
-      
-      [SerializeField, Required] private AudioSource _audioSource;
+	/// <summary>
+	/// The first entry object that shows the splash screen and boots the <see cref="Main"/> scene reference
+	/// </summary>
+	public class BootSplashscreenView : MonoBehaviour
+	{
+		private const string _bootSceneName = "Boot";
+		private const string _mainSceneName = "Main";
 
-      private void Awake()
-      {
-         // Hack to stop errors after build:
-         // Unity: NullReferenceException: Object reference not set to an instance of an object.
-         //     at UnityEngine.Rendering.DebugManager.UpdateActions () [0x00000] in <00000000000000000000000000000000>:0
-         // at UnityEngine.Rendering.DebugUpdater.Update () [0x00000] in <00000000000000000000000000000000>:0
-         //
-         // Know more and follow the Unity issue in https://issuetracker.unity3d.com/issues/isdebugbuild-returns-false-in-the-editor-when-its-value-is-checked-after-a-build
-         // Remove once Unity solves it and we have a patched version
-         DebugManager.instance.enableRuntimeUI = false;
-      }
-      
-      private async void Start()
-      {
-         var asyncOperation = SceneManager.LoadSceneAsync(_mainSceneName, LoadSceneMode.Additive);
+		[SerializeField, Required] private AudioSource _audioSource;
 
-         asyncOperation.allowSceneActivation = false;
+		private void Awake()
+		{
+			// Hack to stop errors after build:
+			// Unity: NullReferenceException: Object reference not set to an instance of an object.
+			//     at UnityEngine.Rendering.DebugManager.UpdateActions () [0x00000] in <00000000000000000000000000000000>:0
+			// at UnityEngine.Rendering.DebugUpdater.Update () [0x00000] in <00000000000000000000000000000000>:0
+			//
+			// Know more and follow the Unity issue in https://issuetracker.unity3d.com/issues/isdebugbuild-returns-false-in-the-editor-when-its-value-is-checked-after-a-build
+			// Remove once Unity solves it and we have a patched version
+			DebugManager.instance.enableRuntimeUI = false;
+		}
 
-         InitializePlugins();
-         
-         await InitAtt();
-         await StartAnalytics();
-         StartSplashScreen();
-         MergeScenes(asyncOperation);
-      }
-      private async Task InitAtt()
-      {
+		private async void Start()
+		{
+			var asyncOperation = SceneManager.LoadSceneAsync(_mainSceneName, LoadSceneMode.Additive);
+
+			asyncOperation.allowSceneActivation = false;
+
+			InitializePlugins();
+
+			await InitAtt();
+			await StartAnalytics();
+			StartSplashScreen();
+			MergeScenes(asyncOperation);
+		}
+
+		private async Task InitAtt()
+		{
 #if UNITY_IOS
          if (Unity.Advertisement.IosSupport.ATTrackingStatusBinding.GetAuthorizationTrackingStatus() == 
              Unity.Advertisement.IosSupport.ATTrackingStatusBinding.AuthorizationTrackingStatus.NOT_DETERMINED)
@@ -60,63 +62,63 @@ namespace FirstLight.Game.Views
             await Task.Delay(500);
          }
 #endif
-      }
+		}
 
-      private async Task StartAnalytics()
-      {
-         var dependencyStatus = FirebaseApp.CheckAndFixDependenciesAsync();
+		private async Task StartAnalytics()
+		{
+			var dependencyStatus = FirebaseApp.CheckAndFixDependenciesAsync();
 
-         await dependencyStatus;
-         
-         if (dependencyStatus.Result != DependencyStatus.Available)
-         {
-            throw new InitializationException(InitResult.FailedMissingDependency,
-                                              $"Firebase could not be initialized properly. Status: {dependencyStatus}");
-         }
-         
-         FirebaseApp.Create();
-         FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
-      }
+			await dependencyStatus;
 
-      private void StartSplashScreen()
-      {
-         var json = PlayerPrefs.GetString(nameof(AppData), "");
-         var isSoundEnabled = string.IsNullOrEmpty(json) || JsonConvert.DeserializeObject<AppData>(json).SfxEnabled;
-         
-         SplashScreen.Begin();
-         SplashScreen.Draw();
-         
-         if (isSoundEnabled)
-         {
-            _audioSource.Play();
-         }
-      }
+			if (dependencyStatus.Result != DependencyStatus.Available)
+			{
+				throw new InitializationException(InitResult.FailedMissingDependency,
+				                                  $"Firebase could not be initialized properly. Status: {dependencyStatus}");
+			}
 
-      private async void MergeScenes(AsyncOperation asyncOperation)
-      {
-         while (!SplashScreen.isFinished || asyncOperation.progress < 0.9f || _audioSource.isPlaying)
-         {
-            await Task.Yield();
-         }
+			FirebaseApp.Create();
+			FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+		}
 
-         asyncOperation.allowSceneActivation = true;
+		private void StartSplashScreen()
+		{
+			var json = PlayerPrefs.GetString(nameof(AppData), "");
+			var isSoundEnabled = string.IsNullOrEmpty(json) || JsonConvert.DeserializeObject<AppData>(json).SfxEnabled;
 
-         while (!asyncOperation.isDone)
-         {
-            await Task.Yield();
-         }
+			SplashScreen.Begin();
+			SplashScreen.Draw();
 
-         SceneManager.MergeScenes(SceneManager.GetSceneByName(_bootSceneName), 
-                                  SceneManager.GetSceneByName(_mainSceneName));
-         Destroy(gameObject);
-      }
+			if (isSoundEnabled)
+			{
+				_audioSource.Play();
+			}
+		}
 
-      private void InitializePlugins()
-      {
-	      if (Debug.isDebugBuild)
-	      {
-		      SRDebug.Init();
-	      }
-      }
-   }
+		private async void MergeScenes(AsyncOperation asyncOperation)
+		{
+			while (!SplashScreen.isFinished || asyncOperation.progress < 0.9f || _audioSource.isPlaying)
+			{
+				await Task.Yield();
+			}
+
+			asyncOperation.allowSceneActivation = true;
+
+			while (!asyncOperation.isDone)
+			{
+				await Task.Yield();
+			}
+
+			SceneManager.MergeScenes(SceneManager.GetSceneByName(_bootSceneName),
+			                         SceneManager.GetSceneByName(_mainSceneName));
+			Destroy(gameObject);
+		}
+
+		private void InitializePlugins()
+		{
+			if (Debug.isDebugBuild)
+			{
+				SRDebug.Init();
+			}
+		}
+	}
 }

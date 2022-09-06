2bb4f254f007970ea23a427c99d994ca248bd61b
Fausto Fonseca <faustofonseca@gmail.com>
2022-09-06 10:25:41 +0100

    BR-753 Fix the Mutator config importer

--- Assets/Src/FirstLight/Editor/SheetImporters/MutatorConfigsImporter.cs
+++ Assets/Src/FirstLight/Editor/SheetImporters/MutatorConfigsImporter.cs
@@ -1,23 +1,12 @@
-using System.Collections.Generic;
-using FirstLight.Editor.EditorTools;
 using FirstLight.Game.Configs;
-using FirstLight.Game.Configs.AssetConfigs;
 using Quantum;
 
 namespace FirstLight.Editor.SheetImporters
 {
 	public class MutatorConfigsImporter :
-		GoogleSheetConfigsAssetImporterBase<QuantumMutatorConfig, MutatorConfigs, CustomAssetConfigs>
+		GoogleSheetQuantumConfigsImporter<QuantumMutatorConfig, MutatorConfigs>
 	{
 		public override string GoogleSheetUrl =>
 			"***REMOVED***/edit#gid=54961578";
-
-		protected override QuantumMutatorConfig DeserializeAsset(Dictionary<string, string> data,
-		                                                         CustomAssetConfigs assetConfigs)
-		{
-			var config = QuantumDeserializer.DeserializeTo<QuantumMutatorConfig>(data);
-
-			return config;
-		}
 	}
 }

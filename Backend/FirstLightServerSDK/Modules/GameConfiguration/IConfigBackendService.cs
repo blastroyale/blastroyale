f55f1a04ce263424ba12ceb4b77d87e0c2792a91
Gabriel Slomka <gabrielslomka@gmail.com>
2022-09-06 11:19:18 +0000

    Server Configuration Updates

--- Backend/FirstLightServerSDK/Modules/GameConfiguration/IConfigBackendService.cs
+++ Backend/FirstLightServerSDK/Modules/GameConfiguration/IConfigBackendService.cs
@@ -13,11 +13,11 @@ namespace FirstLight.Server.SDK.Modules.GameConfiguration
 		/// Obtains the current version of configuration that is in backend.
 		/// Will be performed every request so has to be a fast operation.
 		/// </summary>
-		public Task<ulong> GetVersion();
+		public Task<ulong> GetRemoteVersion();
 
 		/// <summary>
 		/// Obtains a given configuration from the backend.
 		/// </summary>
-		public Task<IConfigsProvider> BuildConfiguration(ulong version);
+		public Task<IConfigsProvider> FetchRemoteConfiguration(ulong version);
 	}
 }

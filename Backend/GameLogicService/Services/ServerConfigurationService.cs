3a0b825cfbdff1a28118f7e5c91909c31b3f3461
Gabriel Slomka <gabrielslomka@gmail.com>
2022-09-26 07:31:29 +0000

    Fix server config

--- Backend/GameLogicService/Services/ServerConfigurationService.cs
+++ Backend/GameLogicService/Services/ServerConfigurationService.cs
@@ -15,12 +15,12 @@ namespace Backend.Game.Services
 		public string AppPath => _path;
 		public string PlayfabSecretKey => FromEnv("PLAYFAB_DEV_SECRET_KEY");
 		public string PlayfabTitle => FromEnv("PLAYFAB_TITLE");
-		public bool NftSync => FromEnv("NFT_SYNC") != "false";
+		public bool NftSync => FromEnv("NFT_SYNC", "true")?.ToLower() != "false";
 		public string DbConnectionString => FromEnv("SqlConnectionString");
 		public string? TelemetryConnectionString => FromEnv("APPLICATIONINSIGHTS_CONNECTION_STRING");
 		public Version MinClientVersion => new Version("0.4.0");
-		public bool DevelopmentMode => FromEnv("DEV_MODE") == "true";
-		public bool RemoteGameConfiguration => FromEnv("REMOTE_CONFIGURATION") == "true";
+		public bool DevelopmentMode => FromEnv("DEV_MODE")?.ToLower() == "true";
+		public bool RemoteGameConfiguration => FromEnv("REMOTE_CONFIGURATION")?.ToLower() == "true";
 		private static string FromEnv(string name, string defaultValue = null)
 		{
 			return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process) ??

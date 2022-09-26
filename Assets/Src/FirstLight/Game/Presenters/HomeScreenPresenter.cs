31f030c2a24f8e6ddb99fa3fe86316665a55d591
Miha Krajnc <miha@krajnc.dev>
2022-09-26 20:39:49 +0200

    BRG-708: Add initial HomeScreen implementation in UI Toolkit

--- Assets/Src/FirstLight/Game/Presenters/HomeScreenPresenter.cs
+++ Assets/Src/FirstLight/Game/Presenters/HomeScreenPresenter.cs
@@ -1,25 +1,19 @@
 using System;
-using System.Diagnostics;
 using UnityEngine;
-using FirstLight.Game.Configs;
 using FirstLight.Game.Utils;
 using FirstLight.Game.Logic;
-using I2.Loc;
 using FirstLight.Game.Services;
-using FirstLight.Game.Messages;
-using FirstLight.Game.Views.MainMenuViews;
-using Quantum;
-using Sirenix.OdinInspector;
-using TMPro;
-using Button = UnityEngine.UI.Button;
-using Debug = UnityEngine.Debug;
+using FirstLight.UiService;
+using I2.Loc;
+using UnityEngine.UIElements;
 
 namespace FirstLight.Game.Presenters
 {
 	/// <summary>
 	/// This Presenter handles the Home Screen.
 	/// </summary>
-	public class HomeScreenPresenter : AnimatedUiPresenterData<HomeScreenPresenter.StateData>
+	[LoadSynchronously]
+	public class HomeScreenPresenter : UiCloseActivePresenterData<HomeScreenPresenter.StateData>
 	{
 		public struct StateData
 		{
@@ -27,7 +21,6 @@ namespace FirstLight.Game.Presenters
 			public Action OnSettingsButtonClicked;
 			public Action OnLootButtonClicked;
 			public Action OnHeroesButtonClicked;
-			public Action OnSocialButtonClicked;
 			public Action OnPlayRoomJoinCreateClicked;
 			public Action OnNameChangeClicked;
 			public Action OnGameModeClicked;
@@ -35,92 +28,74 @@ namespace FirstLight.Game.Presenters
 			public Action OnBattlePassClicked;
 		}
 
-		[SerializeField, Required] private Button _playOnlineButton;
-		[SerializeField, Required] private Button _playRoom;
-		[SerializeField, Required] private Button _nameChangeButton;
-		[SerializeField, Required] private Button _settingsButton;
-		[SerializeField, Required] private Button _feedbackButton;
-		[SerializeField, Required] private Button _gameModeButton;
-		[SerializeField, Required] private Button _leaderboardButton;
-		[SerializeField, Required] private Button _battlePassButton;
-		[SerializeField, Required] private NewFeatureUnlockedView _newFeaturesView;
-		[SerializeField, Required] private TextMeshProUGUI _selectedGameModeText;
-		[SerializeField, Required] private TextMeshProUGUI _selectedGameModeTimerText;
+		[SerializeField] private UIDocument _document;
 
-		// Landscape Mode Buttons
-		[SerializeField, Required] private VisualStateButtonView _lootButton;
-		[SerializeField, Required] private VisualStateButtonView _heroesButton;
-		[SerializeField, Required] private Button _marketplaceButton;
-		[SerializeField, Required] private Button _discordButton;
+		private VisualElement _root;
 
 		private IGameDataProvider _gameDataProvider;
-		private IGameServices _services;
+		private IGameServices _gameServices;
 
-		private void Awake()
+		private Label _playerNameLabel;
+		private Label _playerTrophiesLabel;
+		private Label _gameModeLabel;
+
+		private void Start()
 		{
 			_gameDataProvider = MainInstaller.Resolve<IGameDataProvider>();
-			_services = MainInstaller.Resolve<IGameServices>();
+			_gameServices = MainInstaller.Resolve<IGameServices>();
 
-			_playOnlineButton.onClick.AddListener(OnPlayOnlineClicked);
-			_playRoom.onClick.AddListener(OnPlayRoomlicked);
-			_nameChangeButton.onClick.AddListener(OnNameChangeClicked);
-			_settingsButton.onClick.AddListener(OnSettingsButtonClicked);
-			_lootButton.Button.onClick.AddListener(OpenLootMenuUI);
-			_heroesButton.Button.onClick.AddListener(OpenHeroesMenuUI);
-			_marketplaceButton.onClick.AddListener(OpenMarketplaceLink);
-			_feedbackButton.onClick.AddListener(LeaveFeedbackForm);
-			_discordButton.onClick.AddListener(OpenDiscordLink);
-			_leaderboardButton.onClick.AddListener(OpenLeaderboardUI);
-			_gameModeButton.onClick.AddListener(OpenGameModeClicked);
-			_battlePassButton.onClick.AddListener(OpenBattlePassScreen);
+			_root = _document.rootVisualElement;
 
-			_leaderboardButton.gameObject.SetActive(FeatureFlags.LEADERBOARD_ACCESSIBLE);
-			_marketplaceButton.gameObject.SetActive(Debug.isDebugBuild);
-			_newFeaturesView.gameObject.SetActive(false);
-		}
+			_playerNameLabel = _root.Q<Label>("PlayerNameLabel");
+			_playerTrophiesLabel = _root.Q<Label>("PlayerTrophiesLabel");
+			_gameModeLabel = _root.Q<Label>("GameModeLabel");
 
-		private void Update()
-		{
-			var selectedGameModeInfo = _services.GameModeService.SelectedGameMode.Value;
-			if (!selectedGameModeInfo.IsFixed)
-			{
-				var timeLeft = selectedGameModeInfo.EndTime - DateTime.UtcNow;
-				_selectedGameModeTimerText.text = timeLeft.ToString(@"hh\:mm\:ss");
-			}
+			_root.Q<Button>("PlayButton").clicked += OnPlayButtonClicked;
+			_root.Q<Button>("GameModeButton").clicked += OnGameModeClicked;
+			_root.Q<Button>("SettingsButton").clicked += OnSettingsButtonClicked;
+			_root.Q<Button>("BattlePassButton").clicked += OnBattlePassButtonClicked;
+			_root.Q<Button>("CustomGameButton").clicked += OnCustomGameClicked;
+
+			_root.Q<Button>("EquipmentButton").clicked += OnEquipmentButtonClicked;
+			_root.Q<Button>("HeroesButton").clicked += OnHeroesButtonClicked;
+			_root.Q<Button>("MarketplaceButton").clicked += OnMarketplaceButtonClicked;
+			_root.Q<Button>("LeaderboardsButton").clicked += OnLeaderboardsButtonClicked;
+
+			_gameDataProvider.AppDataProvider.DisplayName.InvokeObserve(OnDisplayNameChanged);
+			_gameDataProvider.PlayerDataProvider.Trophies.InvokeObserve(OnTrophiesChanged);
+
+			_gameServices.GameModeService.SelectedGameMode.InvokeObserve(OnSelectedGameModeChanged);
 		}
 
 		private void OnDestroy()
 		{
-			Services?.MessageBrokerService?.UnsubscribeAll(this);
+			_gameDataProvider.AppDataProvider.DisplayName.StopObserving(OnDisplayNameChanged);
+			_gameDataProvider.PlayerDataProvider.Trophies.StopObserving(OnTrophiesChanged);
+			_gameServices.GameModeService.SelectedGameMode.StopObserving(OnSelectedGameModeChanged);
 		}
 
 		protected override void OnOpened()
 		{
 			base.OnOpened();
+			if (_root == null) return; // First open
 
-			_services.GameModeService.SelectedGameMode.InvokeObserve(RefreshGameModeButton);
+			_root.style.display = DisplayStyle.Flex;
 		}
 
 		protected override void OnClosed()
 		{
 			base.OnClosed();
-
-			_services.GameModeService.SelectedGameMode.StopObserving(RefreshGameModeButton);
+			_root.style.display = DisplayStyle.None;
 		}
 
-		private void OnPlayOnlineClicked()
+		private void OnPlayButtonClicked()
 		{
 			Data.OnPlayButtonClicked();
 		}
 
-		private void OnPlayRoomlicked()
+		private void OnGameModeClicked()
 		{
-			Data.OnPlayRoomJoinCreateClicked();
-		}
-
-		private void OnNameChangeClicked()
-		{
-			Data.OnNameChangeClicked();
+			Data.OnGameModeClicked();
 		}
 
 		private void OnSettingsButtonClicked()
@@ -128,57 +103,49 @@ namespace FirstLight.Game.Presenters
 			Data.OnSettingsButtonClicked();
 		}
 
-		private void OpenLootMenuUI()
-		{
-			Data.OnLootButtonClicked();
-		}
-
-		private void OpenHeroesMenuUI()
-		{
-			Data.OnHeroesButtonClicked();
-		}
-
-		private void OpenMarketplaceLink()
-		{
-			Application.OpenURL(GameConstants.Links.MARKETPLACE_URL);
-		}
-
-		private void OpenLeaderboardUI()
-		{
-			Data.OnLeaderboardClicked();
-		}
-
-		private void OpenGameModeClicked()
-		{
-			Data.OnGameModeClicked();
-		}
-
-		private void OpenSocialMenuUI()
-		{
-			Data.OnSocialButtonClicked();
-		}
-
-		private void OpenBattlePassScreen()
+		private void OnBattlePassButtonClicked()
 		{
 			Data.OnBattlePassClicked();
 		}
 
-		private void LeaveFeedbackForm()
+		private void OnCustomGameClicked()
 		{
-			Application.OpenURL(GameConstants.Links.FEEDBACK_FORM);
+			Data.OnPlayRoomJoinCreateClicked();
 		}
 
-		private void OpenDiscordLink()
+		private void OnEquipmentButtonClicked()
 		{
-			Application.OpenURL(GameConstants.Links.DISCORD_SERVER);
+			Data.OnLootButtonClicked();
 		}
 
-		private void RefreshGameModeButton(GameModeInfo _, GameModeInfo info)
+		private void OnHeroesButtonClicked()
 		{
-			_selectedGameModeTimerText.gameObject.SetActive(!info.IsFixed);
-			_selectedGameModeText.text = string.Format(ScriptLocalization.MainMenu.SelectedGameModeValue,
-			                                           info.Entry.MatchType.GetTranslation().ToUpper(),
-			                                           info.Entry.GameModeId.ToUpper());
+			Data.OnHeroesButtonClicked();
+		}
+
+		private void OnMarketplaceButtonClicked()
+		{
+			Application.OpenURL(GameConstants.Links.MARKETPLACE_URL);
+		}
+
+		private void OnLeaderboardsButtonClicked()
+		{
+			Data.OnLeaderboardClicked();
+		}
+
+		private void OnTrophiesChanged(uint _, uint current)
+		{
+			_playerTrophiesLabel.text = current.ToString();
+		}
+
+		private void OnDisplayNameChanged(string _, string current)
+		{
+			_playerNameLabel.text = current;
+		}
+
+		private void OnSelectedGameModeChanged(GameModeInfo _, GameModeInfo current)
+		{
+			_gameModeLabel.text = string.Format(current.Entry.GameModeId.ToUpper());
 		}
 	}
 }

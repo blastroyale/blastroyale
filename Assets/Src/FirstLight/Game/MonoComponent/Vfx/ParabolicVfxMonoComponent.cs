47e0eeeec0be261f0bd8bc2316bf8066d548765f
Fausto Fonseca <faustofonseca@gmail.com>
2022-10-04 18:35:19 +0100

    BRG-856: Fix for errors we get when the grenades are released right on top of us (with zero travelling time)

--- Assets/Src/FirstLight/Game/MonoComponent/Vfx/ParabolicVfxMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/Vfx/ParabolicVfxMonoComponent.cs
@@ -24,15 +24,18 @@ namespace FirstLight.Game.MonoComponent.Vfx
 		/// </summary>
 		public void StartParabolic(Vector3 targetPosition, float flyTime)
 		{
-			var forceDirection = targetPosition - transform.position;
-			var force = -_rigidbody.velocity;
-			var yForce = Mathf.Abs(Physics.gravity.y) * Mathf.Pow(flyTime, 2) / 2;
+			if (flyTime != 0)
+			{
+				var forceDirection = targetPosition - transform.position;
+				var force = -_rigidbody.velocity;
+				var yForce = Mathf.Abs(Physics.gravity.y) * Mathf.Pow(flyTime, 2) / 2;
 
-			force.x += forceDirection.x / flyTime;
-			force.y += (forceDirection.y + yForce) / flyTime;
-			force.z += forceDirection.z / flyTime;
+				force.x += forceDirection.x / flyTime;
+				force.y += (forceDirection.y + yForce) / flyTime;
+				force.z += forceDirection.z / flyTime;
 
-			_rigidbody.AddForce(force, ForceMode.VelocityChange);
+				_rigidbody.AddForce(force, ForceMode.VelocityChange);
+			}
 
 			Despawner(flyTime);
 		}

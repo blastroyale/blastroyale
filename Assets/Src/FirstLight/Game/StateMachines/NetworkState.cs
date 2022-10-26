bf56ca662f38f90ff767d3fe6508f4aa0bb1532a
Robert Gesicki <robertgesicki@gmail.com>
2022-10-26 17:37:23 +0100

    Re-enable auto reconnections

--- Assets/Src/FirstLight/Game/StateMachines/NetworkState.cs
+++ Assets/Src/FirstLight/Game/StateMachines/NetworkState.cs
@@ -195,13 +195,12 @@ namespace FirstLight.Game.StateMachines
 
 		private async void SubscribeDisconnectEvents()
 		{
-			//TODO TEST
-			//_services.TickService.SubscribeOnUpdate(TickReconnectAttempt, GameConstants.Network.NETWORK_ATTEMPT_RECONNECT_SECONDS, true, true);
+			_services.TickService.SubscribeOnUpdate(TickReconnectAttempt, GameConstants.Network.NETWORK_ATTEMPT_RECONNECT_SECONDS, true, true);
 			_criticalDisconnectCoroutine = _services.CoroutineService.StartCoroutine(CriticalDisconnectCoroutine());
 
 			await Task.Yield();
 		
-			//TickReconnectAttempt(0);
+			TickReconnectAttempt(0);
 		}
 
 		private void UnsubscribeDisconnectEvents()

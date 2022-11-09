bdbf6155ac6da21368f8d3bbfe1f3b02df7480f5
Tiryon Williams <tiryon.w@gmail.com>
2022-11-09 12:24:58 +0000

    fix dynamic range indicators

--- Assets/Src/FirstLight/Game/Views/MatchHudViews/LocalPlayerIndicatorContainerView.cs
+++ Assets/Src/FirstLight/Game/Views/MatchHudViews/LocalPlayerIndicatorContainerView.cs
@@ -149,7 +149,7 @@ namespace FirstLight.Game.Views.MatchHudViews
 			var isEmptied = playerCharacter->IsAmmoEmpty(f, _localPlayerEntity);
 
 			var rangeStat = f.Get<Stats>(_localPlayerEntity).GetStatData(StatType.AttackRange).StatValue;
-			var range = QuantumHelpers.GetDynamicAimValue(kcc, rangeStat + _weaponConfig.AttackRangeAimBonus, rangeStat).AsFloat;
+			var range = QuantumHelpers.GetDynamicAimValue(kcc, rangeStat, rangeStat + _weaponConfig.AttackRangeAimBonus).AsFloat;
 
 			var minAttackAngle = _shootIndicatorId == IndicatorVfxId.Line ? 0 : _weaponConfig.MinAttackAngle;
 			var maxAttackAngle = _shootIndicatorId == IndicatorVfxId.Line ? 0 :_weaponConfig.MaxAttackAngle;

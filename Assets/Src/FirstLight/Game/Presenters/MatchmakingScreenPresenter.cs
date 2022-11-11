6b80ee27bb787641bef9df66899d5609aa061b24
Robert Gesicki <robertgesicki@gmail.com>
2022-11-11 10:46:18 +0000

    Fix few more MM UI elements, add leave button

--- Assets/Src/FirstLight/Game/Presenters/MatchmakingScreenPresenter.cs
+++ Assets/Src/FirstLight/Game/Presenters/MatchmakingScreenPresenter.cs
@@ -1,38 +1,53 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using FirstLight.Game.Services;
+using FirstLight.Game.Utils;
 using FirstLight.UiService;
 using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 namespace FirstLight.Game.Presenters
 {
 	public class MatchmakingScreenPresenter : UiToolkitPresenterData<MatchmakingScreenPresenter.StateData>, IInRoomCallbacks
 	{
+		private Button _leaveButton;
+		
 		public struct StateData
 		{
 			public Action LeaveRoomClicked;
 		}
+		
+		protected override void QueryElements(VisualElement root)
+		{
+			base.QueryElements(root);
+		}
 
 		public void OnPlayerEnteredRoom(Player newPlayer)
 		{
+			
 		}
 
 		public void OnPlayerLeftRoom(Player otherPlayer)
 		{
+			
 		}
 
 		public void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
 		{
+			
 		}
 
 		public void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
 		{
+			
 		}
 
 		public void OnMasterClientSwitched(Player newMasterClient)
 		{
+			
 		}
 	}
 }

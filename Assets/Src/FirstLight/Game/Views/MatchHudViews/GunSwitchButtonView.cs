4e8b595fb07300147b7ee79141a863fec9e71ff5
Nikita Denissov <nik.denissov@gmail.com>
2022-11-24 17:02:36 +0000

    Add a button to match controls to switch between guns or from melee to a gun

---
+++ Assets/Src/FirstLight/Game/Views/MatchHudViews/GunSwitchButtonView.cs
@@ -0,0 +1,52 @@
+using FirstLight.Game.Utils;
+using Quantum;
+using Quantum.Commands;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Button = UnityEngine.UI.Button;
+
+namespace FirstLight.Game.Views.MatchHudViews
+{
+	/// <summary>
+	/// Displays a button to switch between guns.
+	/// </summary>
+	public class GunSwitchButtonView : MonoBehaviour
+	{
+		[SerializeField, Required, TabGroup("Refs")]
+		private Button _button;
+
+		private void Awake()
+		{
+			_button.onClick.AddListener(OnGunSwitchClicked);
+		}
+		
+		private void OnGunSwitchClicked()
+		{
+			var data = QuantumRunner.Default.Game.GetLocalPlayerData(false, out var f);
+
+			// Check if there is a point in switching or not. Avoid extra commands to save network message traffic $$$
+			if (!f.TryGet<PlayerCharacter>(data.Entity, out var pc))
+			{
+				return;
+			}
+			
+			var slotIndexToSwitch = -1;
+			
+			if (pc.CurrentWeaponSlot != 1 && pc.WeaponSlots[1].Weapon.IsValid())
+			{
+				slotIndexToSwitch = 1;
+			}
+			else if (pc.CurrentWeaponSlot != 2 && pc.WeaponSlots[2].Weapon.IsValid())
+			{
+				slotIndexToSwitch = 2;
+			}
+			
+			if (slotIndexToSwitch == -1)
+			{
+				return;
+			}
+
+			QuantumRunner.Default.Game.SendCommand(new WeaponSlotSwitchCommand { WeaponSlotIndex = slotIndexToSwitch });
+		}
+	}
+}

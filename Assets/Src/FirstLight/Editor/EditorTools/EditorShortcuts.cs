670b7821f89f4ed81ba8ada0ba430e9b448f419c
Miha Krajnc <miha@krajnc.dev>
2022-12-01 02:40:28 +0100

    Add merge colliders art tool (menu)

--- Assets/Src/FirstLight/Editor/EditorTools/EditorShortcuts.cs
+++ Assets/Src/FirstLight/Editor/EditorTools/EditorShortcuts.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Object = System.Object;
 
 namespace FirstLight.Editor.EditorTools
 {
@@ -15,59 +19,59 @@ namespace FirstLight.Editor.EditorTools
 		{
 			EditorSceneManager.OpenScene(GetScenePath("FtueDeck"));
 		}
-		
+
 		[MenuItem("FLG/Scene/Open Main Scene &1")]
 		private static void OpenMainScene()
 		{
 			var list = new List<string>(AssetDatabase.FindAssets("t:scene Main"));
-			
+
 			list.Sort();
-			
+
 			EditorSceneManager.OpenScene(AssetDatabase.GUIDToAssetPath(list[0]));
 		}
-		
+
 		[MenuItem("FLG/Scene/Open Main Menu Scene &2")]
 		private static void OpenMainMenuScene()
 		{
 			EditorSceneManager.OpenScene(GetScenePath("MainMenu"));
 		}
-		
+
 		[MenuItem("FLG/Scene/Open FloodCity Scene &3")]
 		private static void OpenFloodCityScene()
 		{
 			EditorSceneManager.OpenScene(GetScenePath("FloodCity"));
 		}
-		
+
 		[MenuItem("FLG/Scene/Open SmallWilderness Scene &4")]
 		private static void OpenSmallWildernessScene()
 		{
 			EditorSceneManager.OpenScene(GetScenePath("SmallWilderness"));
 		}
-		
+
 		[MenuItem("FLG/Scene/Open MainDeck Scene &5")]
 		private static void OpenMainDeckScene()
 		{
 			EditorSceneManager.OpenScene(GetScenePath("MainDeck"));
 		}
-		
+
 		[MenuItem("FLG/Scene/Open BlimpDeck Scene")]
 		private static void OpenBlimpDeckScene()
 		{
 			EditorSceneManager.OpenScene(GetScenePath("BlimpDeck"));
 		}
-		
+
 		[MenuItem("FLG/Scene/Open FloodCitySimple Scene")]
 		private static void OpenFloodCitySimpleScene()
 		{
 			EditorSceneManager.OpenScene(GetScenePath("FloodCitySimple"));
 		}
-		
+
 		[MenuItem("FLG/Scene/Open BRGenesis Scene")]
 		private static void OpenBRGenesisScene()
 		{
 			EditorSceneManager.OpenScene(GetScenePath("BRGenesis"));
 		}
-		
+
 		[MenuItem("FLG/Scene/Open Boot Scene &9")]
 		private static void OpenBootScene()
 		{
@@ -86,13 +90,13 @@ namespace FirstLight.Editor.EditorTools
 		{
 			SROptions.Current.RefillAmmoAndSpecials();
 		}
-		
+
 		[MenuItem("FLG/Cheats/Make Player Super Tough %l")]
 		private static void MakePlayerSuperTough()
 		{
 			SROptions.Current.MakeLocalPlayerSuperTough();
 		}
-		
+
 		[MenuItem("FLG/Cheats/Skip Tutorial Step %o")]
 		private static void SkipTutorialStep()
 		{
@@ -102,6 +106,87 @@ namespace FirstLight.Editor.EditorTools
 		private static string GetScenePath(string scene)
 		{
 			return AssetDatabase.GUIDToAssetPath(AssetDatabase.FindAssets($"t:scene {scene}")[0]);
+		}
+
+		[MenuItem("FLG/Art/Merge Colliders %#m")]
+		private static void MergeColliders()
+		{
+			var go = Selection.activeGameObject;
+			if (go == null)
+			{
+				Debug.LogError("Select a Game Object first!");
+				return;
+			}
+
+			var boxColliders = go.GetComponentsInChildren<BoxCollider>();
+			var sphereColliders = go.GetComponentsInChildren<SphereCollider>();
+			var capsuleColliders = go.GetComponentsInChildren<CapsuleCollider>();
+
+			var rootPos = go.transform.position;
+
+			Undo.RegisterCompleteObjectUndo(go.GetComponentsInChildren<Collider>().Cast<UnityEngine.Object>().Append(go).ToArray(), "Merge Colliders");
+
+			foreach (var bc in boxColliders)
+			{
+				var elementPos = bc.gameObject.transform.position;
+				var localToRoot = elementPos - rootPos;
+
+				var rootBc = Undo.AddComponent<BoxCollider>(go);
+				rootBc.center = localToRoot + bc.center;
+				rootBc.size = bc.size;
+				rootBc.isTrigger = bc.isTrigger;
+
+				if (bc.GetComponents(typeof(Component)).Length == 2 && go.transform.childCount == 0)
+				{
+					Undo.DestroyObjectImmediate(bc.gameObject);
+				}
+				else
+				{
+					Undo.DestroyObjectImmediate(bc);
+				}
+			}
+
+			foreach (var sc in sphereColliders)
+			{
+				var elementPos = sc.gameObject.transform.position;
+				var localToRoot = elementPos - rootPos;
+
+				var rootSc = Undo.AddComponent<SphereCollider>(go);
+				rootSc.center = localToRoot + sc.center;
+				rootSc.radius = sc.radius;
+				rootSc.isTrigger = sc.isTrigger;
+
+				if (sc.GetComponents(typeof(Component)).Length == 2 && go.transform.childCount == 0)
+				{
+					Undo.DestroyObjectImmediate(sc.gameObject);
+				}
+				else
+				{
+					Undo.DestroyObjectImmediate(sc);
+				}
+			}
+
+			foreach (var cc in capsuleColliders)
+			{
+				var elementPos = cc.gameObject.transform.position;
+				var localToRoot = elementPos - rootPos;
+
+				var rootCc = Undo.AddComponent<CapsuleCollider>(go);
+				rootCc.center = localToRoot + cc.center;
+				rootCc.radius = cc.radius;
+				rootCc.height = cc.height;
+				rootCc.direction = cc.direction;
+				rootCc.isTrigger = cc.isTrigger;
+
+				if (cc.GetComponents(typeof(Component)).Length == 2 && go.transform.childCount == 0)
+				{
+					Undo.DestroyObjectImmediate(cc.gameObject);
+				}
+				else
+				{
+					Undo.DestroyObjectImmediate(cc);
+				}
+			}
 		}
 	}
 }

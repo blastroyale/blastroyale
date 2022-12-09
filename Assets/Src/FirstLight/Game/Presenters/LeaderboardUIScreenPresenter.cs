0dd2a2212bbb67ba557ca71b8bb52fb4bbae95e8
Iker Goni <mail.igoni@gmail.com>
2022-12-09 19:27:08 +0700

    [FIX] Positioning independent of screen resolution/ratio

--- Assets/Src/FirstLight/Game/Presenters/LeaderboardUIScreenPresenter.cs
+++ Assets/Src/FirstLight/Game/Presenters/LeaderboardUIScreenPresenter.cs
@@ -65,10 +65,6 @@ namespace FirstLight.Game.Presenters
 		private void Awake()
 		{
 			_services = MainInstaller.Resolve<IGameServices>();
-				
-		//	_playerRankPool =
-		//		new GameObjectPool<LeaderboardUIEntryView>(GameConstants.Network.LEADERBOARD_TOP_RANK_AMOUNT + 
-		//		                                           GameConstants.Network.LEADERBOARD_NEIGHBOR_RANK_AMOUNT, _playerRankEntryRef);
 		}
 		protected override void OnOpened()
 		{
@@ -125,7 +121,7 @@ namespace FirstLight.Game.Presenters
 
 			lowestTopRankedPosition = result.Leaderboard[result.Leaderboard.Count - 1].Position;
 
-			for (int i = 0; i < 10; i++)
+			for (int i = 0; i < 11; i++)
 			{
 				var isLocalPlayer = result.Leaderboard[i].PlayFabId == PlayFabSettings.staticPlayer.PlayFabId;
 
@@ -197,141 +193,4 @@ namespace FirstLight.Game.Presenters
 		}
 		
 	}
-	
-	/*
-	public struct StateData
-		{
-			public Action BackClicked;
-		}
-
-		[SerializeField] private PlayerRankEntryView _playerRankEntryRef;
-		[SerializeField] private Transform _topRankSpawnTransform;
-		[SerializeField] private Transform _farRankSpawnTransform;
-		[SerializeField] private GameObject _farRankLeaderboardRoot;
-		[SerializeField] private TextMeshProUGUI _seasonEndText;
-		[SerializeField] private Button _backButton;
-
-		private IGameServices _services;
-		private IObjectPool<PlayerRankEntryView> _playerRankPool;
-
-		private int lowestTopRankedPosition = 0;
-
-		private void Awake()
-		{
-			_services = MainInstaller.Resolve<IGameServices>();
-			_backButton.onClick.AddListener(OnBackClicked);
-			
-			_playerRankPool =
-				new GameObjectPool<PlayerRankEntryView>(GameConstants.Network.LEADERBOARD_TOP_RANK_AMOUNT + 
-				                                        GameConstants.Network.LEADERBOARD_NEIGHBOR_RANK_AMOUNT, _playerRankEntryRef);
-		}
-
-		private void OnDestroy()
-		{
-			_backButton.onClick.RemoveAllListeners();
-		}
-		
-		protected override async Task OnClosed()
-		{
-			await base.OnClosed();
-
-			_playerRankPool.DespawnAll();
-			_farRankLeaderboardRoot.SetActive(false);
-		}
-
-		protected override void OnOpened()
-		{
-			base.OnOpened();
-
-			_farRankLeaderboardRoot.SetActive(false);
-
-			var nextResetDate = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month + 1, 1);
-			var timeDiff = nextResetDate - DateTime.UtcNow;
-
-			// TODO - ALL TEXT ON THIS PRESENTER NEEDS TO BE ADDED, HOOKED UP AND LOCALIZED. THIS IS PLACEHOLDER
-			_seasonEndText.text = "Season ends in " + timeDiff.ToString(@"d\d\ h\h\ mm\m");
-
-			// Leaderboard is requested and displayed in 2 parts
-			// First - top players, then, if needed - current player and neighbors, in a separate anchored section
-			_services.PlayfabService.GetTopRankLeaderboard(GameConstants.Network.LEADERBOARD_TOP_RANK_AMOUNT,
-			                                               OnLeaderboardTopRanksReceived, OnLeaderboardRequestError);
-		}
-
-		private void OnLeaderboardRequestError(PlayFabError error)
-		{
-			var confirmButton = new GenericDialogButton
-			{
-				ButtonText = ScriptLocalization.General.OK,
-				ButtonOnClick = _services.GenericDialogService.CloseDialog
-			};
-
-			if (error.ErrorDetails != null)
-			{
-				FLog.Error(JsonConvert.SerializeObject(error.ErrorDetails));
-			}
-
-			_services.GenericDialogService.OpenButtonDialog(ScriptLocalization.UITShared.error, error.ErrorMessage, false, confirmButton);
-			
-			Data.BackClicked();
-		}
-
-		private void OnLeaderboardTopRanksReceived(GetLeaderboardResult result)
-		{
-			bool localPlayerInTopRanks = false;
-
-			lowestTopRankedPosition = result.Leaderboard[result.Leaderboard.Count - 1].Position;
-
-			for (int i = result.Leaderboard.Count - 1; i >= 0; i--)
-			{
-				var isLocalPlayer = result.Leaderboard[i].PlayFabId == PlayFabSettings.staticPlayer.PlayFabId;
-				var newEntry = _playerRankPool.Spawn();
-				newEntry.transform.SetParent(_topRankSpawnTransform);
-				newEntry.gameObject.SetActive(true);
-				newEntry.SetInfo(result.Leaderboard[i].Position + 1, result.Leaderboard[i].DisplayName,
-				                 result.Leaderboard[i].StatValue, isLocalPlayer, null);
-
-				if (isLocalPlayer)
-				{
-					localPlayerInTopRanks = true;
-				}
-			}
-
-			if (localPlayerInTopRanks) return;
-
-			_services.PlayfabService.GetNeighborRankLeaderboard(GameConstants.Network.LEADERBOARD_NEIGHBOR_RANK_AMOUNT,
-			                                                    OnLeaderboardNeighborRanksReceived, OnLeaderboardRequestError);
-		}
-
-		private void OnLeaderboardNeighborRanksReceived(GetLeaderboardAroundPlayerResult result)
-		{
-			var localPlayer = result.Leaderboard.First(x => x.PlayFabId == PlayFabSettings.staticPlayer.PlayFabId);
-
-			// If the rank above player joins with the top ranks leaderboard, we just append player to the normal leaderboard
-			if (Math.Abs(lowestTopRankedPosition - localPlayer.Position) == 1)
-			{
-				var newEntry = _playerRankPool.Spawn();
-				newEntry.gameObject.SetActive(true);
-				newEntry.SetInfo(localPlayer.Position + 1, localPlayer.DisplayName, localPlayer.StatValue, true, null);
-				return;
-			}
-
-			_farRankLeaderboardRoot.SetActive(true);
-
-			for (int i = 0; i < result.Leaderboard.Count; i++)
-			{
-				var isLocalPlayer = result.Leaderboard[i].PlayFabId == PlayFabSettings.staticPlayer.PlayFabId;
-				var newEntry = _playerRankPool.Spawn();
-				newEntry.gameObject.SetActive(true);
-				newEntry.transform.SetParent(_farRankSpawnTransform);
-				newEntry.SetInfo(result.Leaderboard[i].Position + 1, result.Leaderboard[i].DisplayName,
-				                 result.Leaderboard[i].StatValue, isLocalPlayer, null);
-			}
-		}
-
-		private void OnBackClicked()
-		{
-			Data.BackClicked();
-		}
-	 * 
-	 */
 }

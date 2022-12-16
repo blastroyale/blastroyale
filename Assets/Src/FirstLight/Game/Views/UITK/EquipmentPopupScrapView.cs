3e97b8409919b5d97186c832ea159f790081f9fc
Miha Krajnc <miha@krajnc.dev>
2022-12-16 01:27:35 +0100

    Fix Scrap popup exception

--- Assets/Src/FirstLight/Game/Views/UITK/EquipmentPopupScrapView.cs
+++ Assets/Src/FirstLight/Game/Views/UITK/EquipmentPopupScrapView.cs
@@ -13,31 +13,29 @@ namespace FirstLight.Game.Views.UITK
 	public class EquipmentPopupScrapView : IUIView
 	{
 		private Label _amount;
-		private VisualElement _buttonsContainer;
 		private VisualElement _requirements;
 		private VisualElement _areYouSureLabel;
 		private VisualElement _bottomFiller;
 		
 		private LocalizedButton _scrapButton;
-		private LocalizedButton _cancelButton;
 
 		private Action _confirmAction;
 
 		public void Attached(VisualElement element)
 		{
 			_amount = element.Q<Label>("Amount").Required();
-			_buttonsContainer = element.Q<VisualElement>("ButtonContainer").Required();
 			_requirements = element.Q<VisualElement>("Requirements").Required();
 			_areYouSureLabel = element.Q<VisualElement>("AreYouSureLabel").Required();
 			_bottomFiller = element.Q<VisualElement>("BottomFiller").Required();
 			
-			element.Q<LocalizedButton>("ScrapButton").clicked += () => _confirmAction();
+			_scrapButton = element.Q<LocalizedButton>("ScrapButton").Required();
+			_scrapButton.clicked += () => _confirmAction();
 		}
 
 		public void SetData(EquipmentInfo info, Action confirmAction)
 		{
 			_amount.text = info.ScrappingValue.Value.ToString();
-			_buttonsContainer.SetVisibility(!info.IsNft);
+			_scrapButton.SetVisibility(!info.IsNft);
 
 			// TODO - Adjust desired behavior when calculations are correct client side and can be displayed
 			//_requirements.SetDisplay(true);

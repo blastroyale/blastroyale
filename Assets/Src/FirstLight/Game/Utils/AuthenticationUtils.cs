d5c2b7fa21e835b456e83ea7b08b5e258d0f4e9a
Iker Goni <mail.igoni@gmail.com>
2023-01-05 22:24:58 +0700

    Update AuthenticationUtils.cs
    Added summary on public methods

--- Assets/Src/FirstLight/Game/Utils/AuthenticationUtils.cs
+++ Assets/Src/FirstLight/Game/Utils/AuthenticationUtils.cs
@@ -10,16 +10,31 @@ namespace FirstLight.Game.Utils
 	public static class AuthenticationUtils
 	{
 		
+		/// <summary>
+		/// This method checks whether a given string, username, is a valid username field.
+		/// </summary>
+		/// <param name="username"></param>
+		/// <returns>true if the username is valid, false if it is not</returns>
 		public static bool IsUsernameFieldValid(string username)
 		{
 			return String.Compare(username, string.Empty, StringComparison.Ordinal) != 0;
 		}
 
+		/// <summary>
+		/// This method checks whether a given string, password, is a valid password field.
+		/// </summary>
+		/// <param name="password"></param>
+		/// <returns>true if the password is valid, false if it is not</returns>
 		public static bool IsPasswordFieldValid(string password)
 		{
 			return String.Compare(password, string.Empty, StringComparison.Ordinal) != 0;
 		}
 
+		/// <summary>
+		/// This method checks whether a given string is a valid email address.
+		/// </summary>
+		/// <param name="email">The email address to be checked.</param>
+		/// <returns>true if the email address is valid, false if it is not.</returns>
 		public static bool IsEmailFieldValid(string email)
 		{
 			var trimmedEmail = email.Trim();

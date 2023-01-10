4bb452787c9f833a8a5e48e90450eaeea3fdb3c3
Gabriel Slomka <gabrielslomka@gmail.com>
2023-01-10 14:20:58 +0000

    Merged in feature/level-0-fix-script (pull request #1579)
    Feature/level 0 fix script
    
    * Fix level 0 script
    
    * Fix
    
    * Improved
    
    
    Approved-by: Carlos

--- Backend/ServerScripts/Scripts/ConvertHammers.cs
+++ Backend/ServerScripts/Scripts/ConvertHammers.cs
@@ -6,85 +6,86 @@ using FirstLight.Game.Data;
 using PlayFab.ServerModels;
 using Quantum;
 
-namespace Scripts.Scripts;
-
-/// <summary>
-/// Script to replace Hammers in all players inventories to a random weapon
-/// Hammer in player inventory should be an invalid state of the data.  
-/// </summary>
-public class ConvertHammers : PlayfabScript
+namespace Scripts.Scripts
 {
-	readonly GameId[] _equip =
+	/// <summary>
+	/// Script to replace Hammers in all players inventories to a random weapon
+	/// Hammer in player inventory should be an invalid state of the data.  
+	/// </summary>
+	public class ConvertHammers : PlayfabScript
 	{
-		GameId.ModSniper,
-		GameId.ModRifle,
-		GameId.ModPistol,
-	};
-
-
-	private readonly Random _rnd = new();
-
-	public override PlayfabEnvironment GetEnvironment()
-	{
-		return PlayfabEnvironment.DEV;
-	}
-
-	public override void Execute(ScriptParameters args)
-	{
-		RunAsync().Wait();
-	}
-
-	public async Task RunAsync()
-	{
-		var tasks = new List<Task<int>>();
-		foreach (var player in await GetAllPlayers())
+		readonly GameId[] _equip =
 		{
-			tasks.Add(RemoveHammer(player));
+			GameId.ModSniper,
+			GameId.ModRifle,
+			GameId.ModPistol,
+		};
+
+
+		private readonly Random _rnd = new();
+
+		public override PlayfabEnvironment GetEnvironment()
+		{
+			return PlayfabEnvironment.DEV;
 		}
 
-		var result = await Task.WhenAll(tasks.ToArray());
-
-		Console.WriteLine($"Done removed {result.Sum()} hammers in total!");
-	}
-
-	private GameId GenerateNewEquipment()
-	{
-		return _equip[_rnd.Next(_equip.Length)];
-	}
-
-
-	private async Task<int> RemoveHammer(PlayerProfile profile)
-	{
-		var state = await ReadUserState(profile.PlayerId);
-		if (state == null)
+		public override void Execute(ScriptParameters args)
 		{
-			return 0;
+			RunAsync().Wait();
 		}
 
-		var removed = 0;
-		var equipmentData = state.DeserializeModel<EquipmentData>();
-		var idData = state.DeserializeModel<IdData>();
-		foreach (var (key, value) in equipmentData.Inventory)
+		public async Task RunAsync()
 		{
-			if (value.GameId == GameId.Hammer)
+			var tasks = new List<Task<int>>();
+			foreach (var player in await GetAllPlayers())
 			{
-				var copy = value;
-				var newEquipment = GenerateNewEquipment();
-				copy.GameId = newEquipment;
-				copy.Manufacturer = EquipmentManufacturer.Military;
-				equipmentData.Inventory[key] = copy;
-				idData.GameIds[key] = newEquipment;
-				idData.NewIds.Add(key);
-				removed++;
+				tasks.Add(RemoveHammer(player));
 			}
+
+			var result = await Task.WhenAll(tasks.ToArray());
+
+			Console.WriteLine($"Done removed {result.Sum()} hammers in total!");
 		}
 
-		if (removed <= 0) return removed;
+		private GameId GenerateNewEquipment()
+		{
+			return _equip[_rnd.Next(_equip.Length)];
+		}
 
-		state.UpdateModel(equipmentData);
-		state.UpdateModel(idData);
-		await SetUserState(profile.PlayerId, state);
-		Console.WriteLine($"Removed {removed} hammers from {profile.PlayerId}");
-		return removed;
+
+		private async Task<int> RemoveHammer(PlayerProfile profile)
+		{
+			var state = await ReadUserState(profile.PlayerId);
+			if (state == null)
+			{
+				return 0;
+			}
+
+			var removed = 0;
+			var equipmentData = state.DeserializeModel<EquipmentData>();
+			var idData = state.DeserializeModel<IdData>();
+			foreach (var (key, value) in equipmentData.Inventory)
+			{
+				if (value.GameId == GameId.Hammer)
+				{
+					var copy = value;
+					var newEquipment = GenerateNewEquipment();
+					copy.GameId = newEquipment;
+					copy.Manufacturer = EquipmentManufacturer.Military;
+					equipmentData.Inventory[key] = copy;
+					idData.GameIds[key] = newEquipment;
+					idData.NewIds.Add(key);
+					removed++;
+				}
+			}
+
+			if (removed <= 0) return removed;
+
+			state.UpdateModel(equipmentData);
+			state.UpdateModel(idData);
+			await SetUserState(profile.PlayerId, state);
+			Console.WriteLine($"Removed {removed} hammers from {profile.PlayerId}");
+			return removed;
+		}
 	}
 }

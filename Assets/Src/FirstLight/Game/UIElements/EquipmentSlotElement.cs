cc24200b93d1650d5b7ea08ab40f766d19a0599d
Miha Krajnc <miha@krajnc.dev>
2023-01-15 22:00:46 +0100

    Fix faction icons on equipment slots

--- Assets/Src/FirstLight/Game/UIElements/EquipmentSlotElement.cs
+++ Assets/Src/FirstLight/Game/UIElements/EquipmentSlotElement.cs
@@ -54,7 +54,7 @@ namespace FirstLight.Game.UIElements
 		private const string UssSpriteSlotRarity = "sprite-home__button-equipmentslot-{0}";
 		private const string UssSpriteEmptySlot = "sprite-home__icon-emptyslot-{0}";
 		private const string UssSpriteEquipmentCategory = "sprite-home__icon-equipmentcategory-{0}";
-		private const string UssSpriteFaction = "sprite-home__card-faction-{0}";
+		private const string UssSpriteFaction = "sprite-equipmentcard__card-faction-{0}";
 
 		public GameIdGroup Category { get; set; }
 

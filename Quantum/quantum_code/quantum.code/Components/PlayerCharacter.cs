841a0c5c42feed21c3ff5c3a64c36ac845218fdf
Fausto Fonseca <faustofonseca@gmail.com>
2023-01-17 15:18:30 +0000

    BRG-1517: Remake the fix for dash when collecting using signals instead

--- Quantum/quantum_code/quantum.code/Components/PlayerCharacter.cs
+++ Quantum/quantum_code/quantum.code/Components/PlayerCharacter.cs
@@ -15,16 +15,6 @@ namespace Quantum
 		/// </summary>
 		public WeaponSlot* WeaponSlot => WeaponSlots.GetPointer(CurrentWeaponSlot);
 
-		public void OnAdded(Frame f)
-		{
-			Collecting = f.AllocateHashSet<EntityRef>();
-		}
-
-		public void OnRemoved(Frame f)
-		{
-			f.FreeHashSet(Collecting);
-		}
-
 		/// <summary>
 		/// Spawns this <see cref="PlayerCharacter"/> with all the necessary data.
 		/// </summary>

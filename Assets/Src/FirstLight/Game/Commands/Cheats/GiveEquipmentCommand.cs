a3113a6573c932fdb836d6823035da430664ca5f
Carlos Feldmann <carlos@feldmann.dev>
2023-01-23 13:43:17 +0000

    feat: give equipment cheat command (pull request #1646)
    feat: give equipment cheat command
    
    * feat: give equipment cheat command
    
    * fix: equipping without flag and docs
    
    
    Approved-by: Matt Ryan
    Approved-by: Gabriel

---
+++ Assets/Src/FirstLight/Game/Commands/Cheats/GiveEquipmentCommand.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Linq;
+using FirstLight.Game.Data;
+using FirstLight.Game.Ids;
+using FirstLight.Game.Logic;
+using FirstLight.Server.SDK.Modules.Commands;
+using Quantum;
+
+namespace FirstLight.Game.Commands.Cheats
+{
+	/// <summary>
+	/// Give Equipment to the player, and equip it!
+	/// Requires admin permission on server.
+	/// </summary>
+	public class GiveEquipmentCommand : IGameCommand
+	{
+		public List<Equipment> EquipmentToGive;
+		public bool Equip;
+		public bool CheckDuplicates;
+
+		public CommandAccessLevel AccessLevel() => CommandAccessLevel.Admin;
+
+		public CommandExecutionMode ExecutionMode() => CommandExecutionMode.Server;
+
+		public void Execute(CommandExecutionContext ctx)
+		{
+			var equips = ctx.Data.GetData<EquipmentData>().Inventory;
+			List<Equipment> toAdd = new(EquipmentToGive);
+
+			if (CheckDuplicates)
+			{
+				// Check for duplicates
+				foreach (var equipment in toAdd.ToList())
+				{
+					var equal = equips.Where(keyValuePair => IsEqual(equipment, keyValuePair.Value)).ToList();
+					if (equal.Count >= 1)
+					{
+						toAdd.Remove(equipment);
+						if (Equip)
+						{
+							ctx.Logic.EquipmentLogic().Equip(equal.First().Key);
+						}
+					}
+				}
+			}
+
+			foreach (var equipment in toAdd)
+			{
+				var id = ctx.Logic.EquipmentLogic().AddToInventory(equipment);
+				if (Equip)
+				{
+					ctx.Logic.EquipmentLogic().Equip(id);
+				}
+			}
+		}
+
+		private bool IsEqual(Equipment eq, Equipment eq2)
+		{
+			return eq.Adjective == eq2.Adjective &&
+				   eq.GameId == eq2.GameId &&
+				   eq.Rarity == eq2.Rarity &&
+				   eq.Faction == eq2.Faction &&
+				   eq.Grade == eq2.Grade;
+		}
+	}
+}

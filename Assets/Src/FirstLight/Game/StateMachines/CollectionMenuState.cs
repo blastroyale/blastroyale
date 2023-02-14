b9ec8ada477178d49b950b4e77e8eb325a16e9a0
Anil Das-Gupta <anil.sdasgupta@gmail.com>
2023-02-14 15:13:22 +0000

    Updated sheets

--- Assets/Src/FirstLight/Game/StateMachines/CollectionMenuState.cs
+++ Assets/Src/FirstLight/Game/StateMachines/CollectionMenuState.cs
@@ -47,62 +47,25 @@ namespace FirstLight.Game.StateMachines
 		{
 			var initial = stateFactory.Initial("Initial");
 			var collectionMenuState = stateFactory.State("Collection Menu Screen State");
-			var collectionMenuSelectionState = stateFactory.State("Collection Menu Selection Screen State");
-			var scrapState = stateFactory.State("Collection Menu Scrap Popup State");
-			var upgradeState = stateFactory.State("Collection Menu Upgrade Popup State");
-			var repairState = stateFactory.State("Collection Menu Repair Popup State");
 			var final = stateFactory.Final("Final");
 
 			initial.Transition().Target(collectionMenuState);
 
 			collectionMenuState.OnEnter(OpenEquipmentScreen);
-			collectionMenuState.Event(_slotClickedEvent).Target(collectionMenuSelectionState);
 			collectionMenuState.Event(_backButtonClickedEvent).Target(final);
 			collectionMenuState.Event(_closeButtonClickedEvent).Target(final);
-
-			collectionMenuSelectionState.OnEnter(OpenEquipmentSelectionScreen);
-			collectionMenuSelectionState.Event(_backButtonClickedEvent).Target(collectionMenuState);
-			collectionMenuSelectionState.Event(_closeButtonClickedEvent).Target(final);
-
+			
 			final.OnEnter(SendLoadoutUpdateCommand);
 		}
-
-		private void OpenScrapPopup()
-		{
-			OpenPopup(EquipmentPopupPresenter.Mode.Scrap);
-		}
-
-		private void OpenUpgradePopup()
-		{
-			OpenPopup(EquipmentPopupPresenter.Mode.Upgrade);
-		}
-
-		private void OpenRepairPopup()
-		{
-			OpenPopup(EquipmentPopupPresenter.Mode.Repair);
-		}
-
-
+		
 		private void CloseEquipmentPopup()
 		{
 			_uiService.CloseUi<PlayerSkinScreenPresenter>();
 		}
 
-		private async void OpenPopup(EquipmentPopupPresenter.Mode mode)
-		{
-			var data = new EquipmentPopupPresenter.StateData
-			{
-				EquipmentIds = new[] {_uiService.GetUi<EquipmentSelectionPresenter>().SelectedItem},
-				PopupMode = mode,
-				// OnActionConfirmed = OnPopupActionConfirmed,
-				OnCloseClicked = () => _statechartTrigger(_closeButtonClickedEvent)
-			};
-
-			await _uiService.OpenUiAsync<EquipmentPopupPresenter, EquipmentPopupPresenter.StateData>(data);
-		}
-
 		private void OpenEquipmentScreen()
 		{
+			/*
 			var data = new PlayerSkinScreenPresenter.StateData
 			{
 				OnSkinSelected = ItemClicked,
@@ -110,6 +73,17 @@ namespace FirstLight.Game.StateMachines
 			};
 
 			_uiService.OpenScreen<PlayerSkinScreenPresenter, PlayerSkinScreenPresenter.StateData>(data);
+			*/
+			
+			var data = new CollectionScreenPresenter.StateData
+			{
+				// OnSkinSelected = ItemClicked,
+				OnBackClicked = () => _statechartTrigger(_closeButtonClickedEvent),
+			};
+
+			_uiService.OpenScreen<CollectionScreenPresenter, CollectionScreenPresenter.StateData>(data);
+			
+			
 			_services.MessageBrokerService.Publish(new SkinsScreenOpenedMessage());
 		}
 
@@ -131,18 +105,6 @@ namespace FirstLight.Game.StateMachines
 			_statechartTrigger(_slotClickedEvent);
 			
 			_services.MessageBrokerService.Publish(new EquipmentSlotOpenedMessage() {Slot = slot} );
-		}
-
-		private void OpenEquipmentSelectionScreen()
-		{
-			var data = new EquipmentSelectionPresenter.StateData
-			{
-				EquipmentSlot = _equipmentSlotTypePicked,
-				OnBackClicked = () => _statechartTrigger(_backButtonClickedEvent),
-				OnCloseClicked = () => _statechartTrigger(_closeButtonClickedEvent),
-			};
-
-			_uiService.OpenScreen<EquipmentSelectionPresenter, EquipmentSelectionPresenter.StateData>(data);
 		}
 	}
 }

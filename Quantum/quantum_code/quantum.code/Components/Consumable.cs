05b4faf3cc466cb7671d633dbf10a15f0e1aff81
Nikita Denissov <nik.denissov@gmail.com>
2023-02-14 11:36:32 +0000

    Health, ammo and shields replenish for teammates as well

--- Quantum/quantum_code/quantum.code/Components/Consumable.cs
+++ Quantum/quantum_code/quantum.code/Components/Consumable.cs
@@ -30,20 +30,58 @@ namespace Quantum
 		internal void Collect(Frame f, EntityRef entity, EntityRef playerEntity, PlayerRef player)
 		{
 			var stats = f.Unsafe.GetPointer<Stats>(playerEntity);
+			var isTeamsMode = f.Context.GameModeConfig.Teams;
+			var team = f.Get<Targetable>(playerEntity).Team;
 
 			switch (ConsumableType)
 			{
 				case ConsumableType.Health:
 					stats->GainHealth(f, playerEntity, new Spell { PowerAmount = (uint) Amount.AsInt});
+					
+					if (isTeamsMode)
+					{
+						foreach (var teammateCandidate in f.Unsafe.GetComponentBlockIterator<Targetable>())
+						{
+							if (!QuantumHelpers.IsDestroyed(f, teammateCandidate.Entity) && teammateCandidate.Component->Team == team)
+							{
+								stats->GainHealth(f, teammateCandidate.Entity, new Spell { PowerAmount = (uint) Amount.AsInt});
+							}
+						}
+					}
+					
 					break;
 				case ConsumableType.Rage:
 					StatusModifiers.AddStatusModifierToEntity(f, playerEntity, StatusModifierType.Rage, Amount.AsInt);
 					break;
 				case ConsumableType.Ammo:
 					f.Unsafe.GetPointer<Stats>(playerEntity)->GainAmmoPercent(f, playerEntity, Amount);
+					
+					if (isTeamsMode)
+					{
+						foreach (var teammateCandidate in f.Unsafe.GetComponentBlockIterator<Targetable>())
+						{
+							if (!QuantumHelpers.IsDestroyed(f, teammateCandidate.Entity) && teammateCandidate.Component->Team == team)
+							{
+								f.Unsafe.GetPointer<Stats>(teammateCandidate.Entity)->GainAmmoPercent(f, teammateCandidate.Entity, Amount);
+							}
+						}
+					}
+					
 					break;
 				case ConsumableType.Shield:
 					stats->GainShield(f, playerEntity, Amount.AsInt);
+					
+					if (isTeamsMode)
+					{
+						foreach (var teammateCandidate in f.Unsafe.GetComponentBlockIterator<Targetable>())
+						{
+							if (!QuantumHelpers.IsDestroyed(f, teammateCandidate.Entity) && teammateCandidate.Component->Team == team)
+							{
+								stats->GainShield(f, teammateCandidate.Entity, Amount.AsInt);
+							}
+						}
+					}
+					
 					break;
 				case ConsumableType.ShieldCapacity:
 					stats->GainShieldCapacity(f, playerEntity, Amount.AsInt);

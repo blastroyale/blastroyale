ac6a7116ab4c5a6b5c76e82768f28d0648658c4a
Anil Das-Gupta <anil.sdasgupta@gmail.com>
2023-02-15 12:05:35 +0000

    Updated CSS cards

--- Assets/Src/FirstLight/Game/UIElements/CollectionCardElement.cs
+++ Assets/Src/FirstLight/Game/UIElements/CollectionCardElement.cs
@@ -24,19 +24,10 @@ namespace FirstLight.Game.UIElements
 		private const string UssSelected = UssBlock + "__selected-bg";
 		private const string UssHighlight = UssBlock + "__highlight";
 		private const string UssBackground = UssBlock + "__background";
-		private const string UssRarity = UssBlock + "__rarity";
 		private const string UssCardHolder = UssBlock + "__card-holder";
-		private const string UssPlusRarity = UssBlock + "__plus-rarity";
 		private const string UssImage = UssBlock + "__image";
 		private const string UssImageShadow = UssImage + "--shadow";
-		private const string UssGrade = UssBlock + "__grade";
-		private const string UssFaction = UssBlock + "__faction";
-		private const string UssMaterial = UssBlock + "__material";
-		private const string UssLevel = UssBlock + "__level";
 		private const string UssName = UssBlock + "__name";
-		private const string UssAdjCatHolder = UssBlock + "__adj-cat-holder";
-		private const string UssCategory = UssBlock + "__category";
-		private const string UssAdjective = UssBlock + "__adjective";
 		private const string UssBadgeHolder = UssBlock + "__badge-holder";
 		private const string UssBadgeNft = UssBlock + "__badge-nft";
 		private const string UssBadgeLoaned = UssBlock + "__badge-loaned";
@@ -44,11 +35,6 @@ namespace FirstLight.Game.UIElements
 
 		private const string UssNotification = UssBlock + "__notification";
 		private const string UssNotificationIcon = "notification-icon";
-		
-		private const string UssSpriteRarity = "sprite-equipmentcard__card-rarity-{0}";
-		private const string UssSpriteFaction = "sprite-equipmentcard__card-faction-{0}";
-		private const string UssSpriteMaterial = "sprite-equipmentcard__card-material-{0}";
-		private const string UssSpriteCategory = "sprite-equipmentcard__card-category-{0}";
 
 		public Equipment Equipment { get; private set; }
 		public UniqueId UniqueId { get; private set; }
@@ -155,10 +141,6 @@ namespace FirstLight.Game.UIElements
 
 			_name.text = equipment.GameId.GetLocalization();
 
-			_category.RemoveSpriteClasses();
-			_category.AddToClassList(string.Format(UssSpriteCategory,
-				equipment.GetEquipmentGroup().ToString().ToLowerInvariant()));
-			
 			Equipment = equipment;
 
 			if (id == UniqueId) return;

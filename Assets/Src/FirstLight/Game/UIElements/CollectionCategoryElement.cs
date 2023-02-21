666ff55451aa15ec644ceb891fd031a0ae7b8f47
Anil Das-Gupta <anil.sdasgupta@gmail.com>
2023-02-21 18:27:31 +0000

    New Category Styles

---
+++ Assets/Src/FirstLight/Game/UIElements/CollectionCategoryElement.cs
@@ -0,0 +1,123 @@
+using System;
+using FirstLight.Game.Infos;
+using FirstLight.Game.Services;
+using FirstLight.Game.Utils;
+using I2.Loc;
+using Quantum;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UIElements.Button;
+
+namespace FirstLight.Game.UIElements
+{
+	/// <summary>
+	/// Displays a category of items in the collection screen, e.g. Characters, Banners, Gliders, etc.
+	/// </summary>
+	public class CollectionCategoryElement : ImageButton
+	{
+		private const string EMPTY_LOC_KEY = "UITCollection/no_{0}";
+
+		private const string ADJECTIVE_LOC_KEY = "UITCollection/adjective_{0}";
+
+		private const string UssBlock = "collection-category";
+		private const string UssBlockSelected = UssBlock + "--selected";
+		private const string UssBlockHighlighted = UssBlock + "--highlighted";
+
+		private const string UssSelected = UssBlock + "__selected-bg";
+		private const string UssHighlight = UssBlock + "__highlight";
+		private const string UssBackground = UssBlock + "__background";
+		private const string UssCardHolder = UssBlock + "__card-holder";
+
+		private const string UssIcon = UssBlock + "__icon";
+		private const string UssImage = UssBlock + "__image";
+		private const string UssImageShadow = UssImage + "--shadow";
+
+		private const string UssName = UssBlock + "__name";
+
+		private const string UssBadgeHolder = UssBlock + "__badge-holder";
+		private const string UssNotification = UssBlock + "__notification";
+		private const string UssNotificationIcon = "notification-icon";
+		
+		
+		public GameIdGroup Category { get; set; }
+		public GameId MenuGameId { get; private set; }
+
+		private readonly VisualElement _icon;
+		private readonly VisualElement _image;
+		private readonly VisualElement _imageShadow;
+		private readonly Label _name;
+
+		private readonly VisualElement _locked;
+		private readonly VisualElement _nftBadge;
+		private readonly VisualElement _loanedBadge;
+		private readonly VisualElement _equippedBadge;
+		private readonly VisualElement _notification;
+		
+		/// <summary>
+		/// Triggered when the card is clicked
+		/// </summary>
+		public new event Action<GameId> clicked;
+
+		public CollectionCategoryElement()
+		{
+			AddToClassList(UssBlock);
+
+			var selectedBg = new VisualElement {name = "selected-bg"};
+			Add(selectedBg);
+			selectedBg.AddToClassList(UssSelected);
+
+			var highlight = new VisualElement {name = "highlight"};
+			Add(highlight);
+			highlight.AddToClassList(UssHighlight);
+
+			var background = new VisualElement {name = "background"};
+			Add(background);
+			background.AddToClassList(UssBackground);
+
+			var cardHolder = new VisualElement {name = "holder"};
+			Add(cardHolder);
+			cardHolder.AddToClassList(UssCardHolder);
+
+			cardHolder.Add(_icon = new VisualElement {name = "icon"});
+			_icon.AddToClassList(UssIcon);
+			
+			cardHolder.Add(_imageShadow = new VisualElement {name = "equipment-image-shadow"});
+			_imageShadow.AddToClassList(UssImage);
+			_imageShadow.AddToClassList(UssImageShadow);
+
+			cardHolder.Add(_image = new VisualElement {name = "item-image"});
+			_image.AddToClassList(UssImage);
+			
+			cardHolder.Add(_name = new Label("CATEGORY") {name = "name"});
+			_name.AddToClassList(UssName);
+
+	
+			cardHolder.Add(_notification = new VisualElement());
+			_notification.AddToClassList(UssNotification);
+			_notification.AddToClassList(UssNotificationIcon);
+
+			base.clicked += () => clicked?.Invoke(MenuGameId);
+
+			// if (equipment.IsValid())
+			{
+				// SetCollectionElement(GameId.Male01Avatar);
+			}
+		}
+		
+		public void SetSelected(bool selected)
+		{
+			if (selected)
+			{
+				AddToClassList(UssBlockSelected);
+			}
+			else
+			{
+				RemoveFromClassList(UssBlockSelected);
+			}
+		}
+
+		public new class UxmlFactory : UxmlFactory<CollectionCategoryElement, UxmlTraits>
+		{
+		}
+	}
+}

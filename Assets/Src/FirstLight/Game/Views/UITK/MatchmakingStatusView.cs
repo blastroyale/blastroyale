67ae4020fb5e8087a7480598fd40d464a1c84242
Miha Krajnc <miha@krajnc.dev>
2023-03-01 12:58:03 +0100

    Fix Matchmaking timer

--- Assets/Src/FirstLight/Game/Views/UITK/MatchmakingStatusView.cs
+++ Assets/Src/FirstLight/Game/Views/UITK/MatchmakingStatusView.cs
@@ -1,9 +1,10 @@
 using System;
+using FirstLight.FLogger;
 using FirstLight.Game.UIElements;
 using FirstLight.Game.Utils;
 using FirstLight.UiService;
+using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEngine.UIElements.Experimental;
 
 namespace FirstLight.Game.Views.UITK
 {
@@ -12,10 +13,12 @@ namespace FirstLight.Game.Views.UITK
 	/// </summary>
 	public class MatchmakingStatusView : IUIView
 	{
+		private const string UssContainerHidden = "matchmaking-container--hidden";
+
 		private VisualElement _container;
 		private Label _timeLabel;
 
-		private ValueAnimation<float> _timer;
+		private long _startTime;
 
 		public event Action CloseClicked;
 
@@ -36,16 +39,16 @@ namespace FirstLight.Game.Views.UITK
 
 		public void Show(bool show)
 		{
-			_container.EnableInClassList("matchmaking-container--hidden", !show);
-
-			_timer?.Stop();
-			_timer?.Recycle();
+			_container.EnableInClassList(UssContainerHidden, !show);
 
 			if (show)
 			{
+				_startTime = 0;
 				_timeLabel.schedule.Execute(ts =>
 				{
-					_timeLabel.text = $"{TimeSpan.FromMilliseconds(ts.now - ts.start):mm\\:ss}";
+					if (_startTime == 0L) _startTime = ts.now;
+
+					_timeLabel.text = $"{TimeSpan.FromMilliseconds(_startTime - ts.start):mm\\:ss}";
 				}).Every(200); // Triggered every 200ms so the second counting is smooth and consistent.
 			}
 		}

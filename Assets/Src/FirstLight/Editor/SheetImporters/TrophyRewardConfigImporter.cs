977f0e6b7dc671fc542da471d4240f8b92d97715
Tiryon Williams <tiryon.w@gmail.com>
2023-05-03 14:53:16 +0100

    rewards data is now stored in an arary

--- Assets/Src/FirstLight/Editor/SheetImporters/TrophyRewardConfigImporter.cs
+++ Assets/Src/FirstLight/Editor/SheetImporters/TrophyRewardConfigImporter.cs
@@ -1,16 +1,26 @@
 using System;
 using System.Collections.Generic;
 using FirstLight.Game.Configs;
-using FirstLightEditor.GoogleSheetImporter;
-using FirstLightServerSDK.Modules;
 
 namespace FirstLight.Editor.SheetImporters
 {
 	/// <inheritdoc />
-	public class TrophyRewardConfigImporter : GoogleSheetQuantumConfigsImporter<TrophyRewardConfig, TrophyRwardConfigs>
+	public class TrophyRewardConfigImporter : GoogleSheetQuantumConfigsImporter<TrophyRewardConfig, TrophyRewardConfigs>
 	{
 		/// <inheritdoc />
 		public override string GoogleSheetUrl => "***REMOVED***/edit#gid=1565053521";
 
+		protected override TrophyRewardConfig Deserialize(Dictionary<string, string> data)
+		{
+			var config = base.Deserialize(data);
+
+			var rewards = new int[data.Count - 1];
+			for(int i = 1; i < data.Count; i++)
+			{
+				rewards[i-1] = int.Parse(data[i.ToString()]);
+			}
+			config.Rewards = rewards;
+			return config;
+		}
 	}
 }
--- Assets/Src/FirstLight/Game/Configs/TrophyRewardConfigs.cs
+++ Assets/Src/FirstLight/Game/Configs/TrophyRewardConfigs.cs
@@ -1,32 +1,30 @@
 using System;
 using System.Collections.Generic;
-using FirstLight.Server.SDK.Modules.GameConfiguration;
 using UnityEngine;
-using I2.Loc;
-using Quantum;
 
 namespace FirstLight.Game.Configs
 {
 	[Serializable]
 	public struct TrophyRewardConfig
 	{
-		public int[] reward;
-		public int trophyRange;
+		public int[] Rewards;
+		public int TrophyRange;
 	}
 
 	/// <summary>
 	/// Scriptable Object tool to import the <seealso cref="TrophyRewardConfig"/> sheet data
 	/// </summary>
 	[CreateAssetMenu(fileName = "TrophyRewardConfigs", menuName = "ScriptableObjects/Configs/TrophyRewardConfigs")]
-	public class TrophyRwardConfigs : ScriptableObject, IConfigsContainer<TrophyRewardConfig>
+	public class TrophyRewardConfigs : ScriptableObject, IConfigsContainer<TrophyRewardConfig>
 	{
 		[SerializeField] private List<TrophyRewardConfig> _configs = new List<TrophyRewardConfig>();
 
+		// ReSharper disable once ConvertToAutoProperty
+		/// <inheritdoc />
 		public List<TrophyRewardConfig> Configs
 		{
 			get => _configs;
 			set => _configs = value;
 		}
-
 	}
 }

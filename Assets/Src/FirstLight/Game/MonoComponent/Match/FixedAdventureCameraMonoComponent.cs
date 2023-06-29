24cfdff9bfb9f9bd147823a637291c8ecd06b362
Gabriel Slomka <gabrielslomka@gmail.com>
2023-06-29 06:36:18 +0000

    Merged in test/blr-camera (pull request #2393)
    Test BLR camera
    
    * Test BLR camera
    
    * Lowered camera a bit
    
    
    Approved-by: Matt Ryan
    Approved-by: Carlos
    Approved-by: Nik Denissov

--- Assets/Src/FirstLight/Game/MonoComponent/Match/FixedAdventureCameraMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/Match/FixedAdventureCameraMonoComponent.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Linq;
 using Cinemachine;
+using FirstLight.FLogger;
 using FirstLight.Game.Logic;
 using FirstLight.Game.Messages;
+using FirstLight.Game.MonoComponent.EntityPrototypes;
 using FirstLight.Game.Services;
 using FirstLight.Game.Utils;
 using Photon.Deterministic;
@@ -31,7 +33,7 @@ namespace FirstLight.Game.MonoComponent.Match
 		private IGameServices _services;
 		private IMatchServices _matchServices;
 		private IGameDataProvider _gameDataProvider;
-
+		
 		private void Awake()
 		{
 			_services = MainInstaller.Resolve<IGameServices>();
@@ -39,14 +41,6 @@ namespace FirstLight.Game.MonoComponent.Match
 			_gameDataProvider = MainInstaller.Resolve<IGameDataProvider>();
 			
 			_matchServices.MatchCameraService.SetCameras(_adventureCamera);
-
-			var input = _matchServices.PlayerInputService.Input.Gameplay;
-
-			input.SpecialButton0.started += SetActiveCamera;
-			input.SpecialButton0.canceled += SetActiveCamera;
-			input.SpecialButton1.started += SetActiveCamera;
-			input.SpecialButton1.canceled += SetActiveCamera;
-			input.CancelButton.canceled += SetActiveCamera;
 
 			_matchServices.SpectateService.SpectatedPlayer.InvokeObserve(OnSpectatedPlayerChanged);
 			_services.MessageBrokerService.Subscribe<SpectateSetCameraMessage>(OnSpectateSetCameraMessage);
@@ -56,57 +50,24 @@ namespace FirstLight.Game.MonoComponent.Match
 			QuantumEvent.Subscribe<EventOnPlayerAlive>(this, OnPlayerAlive);
 			QuantumEvent.Subscribe<EventOnPlayerSkydiveLand>(this, OnPlayerSkydiveLand);
 			gameObject.SetActive(false);
-			QuantumCallback.Subscribe<CallbackUpdateView>(this, OnUpdateView);
-
-			
-		}
-
-		/// <summary>
-		/// This is for camera panning and snapping with aiming
-		/// </summary>
-		private unsafe void OnUpdateView(CallbackUpdateView callback)
-		{
-			var spectatedEntity = _matchServices.SpectateService.SpectatedPlayer.Value.Entity;				
-			var f = callback.Game.Frames.Predicted;
-
-			if (!f.Unsafe.TryGetPointer<PlayerCharacter>(spectatedEntity, out var player)) return;
-
-			var playerInput = f.GetPlayerInput(player->Player);
-			var inputDir = _gameDataProvider.AppDataProvider.UseDynamicCamera ? playerInput->AimingDirection : FPVector2.Zero;
-
-			var config = _services.ConfigsProvider.GetConfig<QuantumWeaponConfig>((int)player->CurrentWeapon.GameId);
-			var rangeModifer = config.AttackRange.AsFloat / 10;
-			//rounds the range modifier to the nearst 0.5 to keep things as consistent as possible
-			var roundedValue = MathF.Round(rangeModifer * 2) / 2 * GameConstants.Camera.DYNAMIC_CAMERA_PAN_DISTANCE_DEFAULT;
-
-			var scalar = inputDir != FPVector2.Zero ?
-				GameConstants.Camera.DYNAMIC_CAMERA_PAN_TO_AIM_TIME : GameConstants.Camera.DYNAMIC_CAMERA_PAN_TO_CENTER_TIME;
-			var dir = new Vector3(inputDir.X.AsFloat, 0, inputDir.Y.AsFloat);
-			dir = Vector3.Scale(dir, new Vector3(1, 0, dir.z < 0 ? GameConstants.Camera.DYNAMIC_CAMERA_PAN_NEGATIVE_Y_DIR_MULTIPLIER : 1)) * roundedValue;
-
-			if(_matchServices.EntityViewUpdaterService.TryGetView(spectatedEntity, out var view))
-			{
-				var playerPos = view.gameObject.transform.position;
-				_followObject.transform.position = Vector3.Lerp(_followObject.transform.position, playerPos + dir, Time.deltaTime / scalar);
-			}
 		}
 
 		private void OnDestroy()
 		{
-			var input = _matchServices?.PlayerInputService?.Input?.Gameplay;
-
-			if (input.HasValue)
-			{
-				input.Value.SpecialButton0.started -= SetActiveCamera;
-				input.Value.SpecialButton0.canceled -= SetActiveCamera;
-				input.Value.SpecialButton1.started -= SetActiveCamera;
-				input.Value.SpecialButton1.canceled -= SetActiveCamera;
-				input.Value.CancelButton.canceled -= SetActiveCamera;
-			}
-
 			_matchServices?.SpectateService?.SpectatedPlayer?.StopObserving(OnSpectatedPlayerChanged);
 			_services?.MessageBrokerService?.UnsubscribeAll(this);
 			QuantumCallback.UnsubscribeListener(this);
+		}
+		
+		private static GameObject GetFollowObject(SpectatedPlayer player)
+		{
+			var component = player.Transform.gameObject.GetComponent<PlayerCharacterMonoComponent>();
+			if (component == null)
+			{
+				FLog.Warn("Camera following something that is not a player :L");
+				return player.Transform.gameObject;
+			}
+			return component.Instance.transform.gameObject;
 		}
 
 		private void OnSpectatedPlayerChanged(SpectatedPlayer previous, SpectatedPlayer next)
@@ -119,14 +80,15 @@ namespace FirstLight.Game.MonoComponent.Match
 				SetActiveCamera(_adventureCamera);
 			}
 
+			_followObject = GetFollowObject(next);
+			RefreshSpectator(_followObject.transform);
+			
 			//when becoming a spectator, disable camera panning and set the follow target to the spectated player's transform
-			if (_services.NetworkService.LocalPlayer.IsSpectator())
+			if (!_services.NetworkService.LocalPlayer.IsSpectator())
 			{
-				QuantumCallback.UnsubscribeListener(this);
-				_followObject = next.Transform.gameObject;
+				return;
 			}
-
-			RefreshSpectator(next.Transform);
+			QuantumCallback.UnsubscribeListener(this);
 			_cinemachineBrain.ActiveVirtualCamera?.SnapCamera();
 		}
 
@@ -164,7 +126,7 @@ namespace FirstLight.Game.MonoComponent.Match
 			
 			SetActiveCamera(_winnerCamera);
 		}
-		
+
 		private void OnPlayerSpawned(EventOnPlayerSpawned callback)
 		{
 			if (callback.Game.PlayerIsLocal(callback.Player))

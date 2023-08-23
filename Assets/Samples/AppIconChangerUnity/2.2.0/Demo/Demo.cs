4c5a5aad54da73d5c5869d2de8d6ee4ce945d830
Matt Ryan <vertexbuffer@protonmail.com>
2023-08-23 14:29:31 +0100

    added app dynamic icons package

---
+++ Assets/Samples/AppIconChangerUnity/2.2.0/Demo/Demo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AppIconChanger.Demo
+{
+    public class Demo : MonoBehaviour
+    {
+        [SerializeField] private Text statusText = default;
+
+        private string supportsAlternateIcons = "(Empty)";
+        private string alternateIconName = "(Empty)";
+
+        public void GetSupportsAlternateIcons()
+        {
+            supportsAlternateIcons = AppIconChanger.iOS.SupportsAlternateIcons.ToString();
+        }
+
+        public void GetAlternateIconName()
+        {
+            alternateIconName = AppIconChanger.iOS.AlternateIconName ?? "(null)";
+        }
+
+        public void ChangeIcon(string iconName)
+        {
+            AppIconChanger.iOS.SetAlternateIconName(iconName);
+        }
+
+        public void ChangeIconToDefault()
+        {
+            AppIconChanger.iOS.SetAlternateIconName(null);
+        }
+
+        public void Update()
+        {
+            statusText.text = $"supportsAlternateIcons = {supportsAlternateIcons}\nalternateIconName = {alternateIconName}";
+        }
+    }
+}

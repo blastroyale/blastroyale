06f7323a9381edd07f708b02d0157c0b1b3e5e68
Matt Ryan <vertexbuffer@protonmail.com>
2023-09-19 11:28:24 +0100

    added player stats presenter

---
+++ Assets/Src/FirstLight/Game/Presenters/PlayerStatisticsPopupPresenter.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+using FirstLight.Game.Ids;
+using FirstLight.Game.Logic;
+using FirstLight.Game.Services;
+using FirstLight.Game.Utils;
+using FirstLight.Game.UIElements;
+using FirstLight.Game.Views.UITK;
+using FirstLight.UiService;
+using I2.Loc;
+using Quantum;
+using UnityEngine.UIElements;
+
+namespace FirstLight.Game.Presenters
+{
+	/// <summary>
+	/// Handles the player statistics screen.
+	/// </summary>
+	[LoadSynchronously]
+	public class PlayerStatisticsPopupPresenter : UiToolkitPresenterData<PlayerStatisticsPopupPresenter.StateData>
+	{
+		public struct StateData
+		{
+			public string PlayerId;
+			public Action OnCloseClicked;
+		}
+		
+		private IGameServices _services;
+		private IGameDataProvider _gameDataProvider;
+		
+
+		private void Awake()
+		{
+			_services = MainInstaller.Resolve<IGameServices>();
+			_gameDataProvider = MainInstaller.Resolve<IGameDataProvider>();
+		}
+
+		protected override void QueryElements(VisualElement root)
+		{
+			root.Q<ImageButton>("CloseButton").clicked += Data.OnCloseClicked;
+			root.Q<VisualElement>("Background")
+				.RegisterCallback<ClickEvent, StateData>((_, data) => data.OnCloseClicked(), Data);
+
+			root.SetupClicks(_services);
+		}
+
+		protected override void OnOpened()
+		{
+			base.OnOpened();
+
+			SetupPopup();
+		}
+
+		private void SetupPopup()
+		{
+		}
+	}
+}

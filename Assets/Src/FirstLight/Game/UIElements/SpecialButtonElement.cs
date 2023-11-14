42d9a809395480e30dee888b82c8ecabd1d556ef
Miha Krajnc <miha@krajnc.dev>
2023-11-14 15:35:19 +0100

    Add special button ping animation when picking up a special

--- Assets/Src/FirstLight/Game/UIElements/SpecialButtonElement.cs
+++ Assets/Src/FirstLight/Game/UIElements/SpecialButtonElement.cs
@@ -124,6 +124,7 @@ namespace FirstLight.Game.UIElements
 			_icon.RemoveSpriteClasses();
 			_icon.AddToClassList(string.Format(USS_SPRITE_SPECIAL,
 				special.ToString().ToLowerInvariant().Replace("special", "")));
+			this.AnimatePing();
 
 			_disableScheduledItem?.Pause();
 			if (availableIn > 0)

e7763dd835d9e079c43289321f6d5a27b0259416
Gabriel Slomka <gabrielslomka@gmail.com>
2024-01-18 10:29:41 +0000

    Merged in bugfix/footsteps (pull request #3159)
    Footsteps
    
    * Footsteps
    
    
    Approved-by: Miha Krajnc

--- Assets/Src/FirstLight/Game/MonoComponent/Vfx/FootprinterMonoComponent.cs
+++ Assets/Src/FirstLight/Game/MonoComponent/Vfx/FootprinterMonoComponent.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using FirstLight.Game.MonoComponent.EntityPrototypes;
 using FirstLight.Game.Commands;
 using FirstLight.Game.Data.DataTypes;
@@ -45,13 +46,12 @@ public class FootprinterMonoComponent : MonoBehaviour
     {
         _services = MainInstaller.Resolve<IGameServices>();
         _matchServices = MainInstaller.Resolve<IMatchServices>();
-        SceneManager.activeSceneChanged += OnSceneChanged;
+        QuantumCallback.Subscribe<CallbackGameDestroyed>(this, OnGameDestroyed);
     }
 
-    private void OnSceneChanged(Scene newScene, Scene oldScene)
+    private void OnGameDestroyed(CallbackGameDestroyed e)
     {
         _globalPool.Clear();
-        SceneManager.activeSceneChanged -= OnSceneChanged;
     }
 
     public void Init(EntityView view, PlayerLoadout loadout)
@@ -64,7 +64,10 @@ public class FootprinterMonoComponent : MonoBehaviour
 
     private void Update()
     {
-        if (_character != null && _view != null && SpawnFootprints && _skin.Id != GameId.Random && _cooldown.CheckTrigger()) Spawn();
+        if (_character != null && _view != null && SpawnFootprints && _skin.Id != GameId.Random && _cooldown.CheckTrigger())
+        {
+            Spawn().Forget();
+        }
     }
 
     private Quaternion GetFootRotation()
@@ -76,6 +79,7 @@ public class FootprinterMonoComponent : MonoBehaviour
         
         if (f.TryGet<AIBlackboardComponent>(_view.EntityRef, out var bb) && bb.HasEntry(f, Constants.MoveDirectionKey))
         {
+            // TODO: Use lookup table for performance
             return Quaternion.LookRotation(bb.GetVector2(f, Constants.MoveDirectionKey).ToUnityVector3());
         }
         return _view.transform.rotation; 
@@ -84,13 +88,13 @@ public class FootprinterMonoComponent : MonoBehaviour
     /// <summary>
     /// Spawns the footstep.
     /// </summary>
-    private async void Spawn()
+    private async UniTaskVoid Spawn()
     {
         if (_character.PlayerView.Culled) return;
+        if (!QuantumRunner.Default.IsDefinedAndRunning()) return;
         
         if (_globalPool.Count > 0) _pooledFootprint = _globalPool.Dequeue();
         else  _pooledFootprint = await _services.CollectionService.LoadCollectionItem3DModel(_skin);
-        if (!QuantumRunner.Default.IsDefinedAndRunning()) return;
         if (_rightStepScale == Vector3.zero)
         {
             _rightStepScale = _pooledFootprint.transform.localScale;
@@ -109,12 +113,10 @@ public class FootprinterMonoComponent : MonoBehaviour
 
     private void PlayEffects()
     {
-        if (_matchServices.EntityVisibilityService.CanSpectatedPlayerSee(_character.PlayerView.EntityRef))
-        {
-            _services.AudioFxService.PlayClip3D(AudioId.PlayerWalkRoad, _character.transform.position);
-        }
+        var clip = _services.AudioFxService.PlayClip3D(AudioId.PlayerWalkRoad, _character.transform.position);
         if (_matchServices.SpectateService.GetSpectatedEntity() == _character.EntityView.EntityRef)
         {
+            clip.Source.volume /= 3;
             _services.VfxService.Spawn(VfxId.StepSmoke).transform.position = _pooledFootprint.transform.position;
         }
     }

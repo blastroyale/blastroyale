67626f90a679bc35698712d92e4014f2b292c75d
Nikita Denissov <nik.denissov@gmail.com>
2024-01-29 13:16:20 +0000

    Remove "Show Real Damage" toggle as we now always show real damage

--- Assets/Src/FirstLight/Game/Data/ClientOnly/AppData.cs
+++ Assets/Src/FirstLight/Game/Data/ClientOnly/AppData.cs
@@ -64,7 +64,6 @@ namespace FirstLight.Game.Data
 		
 		public FrameSnapshot LastCapturedFrameSnapshot;
 
-		public bool ShowRealDamage = false;
 		public bool SfxEnabled = true;
 		public bool BgmEnabled = true;
 		public bool HapticEnabled = true;

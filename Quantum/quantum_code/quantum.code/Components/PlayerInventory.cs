e0c55691870d04f05c7d67e619e42a9943c80e57
Carlos Feldmann <carlos@feldmann.dev>
2024-02-29 11:26:14 +0000

    fix: refactored FTUEStateMachine to not rely on quantum events (pull request #3342)
    Fix FTUEMap state machine
    
    * fix: tutorial based on events
    
    
    Approved-by: Matt Ryan
    Approved-by: Nik Denissov
    Approved-by: Gabriel

--- Quantum/quantum_code/quantum.code/Components/PlayerInventory.cs
+++ Quantum/quantum_code/quantum.code/Components/PlayerInventory.cs
@@ -12,8 +12,21 @@ namespace Quantum
 				}
 
 				Specials[i] = special;
-				f.Events.OnPlayerSpecialUpdated(player, playerEntity, (uint) i, special);
+				f.Events.OnPlayerSpecialUpdated(player, playerEntity, (uint)i, special);
 				return true;
+			}
+
+			return false;
+		}
+
+		public bool HasAnySpecial()
+		{
+			for (var i = 0; i < Specials.Length; i++)
+			{
+				if (Specials[i].IsValid)
+				{
+					return true;
+				}
 			}
 
 			return false;

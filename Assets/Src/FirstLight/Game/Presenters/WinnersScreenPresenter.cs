7872756afd1df6da6a39fed97301a84b8492c6b3
Matt Ryan <vertexbuffer@protonmail.com>
2024-03-08 12:04:29 +0000

    randomise start frames

--- Assets/Src/FirstLight/Game/Presenters/WinnersScreenPresenter.cs
+++ Assets/Src/FirstLight/Game/Presenters/WinnersScreenPresenter.cs
@@ -142,6 +142,13 @@ namespace FirstLight.Game.Presenters
 			}
 
 			await UniTask.WhenAll(tasks);
+			
+			_character1.RandomizeAnimationStateFrame("IdleBT", 0, 0f, 0.25f);
+			
+			if (playerDataCount > 1)
+            {
+            	_character2.RandomizeAnimationStateFrame("IdleBT", 0, 0.25f, 0.75f);
+            }
 
 			await UniTask.Delay(300);
 

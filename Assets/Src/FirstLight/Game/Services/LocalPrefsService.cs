8737762af3f1fe87fb2ead67d72c846ed05a023a
Miha Krajnc <miha@krajnc.dev>
2024-04-23 00:10:47 +0200

    Make InvertSpecialCaceling true by default

--- Assets/Src/FirstLight/Game/Services/LocalPrefsService.cs
+++ Assets/Src/FirstLight/Game/Services/LocalPrefsService.cs
@@ -32,7 +32,7 @@ namespace FirstLight.Game.Services
 		/// <summary>
 		/// Which specials canceling system is used.
 		/// </summary>
-		public ObservableField<bool> InvertSpecialCanceling { get; } = CreateBoolSetting(nameof(InvertSpecialCanceling), false);
+		public ObservableField<bool> InvertSpecialCanceling { get; } = CreateBoolSetting(nameof(InvertSpecialCanceling), true);
 
 		/// <summary>
 		/// If the Aim and Move joysticks are swapped.

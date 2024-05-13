33ce94d7d8c2691f0bef4ed048f9ebdcf0acba37
Matt Ryan <vertexbuffer@protonmail.com>
2024-05-13 12:52:05 +0100

    revert sample code

--- Quantum/quantum_code/quantum.code/BotSDK/Samples/Decisions/HFSM.TimerDecision.cs
+++ Quantum/quantum_code/quantum.code/BotSDK/Samples/Decisions/HFSM.TimerDecision.cs
@@ -11,19 +11,12 @@ namespace Quantum
 
 		public override unsafe bool Decide(Frame frame, EntityRef entity, ref AIContext aiContext)
 		{
+			var blackboard = frame.Has<AIBlackboardComponent>(entity) ? frame.Get<AIBlackboardComponent>(entity) : default;
+
 			var agent = frame.Unsafe.GetPointer<HFSMAgent>(entity);
 			var aiConfig = agent->GetConfig(frame);
 
-			FP requiredTime;
-			if (frame.Has<AIBlackboardComponent>(entity))
-			{
-				requiredTime = TimeToTrueState.Resolve(frame, entity, frame.Unsafe.GetPointer<AIBlackboardComponent>(entity), aiConfig, ref aiContext);
-			}
-			else
-			{
-				AIBlackboardComponent aiBlackboardComponent = default;
-				requiredTime = TimeToTrueState.Resolve(frame, entity, &aiBlackboardComponent, aiConfig, ref aiContext);
-			}
+			FP requiredTime = TimeToTrueState.Resolve(frame, entity, &blackboard, aiConfig, ref aiContext);
 
 			var hfsmData = &agent->Data;
 			return hfsmData->Time >= requiredTime;

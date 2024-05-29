55e4f7b4259b8a60915e9aff97ac5499ba0362e0
Miha Krajnc <miha@krajnc.dev>
2024-05-29 12:34:11 +0200

    Add friends extensions

---
+++ BlastRoyale/Assets/Src/FirstLight/Game/Utils/FriendsExtensions.cs
@@ -0,0 +1,18 @@
+using Unity.Services.Friends.Models;
+
+namespace FirstLight.Game.Utils
+{
+	/// <summary>
+	/// Helpers for the UCS Friends service.
+	/// </summary>
+	public static class FriendsExtensions
+	{
+		/// <summary>
+		/// Checks if the availability of a user / presence is online.
+		/// </summary>
+		public static bool IsOnline(this Presence presence)
+		{
+			return presence.Availability == Availability.Online;
+		}
+	}
+}

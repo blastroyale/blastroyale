97441de5cab1fb81b9937060740e6e52f2b73ac0
Gabriel Slomka <gabrielslomka@gmail.com>
2024-07-08 10:20:14 +0000

    Flat Ftue Level

--- BlastRoyale/Assets/Src/FirstLight/Game/Utils/FeatureFlags.cs
+++ BlastRoyale/Assets/Src/FirstLight/Game/Utils/FeatureFlags.cs
@@ -262,10 +262,13 @@ namespace FirstLight.Game.Utils
 				DESYNC_DETECTION = desyncDetection;
 			}
 
+			/*
 			if (TrySetFlag("TUTORIAL", overrideData, out var tutorial))
 			{
 				TUTORIAL = tutorial;
 			}
+			*/
+			TUTORIAL = true;
 
 			if (TrySetFlag("PAUSE_FREEZE", overrideData, out var pauseFreeze))
 			{

dbb520075ba54840f02ab1635424955c5f701dc7
Miha Krajnc <miha@krajnc.dev>
2024-07-11 15:49:25 +0200

    BRR-1436: Remove Friend online status delay

--- BlastRoyale/Assets/Src/FirstLight/Game/Utils/UCSExtensions/FriendsServiceExtensions.cs
+++ BlastRoyale/Assets/Src/FirstLight/Game/Utils/UCSExtensions/FriendsServiceExtensions.cs
@@ -18,16 +18,10 @@ namespace FirstLight.Game.Utils.UCSExtensions
 		/// </summary>
 		public static bool IsOnline(this Relationship relationship)
 		{
-			const int ONLINE_THRESHOLD_MINUTES = 5;
-
 			var presence = relationship.Member.Presence;
 			if (presence == null) return false;
-			if (presence.Availability != Availability.Online)
-			{
-				return DateTime.UtcNow - relationship.Member.Presence.LastSeen < TimeSpan.FromMinutes(ONLINE_THRESHOLD_MINUTES);
-			}
 
-			return true;
+			return presence.Availability == Availability.Online;
 		}
 
 		/// <summary>

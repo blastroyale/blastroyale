9017c53da5382cdaacf34f0d63f2f7e9ff9d531f
Matt Ryan <vertexbuffer@protonmail.com>
2024-08-20 13:07:50 +0100

    custom vertex and index array to draw shrinking circle danger zone

--- BlastRoyale/Assets/Src/FirstLight/Game/MonoComponent/Match/ShrinkingCircleMonoComponent.cs
+++ BlastRoyale/Assets/Src/FirstLight/Game/MonoComponent/Match/ShrinkingCircleMonoComponent.cs
@@ -16,8 +16,11 @@ namespace FirstLight.Game.MonoComponent.Match
 		[SerializeField, Required] private CircleLineRendererMonoComponent _shrinkingCircleLinerRenderer;
 		[SerializeField, Required] private CircleLineRendererMonoComponent _safeAreaCircleLinerRenderer;
 		[SerializeField, Required] private Transform _damageZoneTransform;
-		[SerializeField, Required] private ParticleSystem _ringOfFireParticle; 
-
+		[SerializeField, Required] private Transform _fireVfxZoneTransform;
+		[SerializeField, Required] private ParticleSystem _ringOfFireParticle;
+		[SerializeField, Required] private Material _damageMaterial;
+		[SerializeField, Required] private MapData _mapData;
+		
 		private QuantumShrinkingCircleConfig _config;
 		private IGameServices _services;
 		
@@ -29,6 +32,9 @@ namespace FirstLight.Game.MonoComponent.Match
 		private FP _innerRadius;
 		private FP _outerRadius;
 		private const int MaxParticles = 50;
+		private Vector3[] _vertices;
+
+		private Mesh _mesh;
 
 		private void Awake()
 		{
@@ -38,14 +44,95 @@ namespace FirstLight.Game.MonoComponent.Match
 			_shrinkingCircleLinerRenderer.gameObject.SetActive(false);
 			_safeAreaCircleLinerRenderer.gameObject.SetActive(false);
 			_damageZoneTransform.gameObject.SetActive(false);
+			_fireVfxZoneTransform.gameObject.SetActive(false);
 			_ringOfFireParticle.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
+		}
+
+		private void Start()
+		{
+			CreateDamageZoneMeshData();
+		}
+
+		private void CreateDamageZoneMeshData()
+		{
+			var meshFilter = _damageZoneTransform.gameObject.AddComponent<MeshFilter>();
+			var meshRenderer = _damageZoneTransform.gameObject.AddComponent<MeshRenderer>();
+			_mesh= meshFilter.mesh;
+			_mesh.Clear();
+			
+			meshRenderer.material = _damageMaterial;
+			var circleVertexResolution = 120;
+			var cornerPositionSize = 4;
+			
+			var totalSize = circleVertexResolution + cornerPositionSize;
+			
+			var circlePositions = new Vector3[circleVertexResolution];
+			
+			var angle = 2 * Mathf.PI / circleVertexResolution;
+
+			for (var i = 0; i < circleVertexResolution; i++)
+			{
+				var cos = Mathf.Cos(angle * i);
+				var sin = Mathf.Sin(angle * i);
+				
+				var rotationMatrix = new Matrix4x4(new Vector4(cos, sin, 0, 0),
+					new Vector4(-sin, cos, 0, 0),
+					new Vector4(0, 0, 1, 0),
+					new Vector4(0, 0, 0, 1));
+				
+				circlePositions[i] = rotationMatrix.MultiplyPoint(new Vector3(0, 1, 0));
+			}
+
+			_vertices= new Vector3[totalSize];
+			for (var i = 0; i < circleVertexResolution; i++)
+			{
+				_vertices[i].Set(circlePositions[i].x, circlePositions[i].z, circlePositions[i].y);
+			}
+			
+			// corner points
+			_vertices[totalSize-1] = new Vector3(-1, 0, 1);
+			_vertices[totalSize-2] = new Vector3(-1, 0, -1);
+			_vertices[totalSize-3] = new Vector3(1, 0, -1);
+			_vertices[totalSize-4] = new Vector3(1, 0, 1);
+			
+			var segmentResolution = circleVertexResolution / 4;
+			
+			var totalResolution = segmentResolution * 4;
+			var triangleCount = totalResolution * 3;
+			var triangles = new int[triangleCount + 12];
+			
+			for (var i = 0; i < totalResolution; i++)
+			{
+				triangles[i * 3] = i;
+				triangles[i * 3 + 1] = (i + 1 == circleVertexResolution) ? 0 : i + 1;
+				triangles[i * 3 + 2] = totalSize - (i / segmentResolution) - 1;
+			}
+			
+			triangles[triangleCount] = 0;
+			triangles[triangleCount+1] = totalSize-1;
+			triangles[triangleCount+2] = totalSize-4;
+			
+			triangles[triangleCount+3] = segmentResolution * 3;
+			triangles[triangleCount+4] = totalSize-3;
+			triangles[triangleCount+5] = totalSize-4;
+			
+			triangles[triangleCount+6] = segmentResolution * 2;
+			triangles[triangleCount+7] = totalSize-3;
+			triangles[triangleCount+8] = totalSize-2;
+			
+			triangles[triangleCount+9] = segmentResolution;
+			triangles[triangleCount+10] = totalSize-2;
+			triangles[triangleCount+11] = totalSize-1;
+
+			_mesh.vertices = _vertices;
+			_mesh.triangles = triangles;
 		}
 
 		private void HandleGameEnded(EventOnGameEnded callback)
 		{
 			QuantumCallback.UnsubscribeListener(this);
 		}
-
+		
 		private unsafe void HandleUpdateView(CallbackUpdateView callback)
 		{
 			var frame = callback.Game.Frames.Predicted;
@@ -57,6 +144,8 @@ namespace FirstLight.Game.MonoComponent.Match
 			_shrinkingCircleLinerRenderer.gameObject.SetActive(true);
 			_safeAreaCircleLinerRenderer.gameObject.SetActive(true);
 			_damageZoneTransform.gameObject.SetActive(true);
+			_fireVfxZoneTransform.gameObject.SetActive(true);
+
 			
 			var targetCircleCenter = circle->TargetCircleCenter.ToUnityVector2();
 			var targetRadius = circle->TargetRadius.AsFloat;
@@ -64,6 +153,20 @@ namespace FirstLight.Game.MonoComponent.Match
 			circle->GetMovingCircle(frame, out var centerFP, out var radiusFP);
 			var radius = radiusFP.AsFloat;
 			var center = centerFP.ToUnityVector2();
+			
+	
+			var cornerPtSize = _mapData.Asset.Settings.WorldSize * 2;
+			var pt1 = new Vector3(-1 * cornerPtSize, 0, 1 * cornerPtSize) / radius;
+			var pt2 = new Vector3(-1 * cornerPtSize, 0, -1 * cornerPtSize) / radius;
+			var pt3 = new Vector3(1 * cornerPtSize, 0, -1 * cornerPtSize) / radius;
+			var pt4 = new Vector3(1 * cornerPtSize, 0, 1 * cornerPtSize) / radius;
+			
+			var vertexLength = _vertices.Length;
+			_vertices[vertexLength - 1] = pt1;
+			_vertices[vertexLength - 2] = pt2;
+			_vertices[vertexLength - 3] = pt3;
+			_vertices[vertexLength - 4] = pt4;
+			_mesh.vertices = _vertices;
 			
 			var cachedShrinkingCircleLineTransform = _shrinkingCircleLinerRenderer.transform;
 			var cachedSafeAreaCircleLine = _safeAreaCircleLinerRenderer.transform;
@@ -82,7 +185,10 @@ namespace FirstLight.Game.MonoComponent.Match
 			_shrinkingCircleLinerRenderer.WidthMultiplier = 1f / radius;
 			
 			_damageZoneTransform.position = position;
-			_damageZoneTransform.localScale = new Vector3(radius * 2f, _damageZoneTransform.localScale.y, radius * 2f);
+			_damageZoneTransform.localScale = new Vector3(radius, _damageZoneTransform.localScale.y, radius);
+
+			_fireVfxZoneTransform.position = position;
+			_fireVfxZoneTransform.localScale = new Vector3(radius  * 2f, _fireVfxZoneTransform.localScale.y, radius * 2f);
 			
 			if (frame.Time < circle->ShrinkingStartTime - _config.WarningTime)
 			{

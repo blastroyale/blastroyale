134d8510ad5c4ec90a685bab7fa1f652e6fd941f
Carlos Feldmann <carlos@feldmann.dev>
2024-10-29 17:35:14 +0000

    Move map lightmaps to assetbundles (pull request #4208)
    Move map lightmaps to assetbundles
    
    * revert addressable changes
    
    * wip
    
    
    Approved-by: Gabriel

--- BlastRoyale/Assets/Src/FirstLight/Editor/Build/Builder.cs
+++ BlastRoyale/Assets/Src/FirstLight/Editor/Build/Builder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -8,6 +9,7 @@ using UnityEditor;
 using UnityEditor.AddressableAssets;
 using UnityEditor.AddressableAssets.Build;
 using UnityEditor.AddressableAssets.Settings;
+using UnityEditor.AddressableAssets.Settings.GroupSchemas;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 using UnityEngine.Assertions;
@@ -39,6 +41,12 @@ namespace FirstLight.Editor.Build
 				"ServerData/Android/addressables_content_state.bin");
 		}
 
+		[MenuItem("FLG/Build/Addressable no debug")]
+		public static void BuildAddressablesNoDebug()
+		{
+			BuildAddressables(false);
+		}
+
 		/// <summary>
 		/// Combines the configure and build steps
 		/// </summary>
@@ -61,7 +69,7 @@ namespace FirstLight.Editor.Build
 			SetupScenes(ref buildConfig);
 			SetupPath(ref buildConfig, buildTarget);
 
-			BuildAddressables();
+			BuildAddressables(isDevelopmentBuild);
 
 			AssetDatabase.Refresh();
 
@@ -112,12 +120,47 @@ namespace FirstLight.Editor.Build
 			AddressableAssetSettingsDefaultObject.Settings.activeProfileId = profileId;
 		}
 
-		private static void BuildAddressables()
+		private static void BuildAddressables(bool enableDebugPackages)
 		{
+			if (!enableDebugPackages) RemoveDebugBundles();
 			AddressableAssetSettings.BuildPlayerContent(out var result);
+			if (!enableDebugPackages) AddDebugBundlesBack();
 			if (!string.IsNullOrEmpty(result.Error))
 			{
 				throw new Exception(result.Error);
+			}
+		}
+
+		private static bool IsDebugGroup(AddressableAssetGroup assetGroup)
+		{
+			return !assetGroup.ReadOnly && (assetGroup.name.EndsWith("_Dev") || assetGroup.name.StartsWith("Dev_"));
+		}
+
+		private static void RemoveDebugBundles()
+		{
+			foreach (var group in AddressableAssetSettingsDefaultObject.Settings.groups)
+			{
+				if (!IsDebugGroup(group)) continue;
+				var schema = group.GetSchema<BundledAssetGroupSchema>();
+				schema.IncludeInBuild = false;
+				schema.IncludeAddressInCatalog = false;
+				schema.IncludeLabelsInCatalog = false;
+				schema.IncludeGUIDInCatalog = false;
+				EditorUtility.SetDirty(schema);
+			}
+		}
+
+		private static void AddDebugBundlesBack()
+		{
+			foreach (var group in AddressableAssetSettingsDefaultObject.Settings.groups)
+			{
+				if (!IsDebugGroup(group)) continue;
+				var schema = group.GetSchema<BundledAssetGroupSchema>();
+				schema.IncludeInBuild = true;
+				schema.IncludeAddressInCatalog = true;
+				schema.IncludeLabelsInCatalog = false;
+				schema.IncludeGUIDInCatalog = true;
+				EditorUtility.SetDirty(schema);
 			}
 		}
 

6153f89788e6c51d0bce9e289535461fd4ab4a2f
Carlos Feldmann <carlos@feldmann.dev>
2024-11-11 14:39:39 +0000

    remove imports (pull request #4224)
    remove imports
    
    * remove imports

--- BlastRoyale/Assets/Src/FirstLight/Game/Cheats/SROptions.Performance.cs
+++ BlastRoyale/Assets/Src/FirstLight/Game/Cheats/SROptions.Performance.cs
@@ -1,18 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
-using FirstLight.Game.Logic;
 using FirstLight.Game.Services;
 using FirstLight.Game.Utils;
-using Photon.Deterministic;
-using Quantum;
-using Quantum.Commands;
-using SRDebugger;
-using UnityEngine;
-using UnityEngine.Diagnostics;
-using UnityEngine.Playables;
-using Object = UnityEngine.Object;
 
 public partial class SROptions
 {

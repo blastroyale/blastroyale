c5d08470106a0f3aa0c4e912f182557b7a281fb5
Stephanie Anderson <stephanie@stephanieanderson.ltd>
2024-11-14 19:15:53 +0000

    new flags added
    importer still requires some fixes for icons

--- BlastRoyale/Assets/Src/FirstLight/Editor/AssetImporters/ContentPipeline/FlagsImporter.cs
+++ BlastRoyale/Assets/Src/FirstLight/Editor/AssetImporters/ContentPipeline/FlagsImporter.cs
@@ -1,111 +1,122 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
-using UnityEditor.AssetImporters;
+using UnityEditor.Animations;
 using UnityEditor.Presets;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace FirstLight.Editor.AssetImporters
 {
 	public class FlagsImporter : AssetPostprocessor
 	{
-		private const string PREFIX = "flag_";
-		private const string ASSET_MANAGER_PATH = "Assets/Asset Manager";
+		private static List<AssetDetails> _importedAssets;
+		private const string PREFIX = "Flag_";
 		private const string ADDRESSABLE_DIR = "Assets/AddressableResources/Collections/Flags";
-		private const string MATERIAL_DIR = "Assets/AddressableResources/Collections/Flags/m_FlagsAtlas.mat";
+		private const string PREFAB_FILE = "Assets/AddressableResources/Collections/Flags/BaseDeathFlag.prefab";
 		
 		public override int GetPostprocessOrder() => 100;
 
 		private void OnPreprocessModel()
 		{
-			if (!Path.GetFileName(assetPath).StartsWith(PREFIX)) return;
-
-			var importer = (ModelImporter) assetImporter;
-
-			// Extract textures
-			var folder = assetPath!.Remove(assetPath.LastIndexOf('/'));
-			importer.ExtractTextures(folder);
-
-			// Apply preset
-			var preset = AssetDatabase.LoadAssetAtPath<Preset>("Assets/Presets/FlagsFBX.preset");
-			preset.ApplyTo(importer);
+			Debug.Log($"is it a flag? {Path.GetFileName(assetPath)} | {Path.GetFileName(assetPath).StartsWith(PREFIX)}");
+			// if (!Path.GetFileName(assetPath).StartsWith(PREFIX)) return;
+			//
+			// var importer = (ModelImporter) assetImporter;
+			//
+			// // Extract textures
+			// var folder = assetPath!.Remove(assetPath.LastIndexOf('/'));
+			// importer.ExtractTextures(folder);
+			//
+			// // Apply preset
+			// var preset = AssetDatabase.LoadAssetAtPath<Preset>("Assets/Presets/FlagsFBX.preset");
+			// preset.ApplyTo(importer);
 		}
-		
 
+
+		private class AssetDetails
+		{
+			public string Destination;
+			public string AssetPath;
+			public string ImportDirectory;
+		}
 		private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
 												   string[] movedFromAssetPaths)
 		{
-			var assetsAdded = false;
-
-			var material = AssetDatabase.LoadAssetAtPath<Material>(MATERIAL_DIR);
-			
+			var basePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(PREFAB_FILE);
+			_importedAssets = new List<AssetDetails>();
 			foreach (var assetPath in importedAssets)
 			{
-				if (AssetDatabase.IsValidFolder(assetPath) && assetPath.StartsWith(ASSET_MANAGER_PATH))
+				Debug.Log($"Check asset : {assetPath} | {AssetDatabase.IsValidFolder(assetPath)}");
+				if (AssetDatabase.IsValidFolder(assetPath))
 				{
 					// New asset has been imported
 					var directoryName = Path.GetFileName(assetPath)!;
 
 					if (directoryName.StartsWith(PREFIX))
 					{
-						var assetName = directoryName[PREFIX.Length..];
-						var assetFbxFilename = $"{PREFIX}{assetName}.fbx";
-						var assetFbxPath = Path.Combine(assetPath, assetFbxFilename);
-						var assetPrefabFilename = $"{PREFIX}{assetName}.prefab";
-						var assetFbx = (GameObject) AssetDatabase.LoadMainAssetAtPath(assetFbxPath);
+						var assetName = Path.GetFileNameWithoutExtension(assetPath);
+						var assetFbxFilename = $"{assetName}.fbx";
+						var assetFbxPath = $"{assetPath}/{assetFbxFilename}";
+						var assetPrefabFilename = $"{assetName}.prefab";
+						var destination = $"{ADDRESSABLE_DIR}/{assetName}";
+						if (!Directory.Exists(destination))
+						{
+							Directory.CreateDirectory(destination);
+						}
+						Debug.Log($"Importing new asset: {destination} | {assetName} | {assetFbxPath} | {Path.Combine(destination, assetFbxFilename)}");
 
-						Debug.Log($"Importing new asset: {assetName}");
-
-						
 						// We need to create a variant because for some reason when we set the animator controller
 						// directly on the FBX it loses the reference.
-						var instantiatedPrefab = (GameObject) PrefabUtility.InstantiatePrefab(assetFbx);
-						instantiatedPrefab.GetComponent<MeshRenderer>().sharedMaterial = material;
+						var instantiatedPrefab = (GameObject) PrefabUtility.InstantiatePrefab(basePrefab);
+						var meshRenderer = instantiatedPrefab.GetComponentInChildren<SkinnedMeshRenderer>();
+						var assetFbx = AssetDatabase.LoadAllAssetsAtPath(assetFbxPath);
+						foreach (var asset in assetFbx)
+						{
+							if (asset is Mesh)
+							{
+								Debug.Log($"Applying mesh {assetFbxPath} | {assetFbx}");
 						
+								meshRenderer.sharedMesh = asset as Mesh;
+							}
+						}
+
 						// Create prefab variant
-						PrefabUtility.SaveAsPrefabAssetAndConnect(instantiatedPrefab, Path.Combine(assetPath, assetPrefabFilename),
+						PrefabUtility.SaveAsPrefabAssetAndConnect(instantiatedPrefab, $"{destination}/{assetPrefabFilename}",
 							InteractionMode.AutomatedAction, out var success);
 						Object.DestroyImmediate(instantiatedPrefab);
-
+						_importedAssets.Add(new AssetDetails{AssetPath = assetFbxPath, Destination = $"{destination}/{assetFbxFilename}", ImportDirectory = assetPath});
+						
 						if (!success)
 						{
 							Debug.LogError($"Error creating prefab for {assetName}.");
 							continue;
 						}
-
-						assetsAdded = true;
 					}
 				}
 			}
-
-			// TODO: If we move the asset directly here it loses the Asset Manager link, this is a workaround
-			if (assetsAdded)
-			{
-				DelayedEditorCall.DelayedCall(MoveAssets, 0.5f);
-			}
+			
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+			MoveAssets();
 		}
 
 		private static void MoveAssets()
 		{
-			var folders = AssetDatabase.GetSubFolders(ASSET_MANAGER_PATH);
-
-			foreach (var folder in folders)
+			foreach (var asset in _importedAssets)
 			{
-				var directoryName = Path.GetFileName(folder);
-				var gliderName = directoryName[PREFIX.Length..];
-
-				if (!directoryName.StartsWith(PREFIX)) continue;
-
-				Debug.Log($"Moving asset: {directoryName}");
-
-				var destination = Path.Combine(ADDRESSABLE_DIR, $"{gliderName}");
-				var result = AssetDatabase.MoveAsset(folder, destination);
-				if (!string.IsNullOrEmpty(result))
-				{
-					Debug.LogError($"Error moving asset {gliderName}: {result}");
-				}
-
-				AssetDatabase.ImportAsset(destination, ImportAssetOptions.ImportRecursive);
+				AssetDatabase.MoveAsset(asset.AssetPath, asset.Destination);
 			}
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+			foreach (var asset in _importedAssets)
+			{
+				Debug.Log(asset.AssetPath);
+				AssetDatabase.DeleteAsset(asset.ImportDirectory);
+			}
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
 		}
 	}
 }

33ced34df40d7625223a7ea71c739bef64119ffe
Carlos Feldmann <carlos@feldmann.dev>
2024-11-20 16:18:26 +0000

    Fix indicators not loading properly (pull request #4239)
    Fix indicators not loading properly
    
    * wip
    
    
    Approved-by: Gabriel

--- BlastRoyale/Assets/Src/FirstLight/Game/Cheats/SROptions.Performance.cs
+++ BlastRoyale/Assets/Src/FirstLight/Game/Cheats/SROptions.Performance.cs
@@ -4,20 +4,32 @@ using FirstLight.Game.Utils;
 
 public partial class SROptions
 {
-	[Category("Performance")] public PerformanceMode CurrentMode => MainInstaller.ResolveServices().GameAppService.PerformanceManager.Mode;
-	
+	[Category("Performance")]
+	public PerformanceMode CurrentMode
+	{
+		get
+		{
+			if (MainInstaller.TryResolve<IGameServices>(out var services))
+			{
+				return services.GameAppService.PerformanceManager.Mode;
+			}
+
+			return PerformanceMode.Mid;
+		}
+	}
+
 	[Category("Performance")]
 	public void LowMode()
 	{
 		MainInstaller.ResolveServices().GameAppService.PerformanceManager.UpdatePerformanceMode(PerformanceMode.Low);
 	}
-	
+
 	[Category("Performance")]
 	public void MidMode()
 	{
 		MainInstaller.ResolveServices().GameAppService.PerformanceManager.UpdatePerformanceMode(PerformanceMode.Mid);
 	}
-	
+
 	[Category("Performance")]
 	public void HighMode()
 	{
